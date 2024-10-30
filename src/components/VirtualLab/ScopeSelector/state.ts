bf3b2de8d0c96c9645e0f21d1c00f376f22d20f0
Gil Arturo Barrios Del Villar <gil.barriosdelvillar@epfl.ch>
2024-10-30 17:26:42 +0100

    implement grid styles

--- src/components/VirtualLab/ScopeSelector/state.ts
+++ src/components/VirtualLab/ScopeSelector/state.ts
@@ -1,8 +1,13 @@
 import { atom } from 'jotai';
 import { memoize } from '@/util/utils';
+import { SimulationType } from '@/types/virtual-lab/lab';
 
 type Tab = 'new' | 'browse';
 
 export const selectedTabFamily = memoize((_key: string) => {
   return atom<Tab>('new');
+});
+
+export const selectedSimTypeFamily = memoize((_key: string) => {
+  return atom<SimulationType | null>(null);
 });
