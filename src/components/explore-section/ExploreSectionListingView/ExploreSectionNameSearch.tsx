69ea366f964a9435096278a5bfad0e9d92ad0f6e
Gil Arturo Barrios Del Villar <gil.barriosdelvillar@epfl.ch>
2024-11-26 17:13:44 +0100

    simplify ExploreSectionNameSearch

--- src/components/explore-section/ExploreSectionListingView/ExploreSectionNameSearch.tsx
+++ src/components/explore-section/ExploreSectionListingView/ExploreSectionNameSearch.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, RefObject, useEffect, useRef } from 'react';
+import { ChangeEvent, RefObject, useEffect, useRef, useState } from 'react';
 import { useAtom } from 'jotai';
 import { SearchOutlined } from '@ant-design/icons';
 
@@ -15,30 +15,37 @@ type SearchProps = {
 
 export default function ExploreSectionNameSearch({ dataType, dataScope }: SearchProps) {
   const [searchString, setSearchString] = useAtom(searchStringAtom({ dataType, dataScope }));
+  const [searchStringLocal, setSearchStringLocal] = useState(searchString);
 
   const searchInputRef: RefObject<HTMLInputElement> = useRef(null);
-  useEffect(() => searchInputRef?.current?.focus(), []); // Auto-focus on render
 
-  const updateAtom = useDebouncedCallback(
-    (e: ChangeEvent<HTMLInputElement>) => setSearchString(e.target.value),
+  const debouncedUpdateAtom = useDebouncedCallback(
+    (searchStr: string) => setSearchString(searchStr),
     [setSearchString],
     600
   );
 
+  useEffect(() => {
+    debouncedUpdateAtom(searchStringLocal);
+  }, [searchStringLocal, debouncedUpdateAtom]);
+
+  // "Clear filters" side-effect
+  useEffect(() => {
+    setSearchStringLocal(searchString);
+  }, [searchString, setSearchStringLocal]);
+
   return (
-    <>
-      <div className="mx-auto flex w-full max-w-2xl items-center border-b border-neutral-2 focus-within:border-b-primary-8">
-        <input
-          className="w-full bg-transparent py-2 text-primary-7 placeholder:text-neutral-3"
-          style={{ outline: 'unset' }}
-          onChange={updateAtom}
-          ref={searchInputRef}
-          placeholder="Search for resources..."
-          type="text"
-          value={searchString}
-        />
-        <SearchOutlined className="py-2 text-primary-8" />
-      </div>
-    </>
+    <div className="mx-auto flex w-full max-w-2xl items-center border-b border-neutral-2 focus-within:border-b-primary-8">
+      <input
+        className="w-full bg-transparent py-2 text-primary-7 placeholder:text-neutral-3"
+        style={{ outline: 'unset' }}
+        onInput={(e: ChangeEvent<HTMLInputElement>) => setSearchStringLocal(e.target.value)}
+        ref={searchInputRef}
+        placeholder="Search for resources..."
+        type="text"
+        value={searchStringLocal}
+      />
+      <SearchOutlined className="py-2 text-primary-8" />
+    </div>
   );
 }
