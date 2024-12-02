2e8953d4e6f72a105ba9e7677d3ddd6a030bc944
Gil Arturo Barrios Del Villar <gil.barriosdelvillar@epfl.ch>
2024-12-02 08:43:45 +0000

    Fix explore search

--- src/components/explore-section/ExploreSectionListingView/ExploreSectionNameSearch.tsx
+++ src/components/explore-section/ExploreSectionListingView/ExploreSectionNameSearch.tsx
@@ -1,50 +1,12 @@
-import {
-  ChangeEvent,
-  ForwardedRef,
-  HTMLProps,
-  RefObject,
-  forwardRef,
-  useCallback,
-  useEffect,
-  useRef,
-  useState,
-} from 'react';
+import { ChangeEvent, RefObject, useEffect, useRef, useState } from 'react';
 import { useAtom } from 'jotai';
 import { SearchOutlined } from '@ant-design/icons';
-import debounce from 'lodash/debounce';
+
 import { searchStringAtom } from '@/state/explore-section/list-view-atoms';
-import useForwardRef from '@/hooks/useForwardRef';
+
 import { DataType } from '@/constants/explore-section/list-views';
 import { ExploreDataScope } from '@/types/explore-section/application';
-
-const Input = forwardRef(
-  (
-    { onBlur, onInput, placeholder = 'Search', type, value }: HTMLProps<HTMLInputElement>,
-    forwardedRef: ForwardedRef<HTMLInputElement>
-  ) => {
-    const ref = useForwardRef<HTMLInputElement>(forwardedRef);
-
-    useEffect(() => ref?.current?.focus(), [ref]); // Auto-focus on render
-
-    return (
-      <div className="mx-auto flex w-full max-w-2xl items-center border-b border-neutral-2 focus-within:border-b-primary-8">
-        <input
-          className="w-full bg-transparent py-2 text-primary-7 placeholder:text-neutral-3"
-          style={{ outline: 'unset' }}
-          onBlur={onBlur}
-          onInput={onInput}
-          ref={ref}
-          placeholder={placeholder}
-          type={type}
-          value={value}
-        />
-        <SearchOutlined className="py-2 text-primary-8" />
-      </div>
-    );
-  }
-);
-
-Input.displayName = 'Input';
+import { useDebouncedCallback } from '@/hooks/hooks';
 
 type SearchProps = {
   dataType: DataType;
@@ -52,35 +14,45 @@ type SearchProps = {
 };
 
 export default function ExploreSectionNameSearch({ dataType, dataScope }: SearchProps) {
-  const [searchStringAtomValue, setSearchStringAtomValue] = useAtom(
-    searchStringAtom({ dataType, dataScope })
-  );
-  const [searchStringLocalState, setSearchStringLocalState] = useState(searchStringAtomValue);
+  const [searchString, setSearchString] = useAtom(searchStringAtom({ dataType, dataScope }));
 
-  const searchInput: RefObject<HTMLInputElement> = useRef(null);
+  const searchInputRef: RefObject<HTMLInputElement> = useRef(null);
+  useEffect(() => searchInputRef?.current?.focus(), []); // Auto-focus on render
 
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  const debouncedUpdateAtom = useCallback(
-    debounce((searchStr: string) => setSearchStringAtomValue(searchStr), 600),
-    [setSearchStringAtomValue]
+  const debouncedUpdateAtom = useDebouncedCallback(
+    (searchStr: string) => setSearchString(searchStr),
+    [setSearchString],
+    600
   );
+
+  /* TODO: Remove these effects and local state, they're only needed because lodash's 
+  debounce can't update the atom for some reason. 
+  Use atomWithDebounce recipe instead: https://jotai.org/docs/recipes/atom-with-debounce */
+
+  const [searchStringLocal, setSearchStringLocal] = useState(searchString);
 
   useEffect(() => {
-    debouncedUpdateAtom(searchStringLocalState);
-  }, [searchStringLocalState, debouncedUpdateAtom]);
+    debouncedUpdateAtom(searchStringLocal);
+  }, [searchStringLocal, debouncedUpdateAtom]);
 
   // "Clear filters" side-effect
   useEffect(() => {
-    setSearchStringLocalState(searchStringAtomValue);
-  }, [searchStringAtomValue, setSearchStringLocalState]);
+    setSearchStringLocal(searchString);
+  }, [searchString, setSearchStringLocal]);
 
   return (
-    <Input
-      onInput={(e: ChangeEvent<HTMLInputElement>) => setSearchStringLocalState(e.target.value)}
-      ref={searchInput}
-      placeholder="Search for resources..."
-      type="text"
-      value={searchStringLocalState}
-    />
+    <div className="mx-auto flex w-full max-w-2xl items-center border-b border-neutral-2 focus-within:border-b-primary-8">
+      <input
+        className="w-full bg-transparent py-2 text-primary-7 placeholder:text-neutral-3"
+        style={{ outline: 'unset' }}
+        onInput={(e: ChangeEvent<HTMLInputElement>) => setSearchStringLocal(e.target.value)}
+        ref={searchInputRef}
+        placeholder="Search for resources..."
+        type="text"
+        value={searchStringLocal}
+        aria-label="Search for resources"
+      />
+      <SearchOutlined className="py-2 text-primary-8" />
+    </div>
   );
 }
