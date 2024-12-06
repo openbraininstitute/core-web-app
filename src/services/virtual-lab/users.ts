4bf6580944d67213c2ca7039909ff36e90f9e2cb
Dinika Saxena <dinika.saxena@epfl.ch>
2024-12-06 08:33:11 +0000

    Fetch current logged in user's agent to add as contributor to me-model

--- src/services/virtual-lab/users.ts
+++ src/services/virtual-lab/users.ts
@@ -1,6 +1,7 @@
-import { TotalUsersResponse } from '@/types/virtual-lab/members';
+import { TotalUsersResponse, Agent } from '@/types/virtual-lab/members';
 import { virtualLabApi } from '@/config';
 import authFetch from '@/authFetch';
+import { VlmResponse } from '@/types/virtual-lab/common';
 
 export async function getTotalUsers(): Promise<TotalUsersResponse> {
   const response = await authFetch(`${virtualLabApi.url}/users_count`);
@@ -8,5 +9,15 @@ export async function getTotalUsers(): Promise<TotalUsersResponse> {
   if (!response.ok) {
     throw new Error(`Status: ${response.status}`);
   }
+  return response.json();
+}
+
+export async function getAgentForUser(): Promise<VlmResponse<Agent>> {
+  const response = await authFetch(`${virtualLabApi.url}/agent`);
+
+  if (!response.ok) {
+    throw new Error(`Agent could not be fetched. Status: ${response.status}`);
+  }
+
   return response.json();
 }
