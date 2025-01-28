0da4832cbb0b44de332709a528fe29465f30ec2a
Pavlo Getta <pavlo.getta@openbraininstitute.org>
2025-01-28 16:27:40 +0100

    Revert "New landing page: fix for Keycloak (#6)"
    This reverts commit 64b13c697136ec0d9c15c94749fdb870c1cc9455.

--- src/components/LandingPage/Hero/LoginPanel/LoginPanel.tsx
+++ src/components/LandingPage/Hero/LoginPanel/LoginPanel.tsx
@@ -1,16 +1,9 @@
 import React from 'react';
 
 import Button from '../../Button';
-import { basePath } from '@/config';
 
 export interface LoginPanelProps {}
 
 export default function LoginPanel() {
-  return (
-    <Button
-      onClick={`/log-in?callbackUrl=${basePath}/virtual-lab`}
-      subTitle="Login to"
-      title="The platform"
-    />
-  );
+  return <Button onClick="/log-in" subTitle="Login to" title="The platform" />;
 }
