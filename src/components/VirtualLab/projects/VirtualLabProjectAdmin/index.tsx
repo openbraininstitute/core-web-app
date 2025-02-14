4e78796c3671d1c79a34121326a7b1c5c874569f
Pavlo Getta <pavlo.getta@openbraininstitute.org>
2025-02-14 10:36:45 +0100

    Clean up empty project admin page

--- src/components/VirtualLab/projects/VirtualLabProjectAdmin/index.tsx
+++ src/components/VirtualLab/projects/VirtualLabProjectAdmin/index.tsx
@@ -1,19 +1,5 @@
 'use client';
 
-import { atomWithExpiration } from '@/util/atoms';
-import { useAtom } from 'jotai';
-import { atomWithRefresh } from 'jotai/utils';
-import { useEffect } from 'react';
-
-const dataAtom = atomWithExpiration(
-  async () => {
-    return fetch('https://jsonplaceholder.typicode.com/posts').then((response) => response.json());
-  },
-  {
-    ttl: 1000,
-  }
-);
-
 export default function VirtualLabProjectAdmin({
   virtualLabId,
   projectId,
@@ -21,16 +7,9 @@ export default function VirtualLabProjectAdmin({
   virtualLabId: string;
   projectId: string;
 }) {
-  const [data, refreshData] = useAtom(dataAtom);
-
-  // useEffect(() => {
-  //   refreshData();
-  // }, [refreshData]);
-
   return (
     <div>
-      <h1>VirtualLabProjectAdmin</h1>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
+      <h1>Project admin page</h1>
     </div>
   );
 }
