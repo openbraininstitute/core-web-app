85fd6941f9cd05a256bb7fbb09c8cf61b906a479
Gil Arturo Barrios del Villar <gilarturo.barriosdelvillar@openbraininstitute.org>
2025-02-17 18:24:57 +0100

    better error handling

--- src/app/virtual-lab/lab/[virtualLabId]/project/[projectId]/(pages)/notebooks/page.tsx
+++ src/app/virtual-lab/lab/[virtualLabId]/project/[projectId]/(pages)/notebooks/page.tsx
@@ -1,12 +1,28 @@
 import NotebookTable from './NotebookTable';
 import { notebookRepoUrl } from '@/config';
 import { ServerSideComponentProp } from '@/types/common';
+import { assertErrorMessage } from '@/util/utils';
 import fetchNotebooks from '@/util/virtual-lab/fetchNotebooks';
+import { Notebook } from '@/util/virtual-lab/github';
 
 export default async function Notebooks({
   params,
 }: ServerSideComponentProp<{ projectId: string; virtualLabId: string }>) {
   const { projectId, virtualLabId } = params;
-  const notebooks = await fetchNotebooks(notebookRepoUrl, true);
-  return <NotebookTable notebooks={notebooks} projectId={projectId} vlabId={virtualLabId} />;
+  let error = '';
+  let notebooks: Notebook[] = [];
+  try {
+    notebooks = await fetchNotebooks(notebookRepoUrl, true);
+  } catch (e) {
+    error = assertErrorMessage(e);
+  }
+
+  return (
+    <NotebookTable
+      notebooks={notebooks}
+      projectId={projectId}
+      vlabId={virtualLabId}
+      serverError={error}
+    />
+  );
 }
