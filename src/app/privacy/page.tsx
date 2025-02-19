bdf2c1a243a9aec86f11dc77e3381c9a75dcb07d
Gil Arturo Barrios del Villar <gilarturo.barriosdelvillar@openbraininstitute.org>
2025-02-19 11:49:05 +0100

    prettier

--- src/app/privacy/page.tsx
+++ src/app/privacy/page.tsx
@@ -3,11 +3,11 @@
 import SectionGeneric from '@/components/LandingPage/sections/SectionGeneric';
 import PaddingBlock from '@/components/LandingPage/components/PaddedBlock/PaddedBlock';
 import Hero from '@/components/LandingPage/layout/Hero/Hero';
-import styles from '@/components/LandingPage/LandingPage.module.css';
 import LogoAsLink from '@/components/logo/as-link';
 
 import { EnumSection } from '@/components/LandingPage/sections/sections';
 import { classNames } from '@/util/utils';
+import styles from '@/components/LandingPage/LandingPage.module.css';
 
 export default function page() {
   return (
