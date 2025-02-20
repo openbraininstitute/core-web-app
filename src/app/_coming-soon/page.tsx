62888e4a355de165cb219dce1f22d809c22ea83d
bilalesi <b_meddah@esi.dz>
2025-02-20 12:31:17 +0100

    use full height to fix mobiles

--- src/app/_coming-soon/page.tsx
+++ src/app/_coming-soon/page.tsx
@@ -6,23 +6,26 @@ import NewsletterForm from '@/components/coming-soon/newsletter-form';
 import Header from '@/components/coming-soon/header';
 import Main from '@/components/coming-soon/main';
 import Hero from '@/components/coming-soon/hero';
+import useFullHeight from '@/hooks/useFullHeight';
 import { classNames } from '@/util/utils';
 
 export default function Page() {
+  const height = useFullHeight();
   const [hydrated, setHydrated] = useState(false);
   useEffect(() => setHydrated(true), []);
 
   if (!hydrated) return null;
   return (
-    <div className="relative min-h-screen overflow-hidden bg-primary-9">
+    <div className="relative min-h-screen overflow-hidden bg-primary-9" style={{ height }}>
       <Header />
-      <div className="relative h-svh">
+      <div className="relative min-h-screen">
         <Hero />
+        <div className="absolute inset-0 bg-black/30" />
         <main
           className={classNames(
             'absolute inset-0 z-50 mx-auto h-svh max-w-3xl',
             'flex flex-col items-center justify-center',
-            'px-6 sm:px-12'
+            'px-4 sm:px-12'
           )}
         >
           <div className="flex w-full min-w-full flex-col items-start justify-start">
@@ -31,7 +34,7 @@ export default function Page() {
           </div>
         </main>
       </div>
-      <div className="absolute bottom-3 left-3 z-50 px-2 py-2 text-base text-white shadow-md md:px-[19px]">
+      <div className="fixed bottom-3 left-3 z-50 px-2 py-2 text-base text-white shadow-md md:px-[19px]">
         Copyright &copy; {new Date().getFullYear()} – Open Brain Institute
       </div>
     </div>
