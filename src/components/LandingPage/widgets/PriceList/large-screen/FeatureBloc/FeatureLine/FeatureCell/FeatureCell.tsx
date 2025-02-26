1d5050760046d7a901f1a1467f181e2ef2390d50
Tolokoban <contact@tolokoban.org>
2025-02-26 15:48:30 +0100

    Fourth round of fixes (#93)
    - (13) subcellular / cellular / circuit / .. grid : is not obvious we have other scale on mobile
    - (6) The news list is too long. Add a More button
    - (50) Currency combo goes behind the plans header.
    - Bottom margin of 42 in paragraphs sections
    - (51) on the desktop, we've lost the little (i) info button over the 'Partial build'.

--- src/components/LandingPage/widgets/PriceList/large-screen/FeatureBloc/FeatureLine/FeatureCell/FeatureCell.tsx
+++ src/components/LandingPage/widgets/PriceList/large-screen/FeatureBloc/FeatureLine/FeatureCell/FeatureCell.tsx
@@ -16,7 +16,19 @@ export interface FeatureCellProps {
 
 export default function FeatureCell({ feature, plan, available }: FeatureCellProps) {
   const item = feature.plans.find((p) => p.id === plan.id);
-  if (!item) return <div />;
+  if (!item)
+    return (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        width="1.5em"
+        height="1.5em"
+        fill="var(--color-neutral)"
+      >
+        <title>minus</title>
+        <path d="M19,13H5V11H19V13Z" />
+      </svg>
+    );
 
   const cls = classNames(styles.featureCell, !available && styles.unavailable);
   if (!item.label)
@@ -28,8 +40,20 @@ export default function FeatureCell({ feature, plan, available }: FeatureCellPro
     );
 
   return (
-    <div className={cls}>
+    <button className={classNames(cls, styles.tooltip)} type="button" data-tooltip={item.tooltip}>
       <span>{item.label}</span>
-    </div>
+      {item.tooltip && (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          width="1.5em"
+          height="1.5em"
+          fill="currentColor"
+        >
+          <title>information-outline</title>
+          <path d="M11,9H13V7H11M12,20C7.59,20 4,16.41 4,12C4,7.59 7.59,4 12,4C16.41,4 20,7.59 20,12C20,16.41 16.41,20 12,20M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M11,17H13V11H11V17Z" />
+        </svg>
+      )}
+    </button>
   );
 }
