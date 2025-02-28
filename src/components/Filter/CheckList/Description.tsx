223913d8554a4a2f2c821c12c67281ee4d5ecee0
bilalesi <b_meddah@esi.dz>
2025-02-28 14:24:57 +0100

    update etype values in filter, fix synapse config data

--- src/components/Filter/CheckList/Description.tsx
+++ src/components/Filter/CheckList/Description.tsx
@@ -2,9 +2,22 @@ import { useAtomValue } from 'jotai';
 import { useMemo } from 'react';
 import { unwrap } from 'jotai/utils';
 import { Spin } from 'antd';
+import findKey from 'lodash/findKey';
+import replace from 'lodash/replace';
+import get from 'lodash/get';
+
+import EXPLORE_FIELDS_CONFIG from '@/constants/explore-section/fields-config';
 import { cellTypesByLabelAtom } from '@/state/build-section/cell-types';
 import { FieldType } from '@/constants/explore-section/fields-config/types';
-import EXPLORE_FIELDS_CONFIG from '@/constants/explore-section/fields-config';
+import { ClassNexus } from '@/api/ontologies/types';
+
+// NOTE: this due nexus es aggregation and the resource it self are having different keys name
+// one with underscore, and the other with hyphen
+const getClassByLabel = (label: string, classes: Record<string, ClassNexus>) => {
+  const normalizedLabel = replace(label, /[_-]/g, '');
+  const matchedKey = findKey(classes, (_, key) => replace(key, /[_-]/g, '') === normalizedLabel);
+  return get(classes, matchedKey ?? '', null);
+};
 
 export function CheckListDescription({
   label,
@@ -26,6 +39,6 @@ function ClassDescription({ label }: { label: string }) {
   if (!classes) {
     return <Spin />;
   }
-
-  return <span className="text-primary-1">{classes[label]?.definition}</span>;
+  const classObj = getClassByLabel(label, classes);
+  return <span className="text-primary-1">{classObj?.definition}</span>;
 }
