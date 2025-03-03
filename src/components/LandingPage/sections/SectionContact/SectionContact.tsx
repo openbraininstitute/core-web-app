2a3c08648bfb1cbad87ed033aa33dec6d357cbb7
Tolokoban <contact@tolokoban.org>
2025-03-03 16:16:04 +0100

    Fix contact page

--- src/components/LandingPage/sections/SectionContact/SectionContact.tsx
+++ src/components/LandingPage/sections/SectionContact/SectionContact.tsx
@@ -2,12 +2,10 @@ import React from 'react';
 
 import SectionGeneric from '../SectionGeneric';
 import { EnumSection } from '../sections';
-import Milestones from '../../widgets/milestones';
 
 export default function SectionContact() {
   return (
     <>
-      <Milestones />
       <SectionGeneric section={EnumSection.Contact} />
     </>
   );
