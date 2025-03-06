594e28a5f54b54de3301271ca0da76f9e9abae02
Gil Arturo Barrios del Villar <gilarturo.barriosdelvillar@openbraininstitute.org>
2025-03-06 22:30:24 +0100

    use hook

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -7,17 +7,17 @@ import FooterPanel from './layout/FooterPanel';
 import Menu from './layout/Menu';
 import SectionContact from './sections/SectionContact';
 import { EnumSection } from './sections/sections';
+import { useSanity } from './content/content';
 import { getSection } from './utils';
 import Hero from './layout/Hero';
 import SectionGeneric from './sections/SectionGeneric';
 import PaddedBlock from './components/PaddedBlock';
 import SectionNews from './sections/SectionNews';
 import VerticalSpace from './components/VerticalSpace';
-import { isServer } from '@/config';
 import { classNames } from '@/util/utils';
 import AcceptInviteErrorDialog from '@/components/Invites/AcceptInviteErrorDialog';
 import { logError } from '@/util/logger';
-import { ComingSoonData } from '@/types/common';
+import { ComingSoonData, comingSoonDataSchema } from '@/types/common';
 
 import styles from './LandingPage.module.css';
 import './global.css';
@@ -25,21 +25,22 @@ import './global.css';
 export interface LandingPageProps {
   className?: string;
   section: EnumSection;
-  popUpData?: ComingSoonData;
   errorCode?: string;
 }
 
-export default function LandingPage({
-  className,
-  section,
-  errorCode,
-  popUpData,
-}: LandingPageProps) {
+export default function LandingPage({ className, section, errorCode }: LandingPageProps) {
   const scrollHasStarted = useScrollHasStarted();
   const [popupOpen, setPopupOpen] = useState(false);
 
+  const popUpData = useSanity(
+    `*[slug.current == "releasing-soon"][0]`,
+    (data): data is ComingSoonData => {
+      comingSoonDataSchema.parse(data);
+      return true;
+    }
+  );
+
   useEffect(() => {
-    if (isServer) return;
     setPopupOpen(localStorage.getItem('popupOpen') === null);
   }, []);
 
@@ -66,14 +67,12 @@ export default function LandingPage({
         {errorCode && <AcceptInviteErrorDialog errorCode={errorCode} />}
       </div>
 
-      {!!popUpData && (
-        <Modal open={popupOpen} onCancel={handleClose} footer={null}>
-          <div>
-            {popUpData?.title}
-            {popUpData?.introduction}
-          </div>
-        </Modal>
-      )}
+      <Modal open={popupOpen} onCancel={handleClose} footer={null}>
+        <div>
+          {popUpData?.title}
+          {popUpData?.introduction}
+        </div>
+      </Modal>
 
       {/* <MatomoAnalytics /> */}
     </>
