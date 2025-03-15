1c8c7a22f9d459e878fc4eee6715df248a113321
bilalesi <b_meddah@esi.dz>
2025-03-15 12:56:25 +0100

    allow creation of multiple virtual labs

--- src/api/virtual-lab-svc/queries/virtual-lab.ts
+++ src/api/virtual-lab-svc/queries/virtual-lab.ts
@@ -115,3 +115,41 @@ export async function listVirtualLabs({
   const result: VirtualLabListResponse = await response.json();
   return result;
 }
+
+/**
+ * List pending virtual labs for a user.
+ *
+ * @param {Object} params - Parameters for listing virtual labs.
+ * @param {number} [params.page=1] - The page number to fetch.
+ * @param {number} [params.pageSize=10] - The number of items per page.
+ * @returns {Promise<VirtualLabListResponse>} - api response with the list of virtual labs.
+ * @throws {Error} - Throws an error if the request fails or the response is invalid.
+ */
+export async function listPendingVirtualLabs({
+  page = 1,
+  pageSize = 10,
+}: {
+  page?: number;
+  pageSize?: number;
+} = {}): Promise<VirtualLabListResponse> {
+  const session = await getSession();
+
+  const url = new URL(`${BASE_URL}/pending`);
+  url.searchParams.append('page', page.toString());
+  url.searchParams.append('page_size', pageSize.toString());
+
+  const response = await fetch(url.toString(), {
+    method: 'get',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${session?.accessToken}`,
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error(`listing virtual labs failed ${response.status}`);
+  }
+
+  const result: VirtualLabListResponse = await response.json();
+  return result;
+}
