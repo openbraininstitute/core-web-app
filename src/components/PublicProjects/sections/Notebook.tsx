ae39434ba3feb65bccca1d79131eb5f757865380
Loris Olivier <53363974+loris-maru@users.noreply.github.com>
2025-03-20 20:46:16 +0100

    Added divider between notebooks

--- src/components/PublicProjects/sections/Notebook.tsx
+++ src/components/PublicProjects/sections/Notebook.tsx
@@ -3,10 +3,10 @@ import { NotebooksProps, ShowCaseProjectQueryType } from '../type';
 
 export default function NotebookSection({ content }: { content: ShowCaseProjectQueryType }) {
   return (
-    <div className="relative flex w-2/3 flex-col gap-y-12">
+    <div className="relative flex w-2/3 flex-col gap-y-5">
       {content.notebook !== null ? (
-        content.notebook.map((notebook: NotebooksProps) => (
-          <SingleNotebook key={`Notebook-${notebook.name}`} content={notebook} />
+        content.notebook.map((notebook: NotebooksProps, index: number) => (
+          <SingleNotebook key={`Notebook-${notebook.name}`} content={notebook} index={index} />
         ))
       ) : (
         <div className="text-2xl font-semibold text-neutral-4">No Notebooks available</div>
