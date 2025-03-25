ff77eefecd6a05cad28470c2be9fd6583259bf28
g-bar <gilarturo.barriosdelvillar@openbraininstitute.org>
2025-03-25 17:44:05 +0100

    Analysis fix (#178)
    Fix EModel analysis.

--- src/components/simulate/SimulationDetails/AnalysisTab.tsx
+++ src/components/simulate/SimulationDetails/AnalysisTab.tsx
@@ -1,16 +1,5 @@
 import { PDFViewerContainer } from '@/components/explore-section/common/pdf/PDFViewerContainer';
-import { AnalysisFileType } from '@/components/explore-section/common/pdf/types';
-import { FileDistribution } from '@/types/explore-section/delta-properties';
 import { NexusMEModel } from '@/types/me-model';
-import { composeUrl, ensureArray } from '@/util/nexus';
-
-const categoryMap: Record<string, string> = {
-  traces: AnalysisFileType.Traces,
-  scores: AnalysisFileType.Scores,
-  parameters_distribution: AnalysisFileType.Distribution,
-  thumbnail: AnalysisFileType.Thumbnail,
-  currentscape: AnalysisFileType.Currentscape,
-};
 
 export default function AnalysisTab({ meModel }: { meModel: NexusMEModel | null }) {
   const image = meModel?.image;
@@ -23,27 +12,5 @@ export default function AnalysisTab({ meModel }: { meModel: NexusMEModel | null 
     );
   }
 
-  const distributions = ensureArray(image).map((i) => {
-    const encodingFormat = i.about?.includes('thumbnail') ? 'image/png' : 'application/pdf';
-
-    const category = i.about?.split('/').at(-1);
-    const name = category ? categoryMap[category] : 'unknown.pdf';
-
-    const [org, project] = i['@id'].split('/').slice(-3, -1);
-
-    return {
-      ...i,
-      '@type': 'DataDownload',
-      name,
-      contentSize: {
-        unitCode: 'bytes',
-        value: 0,
-      },
-      contentUrl: composeUrl('file', i['@id'], { org, project }),
-      encodingFormat,
-      atLocation: {} as FileDistribution['atLocation'],
-    } satisfies FileDistribution;
-  });
-
-  return <PDFViewerContainer distributions={distributions} />;
+  return <PDFViewerContainer distributions={meModel.image as { '@id': string; about: string }[]} />;
 }
