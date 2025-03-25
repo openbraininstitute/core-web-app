9fec99b1fa266b169b7ce2ecc5c6fc4af1aec760
Pavlo Getta <pavlo.getta@openbraininstitute.org>
2025-03-25 10:40:09 +0100

    Use production Sanity dataset for subscription tiers

--- src/api/sanity/client.ts
+++ src/api/sanity/client.ts
@@ -6,7 +6,7 @@ import { ContentForPricing } from '@/components/LandingPage/content/pricing';
 
 const client = new SanityClient({
   projectId: 'fgi7eh1v',
-  dataset: 'staging',
+  dataset: 'production',
   apiVersion: '2023-03-25',
   useCdn: process.env.NODE_ENV === 'production',
   queries: {
