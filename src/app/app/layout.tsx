1c548c23557e5c5046af27fb36e652cd62ec72d5
Tolokoban <contact@tolokoban.org>
2025-03-27 14:55:02 +0100

    Add dialog box for the first login to accept the terms of use

--- src/app/app/layout.tsx
+++ src/app/app/layout.tsx
@@ -2,6 +2,7 @@ import { ReactNode, Suspense } from 'react';
 
 import Providers from './providers';
 import { auth } from '@/auth';
+import { TermsOfUseAcceptance } from '@/components/terms-of-use-acceptance';
 
 type RootLayoutProps = {
   children: ReactNode;
@@ -11,7 +12,10 @@ export default async function AppLayout({ children }: RootLayoutProps) {
   const session = await auth();
   return (
     <Providers session={session}>
-      <Suspense fallback={null}>{children}</Suspense>
+      <Suspense fallback={null}>
+        {children}
+        <TermsOfUseAcceptance />
+      </Suspense>
     </Providers>
   );
 }
