6ac0eecd0ca83318869fd24c6cfbd149d8145a31
Dries Verachtert <dries.verachtert@dries.eu>
2025-03-28 21:15:21 +0100

    Text below a 'Name' filter should be white instead of black
    Fixes openbraininstitute/prod-build-emodel#13

--- src/components/explore-section/ControlPanel/index.tsx
+++ src/components/explore-section/ControlPanel/index.tsx
@@ -138,7 +138,7 @@ function createFilterItemComponent(
                 updateFilterValues(filter.field, event.target.value)
               }
             />
-            <span>
+            <span className="text-white">
               Use the asterix character (<code className="text-semibold font-mono">*</code>) to
               specify a &quot;wildcard&quot; for your search. For example; to search for names{' '}
               <i>beginning with</i> &quot;AA11&quot;, specify{' '}
