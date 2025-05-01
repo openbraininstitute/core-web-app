5992fda8eb5fbfa9c55d5c1b6055ca588d006569
Pavlo Getta <pavlo.getta@openbraininstitute.org>
2025-05-01 11:16:50 +0200

    Resolve MR conflicts

--- src/app/app/layout.tsx
+++ src/app/app/layout.tsx
@@ -1,13 +1,7 @@
 import { ReactNode, Suspense } from 'react';
-import dynamic from 'next/dynamic';
 
 import Providers from '@/app/app/providers';
 import { auth } from '@/auth';
-
-const TermsOfUseAcceptance = dynamic(
-  () => import('@/components/terms-of-use-acceptance/terms-of-use-acceptance'),
-  { ssr: false }
-);
 
 type RootLayoutProps = {
   children: ReactNode;
@@ -17,10 +11,7 @@ export default async function AppLayout({ children }: RootLayoutProps) {
   const session = await auth();
   return (
     <Providers session={session}>
-      <Suspense fallback={null}>
-        {children}
-        <TermsOfUseAcceptance />
-      </Suspense>
+      <Suspense fallback={null}>{children}</Suspense>
     </Providers>
   );
 }
