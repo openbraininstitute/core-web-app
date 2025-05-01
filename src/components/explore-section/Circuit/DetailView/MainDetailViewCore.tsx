eba22f3b5a9756445fc01922e35b55a738e62794
Loris Olivier <53363974+loris-maru@users.noreply.github.com>
2025-05-02 07:15:34 +0900

    Added component to MainDetailView

--- src/components/explore-section/Circuit/DetailView/MainDetailViewCore.tsx
+++ src/components/explore-section/Circuit/DetailView/MainDetailViewCore.tsx
@@ -1,10 +1,12 @@
 import { CircuitSchemaProps } from '../type';
 import HeaderDetailView from './HeaderDetailView';
+import Visualiser from './visualisation/Visualiser';
 
 export default function MainDetailViewCore({ content }: { content: CircuitSchemaProps }) {
   return (
     <div className="relative flex w-full flex-col text-primary-9">
       <HeaderDetailView content={content} />
+      <Visualiser content={content} />
     </div>
   );
 }
