2a43aaead3e0d3fe68220533e107462d99b4b796
Loris Olivier <53363974+loris-maru@users.noreply.github.com>
2025-05-06 13:43:28 +0200

    Added circuit detail view to the project explore

---
+++ src/app/app/virtual-lab/lab/[virtualLabId]/project/[projectId]/explore/interactive/(data)/model/circuit/[key]/page.tsx
@@ -0,0 +1,108 @@
+'use client';
+
+import { useParams } from 'next/navigation';
+import { useEffect, useState } from 'react';
+
+import MainDetailViewCore from '@/components/explore-section/Circuit/DetailView/MainDetailViewCore';
+import { CircuitSchemaProps } from '@/components/explore-section/Circuit/type';
+import { buildCircuitMap } from '@/components/explore-section/Circuit/utils/circuits-map';
+
+export default function CircuitDetailPage() {
+  const [circuitData, setCircuitData] = useState<CircuitSchemaProps | null>(null);
+  const [parentCircuitData, setParentCircuitData] = useState<CircuitSchemaProps | null>(null);
+  const [derivedCircuitsData, setDerivedCircuitsData] = useState<CircuitSchemaProps[] | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  const params = useParams();
+  const circuitKey = params.key as string | undefined;
+
+  useEffect(() => {
+    const fetchCircuit = async () => {
+      if (!circuitKey) {
+        setError('No existing circuit');
+        setLoading(false);
+        return;
+      }
+
+      try {
+        setLoading(true);
+        const response = await fetch('/api/explore-circuits/data', {
+          cache: 'no-store',
+        });
+
+        if (!response.ok) {
+          throw new Error(`Error: ${response.statusText}`);
+        }
+
+        const data = await response.json();
+
+        if (data.error || !Array.isArray(data.circuits)) {
+          throw new Error(data.error || 'Invalid response from API');
+        }
+
+        const { circuits } = data;
+        const circuitMap = buildCircuitMap(circuits);
+
+        // Current Circuit content
+        const matchingCircuit = circuitMap.get(circuitKey);
+        if (!matchingCircuit) {
+          throw new Error(`Circuit with key ${circuitKey} not found!`);
+        }
+
+        // Parent Circuit content
+        let parentCircuit: CircuitSchemaProps | null = null;
+        if (matchingCircuit.parent) {
+          parentCircuit = circuitMap.get(matchingCircuit.parent) || null;
+        }
+
+        // Derived Circuit content
+        const derivedCircuits: CircuitSchemaProps[] = matchingCircuit.derivedFrom
+          .map((key: string) => circuitMap.get(key))
+          .filter(
+            (circuit: CircuitSchemaProps | undefined): circuit is CircuitSchemaProps => !!circuit
+          );
+
+        setCircuitData(matchingCircuit);
+        setParentCircuitData(parentCircuit);
+        setDerivedCircuitsData(derivedCircuits);
+      } catch (er) {
+        setError(
+          er instanceof Error ? er.message : 'An error occurred while fetching the circuit data'
+        );
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchCircuit();
+  }, [circuitKey]);
+
+  if (loading) {
+    return (
+      <div className="relative flex h-[50vh] w-full items-center justify-center text-lg font-normal text-primary-9">
+        Loading...
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div className="relative flex h-[50vh] w-full items-center justify-center text-lg font-normal text-red-600">
+        {error}
+      </div>
+    );
+  }
+
+  return (
+    <div className="relative flex w-full flex-col bg-white p-10">
+      {circuitData && (
+        <MainDetailViewCore
+          content={circuitData}
+          parentCircuit={parentCircuitData}
+          derivedCircuits={derivedCircuitsData}
+        />
+      )}
+    </div>
+  );
+}
