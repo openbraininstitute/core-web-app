5951646d10d924def3f1be87ecd6fe8df5a7b504
Pavlo Getta <pavlo.getta@openbraininstitute.org>
2025-05-06 14:39:53 +0200

    Fix detailed view scroll and top level layout (#318)
    * Fix detailed view scroll and top level layout
    
    * Move detailed page logic to a separate component, use in vlab explore

--- src/components/explore-section/Circuit/DetailView/MainDetailViewCore.tsx
+++ src/components/explore-section/Circuit/DetailView/MainDetailViewCore.tsx
@@ -1,9 +1,17 @@
-import { CircuitSchemaProps } from '../type';
+'use client';
+
+import { useSetAtom } from 'jotai';
+import { useParams } from 'next/navigation';
+import { useEffect, useState } from 'react';
+
 import HeaderDetailView from './HeaderDetailView';
 import SectionMainContainer from './sections/SectionMainContainer';
 import Visualiser from './visualisation/Visualiser';
+import { CircuitSchemaProps } from '@/components/explore-section/Circuit/type';
+import { buildCircuitMap } from '@/components/explore-section/Circuit/utils/circuits-map';
+import { brainRegionSidebarIsCollapsedAtom } from '@/state/brain-regions';
 
-export default function MainDetailViewCore({
+function MainDetailViewCore({
   content,
   parentCircuit,
   derivedCircuits,
@@ -13,7 +21,7 @@ export default function MainDetailViewCore({
   derivedCircuits: CircuitSchemaProps[] | null;
 }) {
   return (
-    <div className="relative flex w-full flex-col text-primary-9">
+    <div className="text-primary-9">
       <HeaderDetailView content={content} />
       <Visualiser content={content} />
       <SectionMainContainer
@@ -21,6 +29,112 @@ export default function MainDetailViewCore({
         parentCircuit={parentCircuit}
         derivedCircuits={derivedCircuits}
       />
+    </div>
+  );
+}
+
+export default function CircuitDetailPage() {
+  const setBrainRegionSidebarIsCollapsed = useSetAtom(brainRegionSidebarIsCollapsedAtom);
+  const params = useParams();
+
+  const [circuitData, setCircuitData] = useState<CircuitSchemaProps | null>(null);
+  const [parentCircuitData, setParentCircuitData] = useState<CircuitSchemaProps | null>(null);
+  const [derivedCircuitsData, setDerivedCircuitsData] = useState<CircuitSchemaProps[] | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  const circuitKey = params.id as string | undefined;
+
+  useEffect(() => {
+    const fetchCircuit = async () => {
+      if (!circuitKey) {
+        setError('No existing circuit');
+        setLoading(false);
+        return;
+      }
+
+      setBrainRegionSidebarIsCollapsed(true);
+
+      try {
+        setLoading(true);
+        const response = await fetch('/api/explore-circuits/data', {
+          cache: 'no-store',
+        });
+
+        if (!response.ok) {
+          throw new Error(`Error: ${response.statusText}`);
+        }
+
+        const data = await response.json();
+
+        if (data.error || !Array.isArray(data.circuits)) {
+          throw new Error(data.error || 'Invalid response from API');
+        }
+
+        const { circuits } = data;
+        const circuitMap = buildCircuitMap(circuits);
+
+        // Current Circuit content
+        const matchingCircuit = circuitMap.get(circuitKey);
+        if (!matchingCircuit) {
+          throw new Error(`Circuit with key ${circuitKey} not found!`);
+        }
+
+        // Parent Circuit content
+        let parentCircuit: CircuitSchemaProps | null = null;
+        if (matchingCircuit.parent) {
+          parentCircuit = circuitMap.get(matchingCircuit.parent) || null;
+        }
+
+        // Derived Circuit content
+        const derivedCircuits: CircuitSchemaProps[] = matchingCircuit.derivedFrom
+          .map((key: string) => circuitMap.get(key))
+          .filter(
+            (circuit: CircuitSchemaProps | undefined): circuit is CircuitSchemaProps => !!circuit
+          );
+
+        setCircuitData(matchingCircuit);
+        setParentCircuitData(parentCircuit);
+        setDerivedCircuitsData(derivedCircuits);
+      } catch (er) {
+        setError(
+          er instanceof Error ? er.message : 'An error occurred while fetching the circuit data'
+        );
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchCircuit();
+  }, [circuitKey]);
+
+  if (loading) {
+    return (
+      <div className="relative flex h-[50vh] w-full items-center justify-center text-lg font-normal text-primary-9">
+        Loading...
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div className="relative flex h-[50vh] w-full items-center justify-center text-lg font-normal text-red-600">
+        {error}
+      </div>
+    );
+  }
+
+  return (
+    <div className="relative overflow-y-scroll bg-white">
+      <div className="p-10">
+        {circuitData && (
+          <MainDetailViewCore
+            content={circuitData}
+            parentCircuit={parentCircuitData}
+            derivedCircuits={derivedCircuitsData}
+          />
+        )}
+      </div>
     </div>
   );
 }
