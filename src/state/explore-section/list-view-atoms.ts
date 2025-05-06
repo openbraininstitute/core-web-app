375b000033e8df7f9fc9a4d87be7048eaab6ff33
bilalesi <b_meddah@esi.dz>
2025-05-06 22:37:19 +0100

    add logic to send brain region with ancestors in list query

--- src/state/explore-section/list-view-atoms.ts
+++ src/state/explore-section/list-view-atoms.ts
@@ -25,6 +25,7 @@ import {
   setSelectedBrainRegionAtomGetter,
 } from '@/state/brain-regions';
 import {
+  buildBrainRegionFilterQuery,
   transformFiltersToQuery,
   transformQueryParamsArrayToString,
 } from '@/api/entitycore/transformers';
