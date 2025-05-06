d39c2e7ff9ae59bbbb0ac3fc93da0e4aaf14bbc9
bilalesi <b_meddah@esi.dz>
2025-05-06 22:37:19 +0100

    add logic to send brain region with ancestors in list query

---
+++ src/features/brain-region-tree/latest/side.tsx
@@ -0,0 +1,42 @@
+import { Dispatch, SetStateAction } from 'react';
+import { useAtomValue } from 'jotai';
+import { PlusOutlined } from '@ant-design/icons';
+import { Button } from 'antd';
+
+import { selectedBrainRegionAtom } from '@/state/brain-regions';
+
+export default function CollapsedBrainRegionsSidebar({
+    setIsCollapsed,
+}: {
+    setIsCollapsed: Dispatch<SetStateAction<boolean>>;
+}) {
+    const selectedBrainRegion = useAtomValue(selectedBrainRegionAtom);
+
+    return (
+        <div className="flex w-[40px] flex-col items-center pt-2">
+            <Button
+                className="mb-2"
+                type="text"
+                size="small"
+                icon={<PlusOutlined style={{ color: 'white' }} />}
+                onClick={() => setIsCollapsed(false)}
+            />
+
+            <div
+                className="flex max-h-[90vh] items-center gap-x-3.5 text-white"
+                style={{
+                    writingMode: 'vertical-rl',
+                    transform: 'rotate(180deg)',
+                    cursor: 'e-resize',
+                }}
+                role="presentation"
+                onClick={() => setIsCollapsed(false)}
+            >
+                <div className="text-secondary-4 text-sm whitespace-nowrap">
+                    {selectedBrainRegion?.title}
+                </div>
+                <div className="text-lg font-bold whitespace-nowrap">Brain region</div>
+            </div>
+        </div>
+    );
+}
