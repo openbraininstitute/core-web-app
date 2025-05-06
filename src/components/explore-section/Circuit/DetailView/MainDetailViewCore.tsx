724f85c0a53daf494e88850ccea9399b0a864ccb
Loris Olivier <53363974+loris-maru@users.noreply.github.com>
2025-05-06 16:19:14 +0200

    Ran linter and prettier

--- src/components/explore-section/Circuit/DetailView/MainDetailViewCore.tsx
+++ src/components/explore-section/Circuit/DetailView/MainDetailViewCore.tsx
@@ -4,12 +4,12 @@ import { useSetAtom } from 'jotai';
 import { useParams } from 'next/navigation';
 import { useEffect, useState } from 'react';
 
-import HeaderDetailView from './HeaderDetailView';
-import SectionMainContainer from './sections/SectionMainContainer';
-import Visualiser from './visualisation/Visualiser';
 import { CircuitSchemaProps } from '@/components/explore-section/Circuit/type';
 import { buildCircuitMap } from '@/components/explore-section/Circuit/utils/circuits-map';
 import { brainRegionSidebarIsCollapsedAtom } from '@/state/brain-regions';
+import HeaderDetailView from './HeaderDetailView';
+import SectionMainContainer from './sections/SectionMainContainer';
+import Visualiser from './visualisation/Visualiser';
 
 function MainDetailViewCore({
   content,
@@ -106,7 +106,7 @@ export default function CircuitDetailPage() {
     };
 
     fetchCircuit();
-  }, [circuitKey]);
+  }, [circuitKey, setBrainRegionSidebarIsCollapsed]);
 
   if (loading) {
     return (
