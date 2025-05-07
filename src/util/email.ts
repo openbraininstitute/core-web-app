dbd78521b27609f329f4c87bb67fabfed50e646d
Tolokoban <contact@tolokoban.org>
2025-05-07 15:08:37 +0200

    189/prevent emails not approved by insurance (#326)
    * Added check on the user profile update form
    
    * Added email verification on virtual lab creation
    
    * Fix linting issue
    
    * PR feedbacks

--- src/util/email.ts
+++ src/util/email.ts
@@ -13,3 +13,32 @@ export function isValidEMail(email: string): boolean {
   RX_EMAIL.lastIndex = -1;
   return RX_EMAIL.test(email);
 }
+
+const FORBIDDEN_COUNTRIES: Record<string, string> = {
+  af: 'Afghanistan',
+  by: 'Belarus',
+  cu: 'Cuba',
+  ir: 'Iran',
+  mm: 'Myanmar',
+  kp: 'North Korea',
+  ru: 'Russia',
+  sy: 'Syria',
+  ua: 'Ukraine',
+  ve: 'Venezuela',
+};
+
+/**
+ * Some countries are not allowed by the insurance.
+ * This function checks the last part of an email to
+ * know if it's from a forbidden country.
+ * @returns The name of the country if it's a forbidden one, or `false` otherwise.
+ */
+export function isEMailFromForbiddenCountry(email: string): false | string {
+  const countryCode = extractCountryCode(email) ?? '';
+  const forbiddenCountry: string | undefined = FORBIDDEN_COUNTRIES[countryCode];
+  return typeof forbiddenCountry === 'string' ? forbiddenCountry : false;
+}
+
+function extractCountryCode(email: string) {
+  return email.trim().split('.').pop();
+}
