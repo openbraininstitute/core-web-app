2b85e6b8270d71597d56a2cbd10a5d6491bd43e9
Loris Olivier <53363974+loris-maru@users.noreply.github.com>
2025-05-07 13:46:32 +0200

    Fixed multiples times import issue

--- src/components/explore-section/Circuit/DetailView/MainDetailViewCore.tsx
+++ src/components/explore-section/Circuit/DetailView/MainDetailViewCore.tsx
@@ -7,13 +7,6 @@ import { useEffect, useState } from 'react';
 import HeaderDetailView from './HeaderDetailView';
 import SectionMainContainer from './sections/SectionMainContainer';
 import Visualiser from './visualisation/Visualiser';
-import { CircuitSchemaProps } from '@/components/explore-section/Circuit/type';
-import { buildCircuitMap } from '@/components/explore-section/Circuit/utils/circuits-map';
-import { brainRegionSidebarIsCollapsedAtom } from '@/state/brain-regions';
-
-import { CircuitSchemaProps } from '@/components/explore-section/Circuit/type';
-import { buildCircuitMap } from '@/components/explore-section/Circuit/utils/circuits-map';
-import { brainRegionSidebarIsCollapsedAtom } from '@/state/brain-regions';
 
 import { CircuitSchemaProps } from '@/components/explore-section/Circuit/type';
 import { buildCircuitMap } from '@/components/explore-section/Circuit/utils/circuits-map';
--- src/components/explore-section/Circuit/DetailView/sections/RelatedCircuits/index.tsx
+++ src/components/explore-section/Circuit/DetailView/sections/RelatedCircuits/index.tsx
@@ -18,24 +18,20 @@ export default function RelatedCircuitsSection({
       {content.parent !== null && (
         <>
           <SubtitleBar title="Parent circuit" />
-          <CircuitTable
-            data={parentCircuit ? [parentCircuit] : []}
-            hasSearch={false}
-            downloadable={false}
-          />
+          <CircuitTable data={parentCircuit ? [parentCircuit] : []} downloadable={false} />
         </>
       )}
       {content.subcircuits.length > 0 && (
         <>
           <SubtitleBar title={content.subcircuits.length > 1 ? 'Subcircuits' : 'Subcircuit'} />
-          <CircuitTable data={content.subcircuits} hasSearch={false} downloadable={false} />
+          <CircuitTable data={content.subcircuits} downloadable={false} />
         </>
       )}
       {(derivedCircuits && derivedCircuits.length > 0) ||
         (derivedCircuits === null && (
           <>
             <SubtitleBar title="Derived from" />
-            <CircuitTable data={derivedCircuits || []} hasSearch={false} downloadable={false} />
+            <CircuitTable data={derivedCircuits || []} downloadable={false} />
           </>
         ))}
     </div>
