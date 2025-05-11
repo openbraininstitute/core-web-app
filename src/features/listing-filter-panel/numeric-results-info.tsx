69ac38b12972b017d1e37b72cb22ce0815abb750
bilalesi <b_meddah@esi.dz>
2025-05-11 14:02:53 +0200

    update result counts component

--- src/features/listing-filter-panel/numeric-results-info.tsx
+++ src/features/listing-filter-panel/numeric-results-info.tsx
@@ -1,14 +1,48 @@
 'use client';
 
-import { useBrainRegionHierarchy } from '../brain-region-tree/v2/brain-region/context';
+import { LoadingOutlined } from '@ant-design/icons';
+import { JSX, useState, useEffect } from 'react';
+
+import { useBrainRegionHierarchy } from '@/features/brain-region-tree/v2/brain-region/context';
 import { ExploreDataScope } from '@/types/explore-section/application';
 import { dataAtom } from '@/state/explore-section/list-view-atoms';
 import { DataType } from '@/constants/explore-section/list-views';
-import { useUnwrappedValue } from '@/hooks/hooks';
+import { getSectionFromDataKey } from '@/utils/key-builder';
+import { useLoadableValue } from '@/hooks/hooks';
 
+import type { EntityCoreResponse } from '@/api/entitycore/types/shared/response';
 import type { WorkspaceContext } from '@/types/common';
 
-function NumericResultsInfo({
+const generateDisplayComponent = (
+  data: EntityCoreResponse<any> | undefined | null
+): JSX.Element => {
+  if (!data || !data.pagination || !data.data) {
+    return <strong>0</strong>;
+  }
+
+  const { pagination, data: resultDataItems } = data;
+  let currentCount =
+    pagination.page && pagination.page_size
+      ? (pagination.page - 1) * pagination.page_size + resultDataItems.length
+      : resultDataItems.length;
+  const totalCount = pagination.total_items || 0;
+
+  currentCount = totalCount > 0 ? currentCount : 0;
+
+  const currentFormatted = currentCount.toLocaleString('en-US');
+  const totalFormatted = totalCount.toLocaleString('en-US');
+  const showFormattedResults = currentCount > 0 && totalCount > 0;
+
+  return showFormattedResults ? (
+    <span>
+      <strong>{currentFormatted}</strong> of <strong>{totalFormatted}</strong>
+    </span>
+  ) : (
+    <strong>0</strong>
+  );
+};
+
+function ResultsCount({
   dataType,
   dataScope,
   virtualLabInfo,
@@ -19,8 +53,9 @@ function NumericResultsInfo({
   virtualLabInfo?: WorkspaceContext;
   dataKey: string;
 }) {
-  const { node } = useBrainRegionHierarchy({ dataKey: dataKey });
-  const res = useUnwrappedValue(
+  const [persistedDisplay, setPersistedDisplay] = useState<JSX.Element | null>(null);
+  const { node } = useBrainRegionHierarchy({ dataKey: getSectionFromDataKey(dataKey) });
+  const result = useLoadableValue(
     dataAtom({
       dataType,
       dataScope,
@@ -29,13 +64,29 @@ function NumericResultsInfo({
       brainRegionId: node.id,
     })
   );
-  let current = res ? (res.pagination.page - 1) * res.pagination.page_size + res.data.length : 0;
 
-  const total = res ? res.pagination.total_items : 0;
-  current = total > 0 ? current : 0;
-  const currentFormatted = current.toLocaleString('en-US');
-  const totalFormatted = total.toLocaleString('en-US');
-  const showFormattedResults = Boolean(current) && Boolean(total);
+  const hasData = result.state === 'hasData' && result.data;
+  let content: JSX.Element | null = null;
+
+  if (result.state === 'loading') {
+    if (persistedDisplay) {
+      content = <span className="opacity-45 backdrop-blur-sm">{persistedDisplay}</span>;
+    } else {
+      content = <LoadingOutlined />;
+    }
+  } else if (result.state === 'hasData' && result.data) {
+    content = generateDisplayComponent(result.data);
+  } else if (result.state === 'hasError') {
+    content = null;
+  } else {
+    content = null;
+  }
+
+  useEffect(() => {
+    if (hasData) {
+      setPersistedDisplay(generateDisplayComponent(result.data));
+    }
+  }, [hasData]);
 
   return (
     <div className="flex w-full justify-start">
@@ -45,16 +96,10 @@ function NumericResultsInfo({
         aria-label="listing-view-title"
       >
         <span>Results </span>
-        {res && showFormattedResults ? (
-          <strong>
-            {currentFormatted}/{totalFormatted}
-          </strong>
-        ) : (
-          <strong>0</strong>
-        )}
+        {content}
       </div>
     </div>
   );
 }
 
-export default NumericResultsInfo;
+export default ResultsCount;
