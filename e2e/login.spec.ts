d335a7e06dabdde2615c9a363781a465a9f28bc8
bilalesi <b_meddah@esi.dz>
2025-05-11 10:34:58 +0200

    fix auth e2e tests

--- e2e/login.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from '@playwright/test';
-import { completeLogin } from './utils/auth';
-
-test('Login to open brain institute', async ({ page }) => {
-  test.slow();
-  await page.goto('http://localhost:3000/app/virtual-lab');
-  await completeLogin(page);
-  await expect(page).toHaveURL(/app\/virtual-lab/);
-});
