d205a6bb4bd3b7cd7c51a71aa82c3ee13b3a302d
Pavlo Getta <pavlo.getta@openbraininstitute.org>
2025-05-12 16:02:54 +0200

    Add measurement annotation types

---
+++ src/api/entitycore/types/entities/measurement-annotation.ts
@@ -0,0 +1,30 @@
+import { Timestamps, EntityCoreIdentifiable } from '@/api/entitycore/types/shared/global';
+import { TimestampsFilter, PaginationFilter } from '@/api/entitycore/types/shared/request';
+import { EntityTypeValue } from '@/api/entitycore/types/entity-type';
+
+export type MeasurementItem = {
+  name: string;
+  unit: string;
+  value: number | string;
+};
+
+export type MeasurementKind = {
+  structural_domain: string;
+  pref_label: string;
+  measurement_items: MeasurementItem[];
+};
+
+export interface MeasurementAnnotation extends EntityCoreIdentifiable, Timestamps {
+  entity_id: string;
+  entity_type: EntityTypeValue;
+  measurement_kinds: MeasurementKind[];
+}
+
+// TODO: Extend with filters implemented in EntityCore
+export type MeasurementAnnotationFilter = Partial<
+  TimestampsFilter &
+    PaginationFilter & {
+      entity_id?: string;
+      entity_type?: EntityTypeValue;
+    }
+>;
