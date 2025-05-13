8d842f0451cb93f5763cb14a68d53d297091aa21
bilalesi <b_meddah@esi.dz>
2025-05-13 11:32:46 +0200

    add new synapse-per-connection filters

--- src/api/entitycore/types/entities/synapses-per-connection.ts
+++ src/api/entitycore/types/entities/synapses-per-connection.ts
@@ -1,11 +1,7 @@
-import {
-  Timestamps,
-  IBrainRegion,
-  EntityCoreIdentifiable,
-  IMType,
-  EntityCoreType,
-} from '@/api/entitycore/types/shared/global';
-import {
+import type { IBrainRegionHierarchy } from '@/api/entitycore/types/entities/brain-region';
+import type { IMType, EntityCoreType } from '@/api/entitycore/types/shared/global';
+import type { IExperimentalDensity } from '@/api/entitycore/types/shared/density';
+import type {
   TimestampsFilter,
   PaginationFilter,
   SharedFilter,
@@ -15,19 +11,50 @@ import {
   EtypeFilter,
   StainFilter,
 } from '@/api/entitycore/types/shared/request';
-import { IExperimentalDensity } from '@/api/entitycore/types/shared/density';
-
-interface SynapticPathway extends EntityCoreIdentifiable, Timestamps {
-  pre_mtype: IMType;
-  post_mtype: IMType;
-  pre_region: IBrainRegion;
-  post_region: IBrainRegion;
-}
 
 export interface IExperimentalSynapsesPerConnection extends IExperimentalDensity, EntityCoreType {
   mtypes: Array<IMType> | null;
-  synaptic_pathway: SynapticPathway;
+  pre_mtype: IMType;
+  post_mtype: IMType;
+  pre_region: IBrainRegionHierarchy;
+  post_region: IBrainRegionHierarchy;
 }
+
+type PreRegionFilter = {
+  pre_region__name?: string | null;
+  pre_region__name__in?: string[] | null;
+  pre_region__name__ilike?: string | null;
+  pre_region__id?: number | null;
+  pre_region__id_in?: number[] | null;
+  pre_region__acronym?: string | null;
+  pre_region__acronym__in?: string[] | null;
+  pre_region__order_by?: string | null;
+};
+
+type PostRegionFilter = {
+  post_region__name?: string | null;
+  post_region__name_in?: string[] | null;
+  post_region__name__ilike?: string | null;
+  post_region__id?: number | null;
+  post_region__id_in?: number[] | null;
+  post_region__acronym?: string | null;
+  post_region__acronym__in?: string[] | null;
+  post_region__order_by?: string | null;
+};
+
+type PreMtypeFilter = {
+  pre_mtype__id?: string | null;
+  pre_mtype__pref_label?: string | null;
+  pre_mtype__pref_label__in?: string[] | null;
+  pre_mtype__order_by?: string | null;
+};
+
+type PostMtypeFilter = {
+  post_mtype__id?: string | null;
+  post_mtype__pref_label?: string | null;
+  post_mtype__pref_label__in?: string[] | null;
+  post_mtype__order_by?: string | null;
+};
 
 export type ExperimentalSynapsesPerConnectionFilter = Partial<
   SharedFilter &
@@ -37,5 +64,9 @@ export type ExperimentalSynapsesPerConnectionFilter = Partial<
     BrainRegionFilter &
     SpeciesFilter &
     StainFilter &
-    EtypeFilter
+    EtypeFilter &
+    PreMtypeFilter &
+    PostMtypeFilter &
+    PreRegionFilter &
+    PostRegionFilter
 >;
