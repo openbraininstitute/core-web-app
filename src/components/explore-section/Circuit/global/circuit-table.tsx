29db16a9311d6138aabbcd0b25914ca5992fab3f
Loris Olivier <53363974+loris-maru@users.noreply.github.com>
2025-05-15 17:13:21 +0200

    Manually fixed linter and prettier

--- src/components/explore-section/Circuit/global/circuit-table.tsx
+++ src/components/explore-section/Circuit/global/circuit-table.tsx
@@ -65,8 +65,7 @@ export default function CircuitTable({
   // DATA
   const cleanedData = useMemo(() => {
     const result = data.map((circuit: CircuitSchemaProps) => {
-      const numberOfNeurons = circuit.numberOfNeurons;
-
+      const { numberOfNeurons } = circuit;
       if (numberOfNeurons === 0) {
         throw new Error('Number of neurons is not a number or is NaN');
       }
