7b1329ed06b93058822679f01fb24f7b4b49a900
bilalesi <b_meddah@esi.dz>
2025-05-15 15:48:15 +0200

    limit the number of entries to store in indexdb, less then the default will be deleted

--- src/components/explore-section/Literature/user-journey/index.ts
+++ src/components/explore-section/Literature/user-journey/index.ts
@@ -1,5 +1,17 @@
+import isNil from 'lodash/isNil';
+
 import { db, ClickTuple, ClickType } from '@/components/explore-section/Literature/user-journey/db';
 import GenericEvent from '@/util/generic-event';
+
+const DEFAULT_COUNT = 10;
+
+function sortTuples(tuples: ClickTuple[]) {
+  return tuples.sort((a, b) => {
+    const aTimestamp = a.clicks.find((c) => c.type === 'brain_region')?.timestamp || 0;
+    const bTimestamp = b.clicks.find((c) => c.type === 'brain_region')?.timestamp || 0;
+    return bTimestamp - aTimestamp;
+  });
+}
 
 class ClickContextTracker {
   public readonly eventChange = new GenericEvent<ClickContextTracker>();
@@ -13,10 +25,23 @@ class ClickContextTracker {
 
     try {
       await this.dbInstance.clickTuples.add(currentTuple);
+      const allTuplesFromDB = await this.dbInstance.clickTuples.toArray();
+      const sortedAllTuples = sortTuples([...allTuplesFromDB]);
+      if (sortedAllTuples.length > DEFAULT_COUNT) {
+        const tuplesToDelete = sortedAllTuples.slice(DEFAULT_COUNT);
+        const idsToDelete = tuplesToDelete
+          .map((tuple) => tuple.id)
+          .filter((id): id is number => typeof id === 'number' && !isNil(id));
+
+        if (idsToDelete.length > 0) {
+          await this.dbInstance.clickTuples.bulkDelete(idsToDelete);
+        }
+      }
+
       await this.dbInstance.activeSession.delete('current');
     } catch (error) {
       // eslint-disable-next-line no-console
-      console.error('Error in save use journey tuple:', error);
+      console.error('Error in save user journey tuple:', error);
     }
   }
 
@@ -53,19 +78,19 @@ class ClickContextTracker {
     await this.updateCurrentTuple(currentTuple);
   }
 
-  async getLastTuples(count: number = 10): Promise<Array<Array<[ClickType, string]>>> {
+  async getLastTuples(count: number = DEFAULT_COUNT): Promise<Array<Array<[ClickType, string]>>> {
     const tuples = await this.dbInstance.clickTuples.orderBy('id').toArray();
     const currentActiveTuple = await this.getCurrentTuple();
-    const sortedClicks = [...tuples, ...(currentActiveTuple ? [currentActiveTuple] : [])]
-      .sort((a, b) => {
-        const aTimestamp = a.clicks.find((c) => c.type === 'brain_region')?.timestamp || 0;
-        const bTimestamp = b.clicks.find((c) => c.type === 'brain_region')?.timestamp || 0;
-        return bTimestamp - aTimestamp;
-      })
+    const sortedClicks = sortTuples([
+      ...tuples,
+      ...(currentActiveTuple ? [currentActiveTuple] : []),
+    ])
       .slice(0, count)
       .reverse();
 
-    return sortedClicks.map((tuple) => tuple.clicks.map((click) => [click.type, click.data]));
+    return sortedClicks.map((tuple: ClickTuple) =>
+      tuple.clicks.map((click: ClickTuple['clicks'][number]) => [click.type, click.data])
+    );
   }
 }
 
