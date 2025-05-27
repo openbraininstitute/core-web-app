72f901cdf4ebbbeec113ca3c691931fe53156a72
Loris Olivier <53363974+loris-maru@users.noreply.github.com>
2025-05-27 11:56:44 +0200

    Init

--- src/components/explore-section/Circuit/DetailView/main-detail-view-core.tsx
+++ src/components/explore-section/Circuit/DetailView/main-detail-view-core.tsx
@@ -13,10 +13,6 @@ import { CircuitSchemaProps } from '@/components/explore-section/Circuit/type';
 import { buildCircuitMap } from '@/components/explore-section/Circuit/utils/circuits-map';
 import { brainRegionSidebarIsCollapsedAtom } from '@/state/brain-regions';
 
-import { CircuitSchemaProps } from '@/components/explore-section/Circuit/type';
-import { buildCircuitMap } from '@/components/explore-section/Circuit/utils/circuits-map';
-import { brainRegionSidebarIsCollapsedAtom } from '@/state/brain-regions';
-
 function MainDetailViewCore({
   content,
   parentCircuit,
