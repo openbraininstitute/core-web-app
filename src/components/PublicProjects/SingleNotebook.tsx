d41a1d1f023c260f31d8fe2cabeb628e76694fc7
Loris Olivier <olivier.loris@openbraininstitute.org>
2025-06-03 09:27:00 +0200

    Public project notebook update complete metadata (#405)
    * Removed the component of search bar and filter options
    
    * Added object of interest, scale and authors in addition to the readme in a dropdown
    
    * Refine H1, H2 and H3 of the readMe
    
    * Corrected import order
    
    ---------
    
    Co-authored-by: Loris Olivier <53363974+loris-maru@users.noreply.github.com>

--- src/components/PublicProjects/SingleNotebook.tsx
+++ src/components/PublicProjects/SingleNotebook.tsx
@@ -1,6 +1,12 @@
+'use client';
+
+import { useState } from 'react';
+
 import { PortableText } from 'next-sanity';
+import { ChevronRight, ReadMeIcon } from '../icons';
 import { NotebooksProps } from './type';
 
+import { classNames } from '@/util/utils';
 import styles from './style/notebook.module.css';
 
 export default function SingleNotebook({
@@ -10,34 +16,81 @@ export default function SingleNotebook({
   content: NotebooksProps;
   index: number;
 }) {
+  const [readMeOpen, setReadMeOpen] = useState(false);
+
   return (
-    <>
-      {index !== 0 && <div className="my-1 h-px w-full bg-neutral-2" />}
-      <div className="relative flex w-full flex-col items-start">
-        <header className="relative flex w-full flex-row items-center justify-between text-primary-9">
-          <div className="flex w-full flex-col">
-            <div className="text-sm font-normal">{index + 1}</div>
-            <div className="w-2/3 hyphens-auto text-3xl font-bold leading-[1.3]">
-              {content.name}
-            </div>
+    <div className="relative flex w-full flex-col items-start">
+      <header className="relative flex w-full flex-row items-center justify-between text-primary-9">
+        <div className="flex w-full flex-col">
+          <div className="after:contents[''] flex flex-row items-center text-base font-normal after:ml-2 after:block after:h-px after:w-12 after:bg-primary-9">
+            {index + 1 < 10 ? '0' : ''}
+            {index + 1}
           </div>
-          {content.url && (
-            <a
-              href={content.url}
-              className="whitespace-nowrap border border-solid border-primary-9 bg-white px-6 py-3 text-lg transition-colors duration-300 hover:bg-primary-9 hover:text-white"
-              target="_blank"
-            >
-              View Notebook
-            </a>
-          )}
-        </header>
+          <div className="w-full hyphens-auto text-3xl font-bold leading-[1.3]">{content.name}</div>
+        </div>
+        {content.url && (
+          <a
+            href={content.url}
+            className="whitespace-nowrap border border-solid border-primary-9 bg-white px-6 py-3 text-lg transition-colors duration-300 hover:bg-primary-9 hover:text-white"
+            target="_blank"
+          >
+            View Notebook
+          </a>
+        )}
+      </header>
 
-        <div className="my-3 h-px w-12 bg-neutral-3" />
+      <div className="my-3 h-px w-full bg-gray-200" />
 
-        <div className={styles.readMeParagraph}>
-          <PortableText value={content.readMe} />
+      <div className="flex flex-row gap-x-16">
+        <div>
+          <div className="text-sm uppercase tracking-wider text-gray-400">Scale</div>
+          <div className="text-lg">{content.scale}</div>
+        </div>
+        <div>
+          <div className="text-sm uppercase tracking-wider text-gray-400">Object of interest</div>
+          <div className="text-lg">{content.objectOfInterest}</div>
+        </div>
+        <div>
+          <div className="text-sm uppercase tracking-wider text-gray-400">Author(s)</div>
+          <div className="text-lg">{content.authors}</div>
         </div>
       </div>
-    </>
+      <div className="my-3 h-px w-full bg-gray-200" />
+      <div className="w-full">
+        <div>
+          <div className="mt-2 text-sm font-normal uppercase tracking-wider text-gray-400">
+            Description
+          </div>
+          <div className="text-lg font-normal leading-normal">{content.description}</div>
+        </div>
+        <div className="relative w-full">
+          <button
+            type="button"
+            aria-label="Open read-me block"
+            onClick={() => setReadMeOpen(!readMeOpen)}
+            className="mt-6 flex w-full flex-row justify-between border border-solid border-gray-200 px-4 py-2"
+          >
+            <div className="flex flex-row items-center text-lg font-normal text-primary-9">
+              <ReadMeIcon iconColor="#A6A6A6" className="mr-2 h-4 w-auto" />
+              Read me
+            </div>
+            <div>
+              <ChevronRight
+                fill="#002766"
+                className={classNames(
+                  'relative top-2 transition-transform duration-300 ease-in-out',
+                  readMeOpen ? 'rotate-90' : 'rotate-0'
+                )}
+              />
+            </div>
+          </button>
+          {readMeOpen && (
+            <div className={styles.readMeParagraph}>
+              <PortableText value={content.readMe} />
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
   );
 }
--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -34,6 +34,7 @@ import { MembersGroupIcon } from './MembersGroupIcon';
 import MissingData from './MissingData';
 import IconPlus from './Plus';
 import RangeIcon from './Range';
+import ReadMeIcon from './ReadMeIcon';
 import ResetIcon from './ResetIcon';
 import SettingsIcon from './Settings';
 import SimulateIcon from './SimulateIcon';
@@ -81,6 +82,7 @@ export {
   MissingData,
   IconPlus as PlusIcon,
   RangeIcon,
+  ReadMeIcon,
   ResetIcon,
   SettingsIcon,
   SimulateIcon,
