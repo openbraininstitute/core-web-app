bfb2d30abdd044ffbb27510b169575666fe4e461
Loris Olivier <53363974+loris-maru@users.noreply.github.com>
2025-06-03 09:12:45 +0200

    Added documentation page

---
+++ src/app/app/virtual-lab/(home)/documentation/page.tsx
@@ -0,0 +1,7 @@
+export default function DocumentationPage() {
+  return (
+    <div className="relative h-full w-full">
+      <h1 className="text-3xl font-bold text-white">Documentation</h1>
+    </div>
+  );
+}
--- src/components/VirtualLab/side-bar/control.tsx
+++ src/components/VirtualLab/side-bar/control.tsx
@@ -1,15 +1,27 @@
 'use client';
 
-import Link from 'next/link';
 import { HomeOutlined, QuestionCircleOutlined, UserOutlined } from '@ant-design/icons';
+import Link from 'next/link';
 
-import { classNames } from '@/util/utils';
-import UserMenu from '@/components/user-menu';
 import HelpMenu from '@/components/HelpMenu';
+import { DocumentIcon } from '@/components/icons';
+import UserMenu from '@/components/user-menu';
+import { classNames } from '@/util/utils';
 
 export default function Profile() {
   return (
     <div className="mt-auto flex flex-col gap-4">
+      <Link
+        href="/app/virtual-lab/documentation"
+        aria-label="documentation"
+        className={classNames(
+          'flex h-12 w-12 items-center justify-center p-3',
+          'text-white transition-all duration-200',
+          'border border-primary-6 hover:border-primary-5 hover:bg-primary-5'
+        )}
+      >
+        <DocumentIcon iconColor="white" className="h-auto w-4" />
+      </Link>
       <HelpMenu
         cls={{
           trigger: '!p-0',
