3bec7bab63ff78e0c6ffa6d12ba63d3f7cdc3001
Loris Olivier <53363974+loris-maru@users.noreply.github.com>
2025-06-05 15:22:45 +0200

    Added link to home, user account and information

--- src/app/app/documentation/layout.tsx
+++ src/app/app/documentation/layout.tsx
@@ -1,9 +1,16 @@
+'use client';
+
+import GlobalSideMenu from '@/components/documentation/global/global-side-menu';
 import DocumentationSideBloc from '@/components/documentation/global/side-bloc';
 
 export default function DocumentationLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="relative min-h-screen w-screen bg-primary-9 p-8">
-      <DocumentationSideBloc />
+      <div className="fixed left-0 top-0 flex h-screen w-[255px] flex-col justify-between p-8 ">
+        <DocumentationSideBloc />
+        <GlobalSideMenu />
+      </div>
+
       <main className="ml-[255px] w-2/3">{children}</main>
     </div>
   );
