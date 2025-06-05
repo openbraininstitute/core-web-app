6a7eb619ffb7b41acc0484866a1ab19a8a4e2a23
Loris Olivier <olivier.loris@openbraininstitute.org>
2025-06-05 16:50:47 +0200

    Add global side navigation in documentation (#415)
    * Removed the component of search bar and filter options
    
    * Added link to home, user account and information
    
    * Moved the import to fix the linter error
    
    ---------
    
    Co-authored-by: Loris Olivier <53363974+loris-maru@users.noreply.github.com>

--- src/components/documentation/global/side-bloc.tsx
+++ src/components/documentation/global/side-bloc.tsx
@@ -2,11 +2,12 @@ import DOCUMENTATION_ARCHITECTURE from '../CONTENT/ARCHITECTURE';
 import { SingleSectionProps } from '../type';
 import NavItem from './nav-item';
 import TutorialNavList from './tutorial-nav-list';
+
 import LogoAsLink from '@/components/logo/as-link';
 
 export default function DocumentationSideBloc() {
   return (
-    <nav className="fixed left-8 top-8 w-[235px] text-white">
+    <nav className="w-full text-white">
       <LogoAsLink />
       <div className="relative mt-10 flex w-full flex-col gap-y-3">
         {DOCUMENTATION_ARCHITECTURE.map((item: SingleSectionProps) => {
