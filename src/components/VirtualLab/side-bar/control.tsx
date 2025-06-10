b7752e9e63a83c6079a42cf54e2aec141850ea8d
Loris Olivier <olivier.loris@openbraininstitute.org>
2025-06-10 17:48:19 +0200

    Global side button home button (#423)

--- src/components/VirtualLab/side-bar/control.tsx
+++ src/components/VirtualLab/side-bar/control.tsx
@@ -2,6 +2,7 @@
 
 import { HomeOutlined, QuestionCircleOutlined, UserOutlined } from '@ant-design/icons';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 import HelpMenu from '@/components/HelpMenu';
 import { DocumentationIcon } from '@/components/icons';
@@ -9,8 +10,24 @@ import UserMenu from '@/components/user-menu';
 import { classNames } from '@/util/utils';
 
 export default function Profile() {
+  const pathName = usePathname();
+
   return (
     <div className="mt-auto flex flex-col gap-4">
+      {pathName !== '/app/virtual-lab' && (
+        <Link
+          href="/app/virtual-lab"
+          aria-label="home"
+          type="button"
+          className={classNames(
+            'flex w-max items-center justify-center p-3',
+            'text-white transition-all duration-200',
+            'border border-primary-6 hover:border-primary-5 hover:bg-primary-5'
+          )}
+        >
+          <HomeOutlined className="text-xl" />
+        </Link>
+      )}
       <Link
         href="/app/documentation"
         aria-label="documentation"
@@ -37,18 +54,6 @@ export default function Profile() {
           <QuestionCircleOutlined className="text-xl" />
         </div>
       </HelpMenu>
-      <Link
-        href="/app/virtual-lab"
-        aria-label="home"
-        type="button"
-        className={classNames(
-          'flex w-max items-center justify-center p-3',
-          'text-white transition-all duration-200',
-          'border border-primary-6 hover:border-primary-5 hover:bg-primary-5'
-        )}
-      >
-        <HomeOutlined className="text-xl" />
-      </Link>
       <UserMenu
         cls={{
           trigger: '!p-0',
