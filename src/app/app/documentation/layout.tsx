5163d800651402b78f0b29073e54c4ad3c4821a8
Dries Verachtert <dries.verachtert@dries.eu>
2025-06-13 17:35:35 +0200

    Apply all changes proposed by prettier

--- src/app/app/documentation/layout.tsx
+++ src/app/app/documentation/layout.tsx
@@ -8,11 +8,11 @@ import styles from './styles.module.css';
 
 export default function DocumentationLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="relative min-h-screen w-screen bg-primary-9">
+    <div className="bg-primary-9 relative min-h-screen w-screen">
       <SideBarNavigation />
       <div
         className={classNames(
-          'fixed left-[40px] top-0 flex h-screen w-[255px] flex-col justify-between overflow-y-scroll p-8',
+          'fixed top-0 left-[40px] flex h-screen w-[255px] flex-col justify-between overflow-y-scroll p-8',
           styles['hide-scrollbar']
         )}
       >
