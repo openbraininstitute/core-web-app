a0ebae8794b15dfd671501209d0ddc87c4681240
Bilal MEDDAH <b_meddah@esi.dz>
2025-06-19 12:18:14 +0200

    Fix/function passing 1 (#464)
    * fix the function passing to component

--- src/app/app/virtual-lab/(free)/explore/(interactive)/interactive/(data)/experimental/[type]/Page.1.tsx
+++ src/app/app/virtual-lab/(free)/explore/(interactive)/interactive/(data)/experimental/[type]/Page.1.tsx
@@ -1,22 +1,12 @@
-import { notFound } from 'next/navigation';
-import omit from 'lodash/omit';
-
 import { getEntityBySlug } from '@/entity-configuration/domain/helpers';
 import ListingView from '@/features/views/listing';
+import { notFound } from 'next/navigation';
+import { Props } from './page';
 
-import type { ServerSideComponentProp, WorkspaceContext } from '@/types/common';
-import type { ExperimentalEntitySlugValue } from '@/entity-configuration/domain/slug';
-
-export default async function Page({
-  params: promisedParams,
-}: ServerSideComponentProp<
-  WorkspaceContext & {
-    type: ExperimentalEntitySlugValue;
-  },
-  null
->) {
+export default async function Page({ params: promisedParams }: Props) {
   const params = await promisedParams;
   const entity = getEntityBySlug({ slug: params.type });
+
   if (!entity) {
     notFound();
   }
