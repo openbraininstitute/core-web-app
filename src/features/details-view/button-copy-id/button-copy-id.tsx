35e2633f721b132c01925ba2d5a51a4737782e39
Tolokoban <contact@tolokoban.org>
2025-06-20 17:53:16 +0200

    Add Copy ID button (#472)

---
+++ src/features/details-view/button-copy-id/button-copy-id.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Button } from 'antd';
+import { CopyOutlined } from '@ant-design/icons';
+
+import { classNames } from '@/util/utils';
+
+import styles from './button-copy-id.module.css';
+
+export interface ButtonCopyIdProps {
+  className?: string;
+  value: string;
+  tooltip?: string;
+}
+
+export function ButtonCopyId({ className, value, tooltip = 'Database ID' }: ButtonCopyIdProps) {
+  const [copied, setCopied] = React.useState(false);
+  const handleClick = () => {
+    setCopied(true);
+    navigator.clipboard.writeText(value);
+    window.setTimeout(() => setCopied(false), 5000);
+  };
+
+  return (
+    <div className={classNames(styles.buttonCopyId, 'text-primary-7', copied && styles.copied)}>
+      <Button
+        type="text"
+        className={classNames(
+          className,
+          styles.actualButton,
+          'text-primary-7 flex items-center gap-2 hover:bg-transparent!'
+        )}
+        onClick={handleClick}
+      >
+        Copy ID
+        <CopyOutlined className="border-neutral-2 border px-4 py-3" />
+      </Button>
+      <div className={styles.tooltip}>{tooltip}</div>
+      <HasBeenCopied />
+    </div>
+  );
+}
+
+function HasBeenCopied() {
+  return (
+    <div className={styles.copiedLabel}>
+      <div>Copied</div>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="1em" height="1em">
+        <title>check</title>
+        <path d="M21,7L9,19L3.5,13.5L4.91,12.09L9,16.17L19.59,5.59L21,7Z" fill="#3e0" />
+      </svg>
+    </div>
+  );
+}
--- src/features/details-view/header.tsx
+++ src/features/details-view/header.tsx
@@ -4,15 +4,15 @@ import { ReactNode } from 'react';
 import { Button } from 'antd';
 import { useAtomValue } from 'jotai';
 
+import { ButtonCopyId } from './button-copy-id';
+
 import { getEntityBySlug } from '@/entity-configuration/domain/helpers';
 import BookmarkButton from '@/features/bookmark/control';
 import usePathname from '@/hooks/pathname';
 import sessionAtom from '@/state/session';
 import Link from '@/components/Link';
-
 import { InteractiveViewIcon } from '@/components/icons';
 import { ensureArray } from '@/utils/array';
-
 import type { EntityCoreIdentifiableNamed } from '@/api/entitycore/types/shared/global';
 import type { EntitySlugValue } from '@/entity-configuration/domain/slug';
 
@@ -30,10 +30,11 @@ export default function Header<T extends EntityCoreIdentifiableNamed>({
   const isSimCampDetail = simCampMatch && path === simCampMatch[0];
   const session = useAtomValue(sessionAtom);
 
-  const { virtualLabId, projectId, type } = useParams<{
+  const { virtualLabId, projectId, type, id } = useParams<{
     virtualLabId?: string;
     projectId?: string;
     type: EntitySlugValue;
+    id: string;
   }>();
 
   const entity = getEntityBySlug({ slug: type });
@@ -41,12 +42,13 @@ export default function Header<T extends EntityCoreIdentifiableNamed>({
   return (
     <div className="text-primary-7 flex flex-col">
       <div className="text font-thin">Name</div>
-      <div className="flex justify-between">
+      <div className="flex items-start justify-between">
         <div className="grid grid-cols-6 items-center gap-5">
           <div className="col-span-3 text-2xl font-bold">{detail?.name}</div>
         </div>
         {session && (
-          <div className="flex items-center gap-2">
+          <div className="flex flex-wrap items-center justify-end gap-2">
+            <ButtonCopyId value={id} />
             {extraHeaderAction}
             {virtualLabId && projectId && entity?.isBookmarkable && (
               <BookmarkButton
