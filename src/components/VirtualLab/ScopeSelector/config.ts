dbc564810b5a1a6e0a96a4c2268cee8f91ec1082
Tolokoban <contact@tolokoban.org>
2025-06-26 08:33:05 +0200

    Add all images as webp to match the ones on the landing page

--- src/components/VirtualLab/ScopeSelector/config.ts
+++ src/components/VirtualLab/ScopeSelector/config.ts
@@ -42,7 +42,12 @@ export type TTileConfig = {
   } | null;
 };
 
-function makeTile(type: ModelTileType, title: string, description = 'Coming soon.'): TTileConfig {
+function makeTile(
+  type: ModelTileType,
+  title: string,
+  description = 'Coming soon.',
+  more: Partial<TTileConfig> = {}
+): TTileConfig {
   return {
     id: type,
     title,
@@ -51,6 +56,7 @@ function makeTile(type: ModelTileType, title: string, description = 'Coming soon
     img: imageUrl(type),
     disabled: true,
     url: null,
+    ...more,
   };
 }
 
@@ -62,7 +68,7 @@ export const ModelTilesConfig: Array<TTileConfig> = [
     type: ModelTileType.SingleNeuron,
     description:
       'Load Hodgkin-Huxley single cell models, perform current clamp experiments with different levels of input current, and observe the resulting changes in membrane potential.',
-    img: imageUrl('singleNeuron'),
+    img: imageUrl('single-neuron'),
     disabled: false,
     url: {
       build: 'build/me-model/new',
@@ -73,21 +79,41 @@ export const ModelTilesConfig: Array<TTileConfig> = [
       simulate: EntityCoreConfiguration.SingleNeuronSimulation,
     },
   },
-  makeTile(ModelTileType.SmallMicrocircuit, 'Small microcircuit'),
+  makeTile(
+    ModelTileType.SmallMicrocircuit,
+    'Small microcircuit',
+    'Circuit with 3-20 neurons together with synapses coming from inside and outside its volume (usually called intrinsic and extrinsic synapses respectively).',
+    {
+      disabled: false,
+      url: null,
+      // entities: {
+      //   build: EntityCoreConfiguration.Circuit,
+      //   simulate: EntityCoreConfiguration.Circuit,
+      // },
+    }
+  ),
   makeTile(ModelTileType.BrainRegion, 'Brain Region'),
   makeTile(ModelTileType.Metabolism, 'Metabolism'),
   makeTile(
     ModelTileType.Synaptome,
     'Synaptome',
-    'Introduce spikes into the synapses of Hodgkin-Huxley cell models and carry out a virtual experiment by setting up a stimulation and reporting protocol.'
+    'Introduce spikes into the synapses of Hodgkin-Huxley cell models and carry out a virtual experiment by setting up a stimulation and reporting protocol.',
+    {
+      disabled: false,
+      url: {
+        build: 'build/synaptome/new',
+        explore: 'explore/interactive/model/synaptome',
+      },
+      entities: {
+        build: EntityCoreConfiguration.SingleNeuronSynaptome,
+        simulate: EntityCoreConfiguration.SingleNeuronSynaptomeSimulation,
+      },
+    }
   ),
   makeTile(ModelTileType.Microcircuit, 'Microcircuit'),
   makeTile(ModelTileType.BrainSystem, 'Brain system'),
   makeTile(ModelTileType.NeuroGliaVasculatureUnit, 'NGV unit'),
-  makeTile(
-    ModelTileType.PairedNeuron,
-    'Retrieve interconnected Hodgkin-Huxley cell models from a circuit and conduct a simulated experiment by establishing a stimulation and reporting protocol.'
-  ),
+  makeTile(ModelTileType.PairedNeuron, 'Paired neurons'),
   makeTile(ModelTileType.NeuroGliaVasculatureCircuit, 'NGV circuit'),
   makeTile(ModelTileType.WholeBrain, 'Whole brain'),
 ];
