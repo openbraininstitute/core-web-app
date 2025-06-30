a17a36d4d7b1beb730a5263e66ed1d14172abcac
g-bar <gilarturo.barriosdelvillar@openbraininstitute.org>
2025-06-30 19:47:59 +0200

    Types (#539)
    * fix one error
    
    * prettier
    
    * lint

--- src/app/app/virtual-lab/lab/[virtualLabId]/(project)/project/[projectId]/(secondary)/simulate/small-microcircuit/new/[circuit_id]/_components/components.tsx
+++ src/app/app/virtual-lab/lab/[virtualLabId]/(project)/project/[projectId]/(secondary)/simulate/small-microcircuit/new/[circuit_id]/_components/components.tsx
@@ -4,17 +4,12 @@ import { InputNumber, Input, Select } from 'antd';
 import { CheckCircleOutlined, CloseCircleOutlined, PlusCircleOutlined } from '@ant-design/icons';
 import { JSONSchema } from '../types';
 
-import { isPlainObject } from './utils';
+import { isPlainObject, Primitive, ConfigObject } from './utils';
 import { classNames } from '@/util/utils';
 
-type Primitive = null | boolean | number | string;
-export interface Object {
-  [key: string]: Primitive | Primitive[] | Object;
-}
+export type ConfigValue = Primitive | Primitive[] | ConfigObject;
 
-export type ConfigValue = Primitive | Primitive[] | Object;
-
-export type Config = Record<string, Object | string>;
+export type Config = Record<string, ConfigObject | string>;
 
 export function JSONSchemaForm({
   disabled,
