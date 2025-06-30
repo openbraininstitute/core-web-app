6b523ff61cff81ae202bfa0cc7b64dae99ca8f38
Gil Arturo Barrios del Villar <gilarturo.barriosdelvillar@openbraininstitute.org>
2025-07-01 01:28:34 +0200

    more refactoring

--- src/app/app/virtual-lab/lab/[virtualLabId]/(project)/project/[projectId]/(secondary)/simulate/small-microcircuit/new/[circuit_id]/page.tsx
+++ src/app/app/virtual-lab/lab/[virtualLabId]/(project)/project/[projectId]/(secondary)/simulate/small-microcircuit/new/[circuit_id]/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import SimulationConfig from './SimulationConfig';
+import SimulationConfig from '@/features/small-microcircuit/SimulationConfig';
 
 export default function TinyCircuitSimulation() {
   return <SimulationConfig />;
