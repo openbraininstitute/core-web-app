65f6701c626291d4e31f96094633ed834d32edb5
Bilal MEDDAH <b_meddah@esi.dz>
2025-07-03 12:58:19 +0200

    Add attribute to pass section to the AI Assistant

--- src/components/LandingPage/components/ProgressiveImage/ProgressiveImage.tsx
+++ src/components/LandingPage/components/ProgressiveImage/ProgressiveImage.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
-
 import Image from 'next/image';
-import { classNames } from '@/util/utils';
 
 import { classNames } from '@/util/utils';
 
--- src/components/ai-assistant/hooks.ts
+++ src/components/ai-assistant/hooks.ts
@@ -1,3 +1,4 @@
+import React from 'react';
 import { useAtom } from 'jotai';
 import { atomWithStorage } from 'jotai/utils';
 
@@ -6,3 +7,15 @@ const atomSuggestedValue = atomWithStorage('AI-assistant/collapsed-panel', false
 export function useCollapsedPanel(): [value: boolean, setValue: (value: boolean) => void] {
   return useAtom(atomSuggestedValue);
 }
+
+export interface AiContext {
+  section: 'explore' | 'build' | 'simulate' | 'bookmark' | 'activity';
+}
+
+export function useAiContext(): AiContext {
+  return React.useContext(AiContextProvider);
+}
+
+export const AiContextProvider = React.createContext<AiContext>({
+  section: 'explore',
+});
