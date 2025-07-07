9cd7d6b05bce93779e570047b6f35c4ff795c681
bilalesi <b_meddah@esi.dz>
2025-07-07 11:08:25 +0200

    fix types in entitycore

--- src/api/entitycore/types/index.ts
+++ src/api/entitycore/types/index.ts
@@ -10,7 +10,7 @@ import type { IElectricalCellRecording } from '@/api/entitycore/types/entities/e
 import type { ISingleNeuronSynaptome } from '@/api/entitycore/types/entities/single-neuron-synaptome';
 import type { IExperimentalNeuronDensity } from '@/api/entitycore/types/entities/neuron-density';
 import type { IExperimentalBoutonDensity } from '@/api/entitycore/types/entities/bouton-density';
-import type { ISimulationCampaign } from '@/api/entitycore/types/entities/simulation';
+import type { ICircuitSimulationCampaign } from '@/api/entitycore/types/entities/circuit-simulation-campaign';
 import type { IMEModel } from '@/api/entitycore/types/entities/me-model';
 import type { IEModel } from '@/api/entitycore/types/entities/e-model';
 
@@ -38,7 +38,7 @@ export type EntityCoreDensityObjectTypes =
 type EntityCoreSimulationObjectTypes =
   | ISingleNeuronSynaptomeSimulation
   | ISingleNeuronSimulation
-  | ISimulationCampaign;
+  | ICircuitSimulationCampaign;
 
 export type EntityCoreObjectTypes =
   | IExperimentalSynapsesPerConnection
@@ -53,4 +53,4 @@ export type EntityCoreObjectTypes =
   | IEModel
   | ISingleNeuronSimulation
   | ICircuit
-  | ISimulationCampaign;
+  | ICircuitSimulationCampaign;
