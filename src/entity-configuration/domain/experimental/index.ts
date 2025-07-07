28ddff9db35bc25ea3a488579aa6b751d0e6ff8f
bilalesi <b_meddah@esi.dz>
2025-07-07 13:19:06 +0200

    remove unnecessary config values

--- src/entity-configuration/domain/experimental/index.ts
+++ src/entity-configuration/domain/experimental/index.ts
@@ -1,12 +1,6 @@
 // TODO: this data type should be moved from this file
 import { DataType } from '@/constants/explore-section/list-views';
 
-// export * from '@/entity-configuration/domain/experimental/reconstruction-morphology';
-// export * from '@/entity-configuration/domain/experimental/synapse-per-connection';
-// export * from '@/entity-configuration/domain/experimental/electrical-cell-recording';
-// export * from '@/entity-configuration/domain/experimental/bouton-density';
-// export * from '@/entity-configuration/domain/experimental/neuron-density';
-
 export const EXPERIMENTAL_DATATYPES = [
   DataType.ExperimentalBoutonDensity,
   DataType.ExperimentalNeuronDensity,
@@ -15,7 +9,7 @@ export const EXPERIMENTAL_DATATYPES = [
   DataType.ExperimentalNeuronMorphology,
 ] as const;
 
-enum ExperimentTypeNames {
+export enum ExperimentTypeNames {
   MORPHOLOGY = 'morphology',
   ELECTROPHYSIOLOGY = 'electrophysiology',
   NEURON_DENSITY = 'neuron-density',
