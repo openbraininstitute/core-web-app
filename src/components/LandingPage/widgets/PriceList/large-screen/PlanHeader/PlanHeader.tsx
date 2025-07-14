9632c400b15b124beaea630d67b5eeff3049c06c
Loris Olivier <53363974+loris-maru@users.noreply.github.com>
2025-07-14 09:47:33 +0200

    Fixed issue with type guard and aligned the price header block

--- src/components/LandingPage/widgets/PriceList/large-screen/PlanHeader/PlanHeader.tsx
+++ src/components/LandingPage/widgets/PriceList/large-screen/PlanHeader/PlanHeader.tsx
@@ -107,7 +107,7 @@ export default function PlanHeader({ className, plan }: PlanHeaderProps) {
               </>
             )}
           </div>
-          <ul className="mt-12 border-b border-solid border-gray-300 pb-4">
+          <ul className="mt-12 h-40 border-b border-solid border-gray-300 pb-4">
             {plan.notes.map((note, index) => (
               <li key={index}>{note}</li>
             ))}
