c79da4a5cad6268c2862f1cd9957112cc7fc9eba
Pavlo Getta <pavlo.getta@openbraininstitute.org>
2025-07-24 15:47:55 +0200

    Add low funds message for circuit sim (#667)
    * add low funds message for circuit sim
    
    * Fix typo
    
    * Re-use run sim error msg from i18n folder

---
+++ src/api/error.ts
@@ -0,0 +1,21 @@
+interface ApiErrorCause {
+  code?: string;
+  message?: string;
+  status?: number;
+  details?: any;
+}
+
+/*
+ * ApiError is a custom error class that extends the built-in Error class.
+ *
+ * It is used to represent errors that occur when making API requests.
+ */
+export default class ApiError extends Error {
+  override cause?: ApiErrorCause;
+
+  constructor(message: string, cause?: ApiErrorCause) {
+    super(message);
+    this.name = 'ApiError';
+    this.cause = cause;
+  }
+}
