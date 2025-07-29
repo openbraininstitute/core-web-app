fb95fd1cf890988734682b7aae95a2a185c00f38
bilalesi <b_meddah@esi.dz>
2025-07-29 18:16:57 +0200

    add react query

--- src/app/app/providers.tsx
+++ src/app/app/providers.tsx
@@ -1,18 +1,20 @@
 'use client';
 
-import { ReactNode } from 'react';
-import { ConfigProvider, App } from 'antd';
+import { AntdRegistry } from '@ant-design/nextjs-registry';
+import { NuqsAdapter } from 'nuqs/adapters/next/app';
 import { SessionProvider } from 'next-auth/react';
 import { Provider as JotaiProvider } from 'jotai';
+import { ConfigProvider, App } from 'antd';
 import { DevTools } from 'jotai-devtools';
-import { NuqsAdapter } from 'nuqs/adapters/next/app';
-import { AntdRegistry } from '@ant-design/nextjs-registry';
+import { ReactNode } from 'react';
 
-import commonAntdTheme from '@/theme/antd';
 import SessionStateProvider from '@/components/SessionStateProvider';
 import ThemeProvider from '@/components/ThemeProvider';
+import commonAntdTheme from '@/theme/antd';
+
 import { SessionOrNull } from '@/hooks/session';
 import { AtomProvider } from '@/state/state';
+import { QueryProvider } from '@/query-provider/client';
 
 import 'jotai-devtools/styles.css';
 
@@ -28,16 +30,18 @@ export default function Providers({ children, session }: ProvidersProps) {
         <AntdRegistry>
           <AtomProvider>
             <NuqsAdapter>
-              <JotaiProvider>
-                {process.env.NEXT_PUBLIC_JOTAI_DEVTOOLS_ENABLED && (
-                  <DevTools {...{ isInitialOpen: false, position: 'bottom-right' }} />
-                )}
-                <ThemeProvider>
-                  <SessionProvider session={session} refetchInterval={2 * 60}>
-                    <SessionStateProvider>{children}</SessionStateProvider>
-                  </SessionProvider>
-                </ThemeProvider>
-              </JotaiProvider>
+              <QueryProvider>
+                <JotaiProvider>
+                  {process.env.NEXT_PUBLIC_JOTAI_DEVTOOLS_ENABLED && (
+                    <DevTools {...{ isInitialOpen: false, position: 'bottom-right' }} />
+                  )}
+                  <ThemeProvider>
+                    <SessionProvider session={session} refetchInterval={2 * 60}>
+                      <SessionStateProvider>{children}</SessionStateProvider>
+                    </SessionProvider>
+                  </ThemeProvider>
+                </JotaiProvider>
+              </QueryProvider>
             </NuqsAdapter>
           </AtomProvider>
         </AntdRegistry>
