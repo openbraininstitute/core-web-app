fda0b3974c4a11d4f5f5892367049dcf79bb1da4
bilalesi <b_meddah@esi.dz>
2025-07-29 18:45:44 +0200

    remove legacy hardcoded circuits

--- src/app/app/virtual-lab/(free)/explore/(interactive)/interactive/(data)/model/circuit/[id]/page.tsx
+++
@@ -1,5 +0,0 @@
-import CircuitDetails from '@/components/explore-section/Circuit/DetailView/main-detail-view-core';
-
-export default function CircuitDetailPage() {
-  return <CircuitDetails />;
-}
--- src/app/app/virtual-lab/(free)/explore/(interactive)/interactive/(data)/model/circuit/page.tsx
+++
@@ -1,5 +0,0 @@
-import CircuitsListingPageComponent from '@/components/explore-section/Circuit/global/circuits-listing-page-component';
-
-export default function ExploreModelCircuitListingPage() {
-  return <CircuitsListingPageComponent />;
-}
