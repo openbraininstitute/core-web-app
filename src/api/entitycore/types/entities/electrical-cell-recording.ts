ead631a428dce693698fb9a73a73d59ea50c202f
bilalesi <b_meddah@esi.dz>
2025-08-04 15:09:44 +0200

    add filters to electrical cell recoding

--- src/api/entitycore/types/entities/electrical-cell-recording.ts
+++ src/api/entitycore/types/entities/electrical-cell-recording.ts
@@ -18,13 +18,74 @@ import type {
   EntityCoreOwnership,
 } from '@/api/entitycore/types/shared/global';
 
+const RecordingType = {
+  Intracellular: {
+    key: 'intracellular',
+    label: 'Intracellular',
+  },
+  Extracellular: {
+    key: 'extracellular',
+    label: 'extracellular',
+  },
+  Both: {
+    key: 'both',
+    label: 'both',
+  },
+  Unknown: {
+    key: 'unknown',
+    label: 'unknown',
+  },
+} as const;
+
+export const RecordingTypeDictionary = Object.fromEntries(
+  Object.entries(RecordingType).map(([name, value]) => [name, value.key])
+) as {
+  [K in keyof typeof RecordingType]: (typeof RecordingType)[K]['key'];
+};
+
+export type TRecordingTypeDictionary =
+  (typeof RecordingTypeDictionary)[keyof typeof RecordingTypeDictionary];
+
+export const ElectricalRecordingOrigin = {
+  InVivo: {
+    key: 'in_vivo',
+    label: 'in vivo',
+  },
+  InVitro: {
+    key: 'in_vitro',
+    label: 'in vitro',
+  },
+  InSilico: {
+    key: 'in_silico',
+    label: 'in silico',
+  },
+  Unknown: {
+    key: 'unknown',
+    label: 'unknown',
+  },
+} as const;
+
+export const ElectricalRecordingOriginDictionary = Object.fromEntries(
+  Object.entries(ElectricalRecordingOrigin).map(([name, value]) => [name, value.key])
+) as {
+  [K in keyof typeof ElectricalRecordingOrigin]: (typeof ElectricalRecordingOrigin)[K]['key'];
+};
+
+export type TElectricalRecordingOriginDictionary =
+  (typeof ElectricalRecordingOriginDictionary)[keyof typeof ElectricalRecordingOriginDictionary];
+
 export type ElectricalCellRecordingFilter = Partial<
   IDFilter &
     TimestampsFilter &
     ContributionFilter &
     BrainRegionFilter &
     PaginationFilter &
-    SharedFilter
+    SharedFilter & {
+      recording_type: TRecordingTypeDictionary | null;
+      recording_type__in: Array<TRecordingTypeDictionary> | null;
+      recording_origin: TElectricalRecordingOriginDictionary | null;
+      recording_origin__in: Array<TElectricalRecordingOriginDictionary> | null;
+    }
 >;
 
 interface IElectricalCellRecordingBase extends EntityCoreIdentifiable, EntityCoreOwnership {
