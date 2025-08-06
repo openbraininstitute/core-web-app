d92d5618cf9ddf460aeeaa6271b984d01e8afeed
Tolokoban <contact@tolokoban.org>
2025-08-06 09:32:53 +0200

    Added piechart

---
+++ src/components/ai-assistant/message-item/tools-components/tools/tool-plot-generator/generic-plot/generic-plot.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import Plot from 'react-plotly.js';
+import { Data, Layout } from 'plotly.js-dist-min';
+
+import { classNames } from '@/util/utils';
+import { logError } from '@/util/logger';
+
+import styles from './generic-plot.module.css';
+
+export interface GenericPlotProps<T> {
+  className?: string;
+  plotType: string;
+  value: string;
+  convert(value: T): { data: Data[]; layout: Partial<Layout> } | null;
+  assert(data: unknown): asserts data is T;
+}
+
+export default function GenericPlot<T>({
+  className,
+  plotType,
+  value,
+  convert,
+  assert,
+}: GenericPlotProps<T>) {
+  const props = React.useMemo(
+    () => makeProps(plotType, value, convert, assert),
+    [plotType, value, convert, assert]
+  );
+
+  if (!props) return null;
+
+  return (
+    <Plot
+      className={classNames(className, styles.genericPlot)}
+      style={{}}
+      data={props.data}
+      layout={props.layout}
+      useResizeHandler
+    />
+  );
+}
+
+function makeProps<T>(
+  plotType: string,
+  value: string,
+  convert: (value: T) => { data: Data[]; layout: Partial<Layout> } | null,
+  assert: (data: unknown) => asserts data is T
+): any {
+  try {
+    const obj = JSON.parse(value);
+    try {
+      assert(obj);
+      return convert(obj);
+    } catch (ex) {
+      logError(`Unexpected format for "${plotType}" chart:`, obj);
+      logError(ex);
+      return null;
+    }
+  } catch (ex) {
+    logError('Unable to parse JSON:', value);
+    logError(ex);
+    return null;
+  }
+}
--- src/components/ai-assistant/message-item/tools-components/tools/tool-plot-generator/tool-plot-generator.tsx
+++ src/components/ai-assistant/message-item/tools-components/tools/tool-plot-generator/tool-plot-generator.tsx
@@ -1,9 +1,12 @@
 /* eslint-disable react/no-array-index-key */
 import React from 'react';
+import { Data, Layout } from 'plotly.js-dist-min';
 
 import { usePlotFile } from '../hooks';
 import { ToolResult } from '../types';
-import LineChart from './line-chart';
+import { assertLineChart, convertLineChart } from './charts/line';
+import { assertPieChart, convertPieChart } from './charts/pie';
+import GenericPlot from './generic-plot';
 
 import { classNames } from '@/util/utils';
 import { isString } from '@/util/type-guards';
@@ -25,19 +28,36 @@ export default function ToolPlotGenerator({ className, results }: ToolPlotGenera
   );
 }
 
-const plotters: Record<string, React.FC<{ value: string; className?: string }>> = {
-  'json-linechart': LineChart,
+const plotters: Record<
+  string,
+  {
+    convert(value: any): { data: Data[]; layout: Partial<Layout> } | null;
+    assert(data: unknown): asserts data is any;
+  }
+> = {
+  'json-linechart': { convert: convertLineChart, assert: assertLineChart },
+  'json-piechart': { convert: convertPieChart, assert: assertPieChart },
+  'json-scatterplot': { convert: convertLineChart, assert: assertLineChart },
 };
 
 function CustomPlot({ result, className }: { result: ToolResult; className?: string }) {
   const { data } = usePlotFile(result.storage_id);
   if (!data) return null;
 
-  const Plotter = plotters[data.type];
-  if (!Plotter) return <b>{data.type}</b>;
+  const plotter = plotters[data.type];
+  if (!plotter) return <b>{data.type}</b>;
 
   const { content } = data;
   if (!isString(content)) return null;
 
-  return <Plotter className={classNames(className, styles.toolPlotGenerator)} value={content} />;
+  const { convert, assert } = plotter;
+  return (
+    <GenericPlot
+      className={classNames(className, styles.toolPlotGenerator)}
+      plotType={data.type}
+      value={content}
+      convert={convert}
+      assert={assert}
+    />
+  );
 }
