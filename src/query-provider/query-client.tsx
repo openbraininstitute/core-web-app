d4cfc93529de07417f2e070c7fbc6a3d3d5b6d68
g-bar <gilarturo.barriosdelvillar@openbraininstitute.org>
2025-08-11 14:08:23 +0200

    error for non existing entities (#695)

---
+++ src/query-provider/query-client.tsx
@@ -0,0 +1,30 @@
+import { QueryClient, defaultShouldDehydrateQuery } from '@tanstack/react-query';
+import superjson from 'superjson';
+
+export function makeQueryClient() {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 60 * 1000,
+      },
+      dehydrate: {
+        serializeData: superjson.serialize,
+        /*
+        As of React Query v5.40.0, you don't have to await all prefetches for this to work, 
+        as pending Queries can also be dehydrated and sent to the client. 
+        This lets you kick off prefetches as early as possible without letting them block an entire Suspense boundary, 
+        and streams the data to the client as the query finishes. 
+        This can be useful for example if you want to prefetch some content that is only visible after some user interaction, 
+        or say if you want to await and render the first page of an infinite query, 
+        but start prefetching page 2 without blocking rendering.
+         */
+        shouldDehydrateQuery: (query) =>
+          defaultShouldDehydrateQuery(query) || query.state.status === 'pending',
+        shouldRedactErrors: () => false,
+      },
+      hydrate: {
+        deserializeData: superjson.deserialize,
+      },
+    },
+  });
+}
---
+++ src/query-provider/server.tsx
@@ -0,0 +1,16 @@
+import 'server-only';
+
+import { HydrationBoundary, dehydrate } from '@tanstack/react-query';
+import { cache } from 'react';
+
+import { makeQueryClient } from '@/query-provider/query-client';
+
+// IMPORTANT: Create a stable getter for the query client that
+//            will return the same client during the same request.
+export const getQueryClient = cache(makeQueryClient);
+
+export function HydrateClient({ children }: { children: React.ReactNode }) {
+  const queryClient = getQueryClient();
+
+  return <HydrationBoundary state={dehydrate(queryClient)}>{children}</HydrationBoundary>;
+}
