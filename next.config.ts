2c9383f634e64515409eb3d8e9ecd40fdfc3f40d
bilalesi <b_meddah@esi.dz>
2025-08-11 16:53:26 +0200

    redirect temporarly to v2

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,8 @@ import { withSentryConfig } from '@sentry/nextjs';
 import NextBundleAnalyzer from '@next/bundle-analyzer';
 import { PHASE_DEVELOPMENT_SERVER } from 'next/constants';
 
-import { env } from './src/env';
-
 import type { NextConfig } from 'next/dist/types';
+import { env } from './src/env';
 
 const withBundleAnalyzer = NextBundleAnalyzer({
   enabled: process.env.ANALYZE === 'true',
