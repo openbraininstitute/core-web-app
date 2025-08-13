96c0a7a3c90b2a33d58e2d8a7963a85f2a51e3ce
Gil Arturo Barrios del Villar <gilarturo.barriosdelvillar@openbraininstitute.org>
2025-08-13 16:33:53 +0200

    breadcrumbs

--- src/app/app/v2/[virtualLabId]/[projectId]/explore/view/[type]/[id]/layout.tsx
+++ src/app/app/v2/[virtualLabId]/[projectId]/explore/view/[type]/[id]/layout.tsx
@@ -1,7 +1,36 @@
+'use client';
+
 import { ReactNode } from 'react';
+import NextLink from 'next/link';
+import { useParams } from 'next/navigation';
+import Breadcrumb from '@/ui/molecules/breadcrumb';
+import { basePath } from '@/config';
+import useWorkspace from '@/ui/hooks/use-workspace';
 
 export default function Layout({ children }: { children: ReactNode }) {
+  const { virtualLabId, projectId } = useWorkspace();
+  const { type } = useParams();
+
   return (
-    <div className="ml-5 h-full rounded-md border-[1px] border-[#D9D9D9] px-5 py-3">{children}</div>
+    <div className="ml-5 flex h-full rounded-md border-[1px] border-[#D9D9D9] px-5 py-3">
+      <div className="flex basis-1/5">
+        <div className="flex flex-wrap gap-3">
+          <Breadcrumb>
+            <NextLink href={`${basePath}/app/v2/${virtualLabId}/${projectId}/explore`}>
+              Explore
+            </NextLink>
+          </Breadcrumb>
+          <Breadcrumb>
+            <NextLink
+              href={`${basePath}/app/v2/${virtualLabId}/${projectId}/explore/browse/${type}`}
+            >
+              {type}
+            </NextLink>
+          </Breadcrumb>
+          <Breadcrumb showChevron={false}>One</Breadcrumb>
+        </div>
+      </div>
+      <div className="grow basis-4/5">{children}</div>
+    </div>
   );
 }
---
+++ src/ui/molecules/breadcrumb.tsx
@@ -0,0 +1,21 @@
+import { RightOutlined } from '@ant-design/icons';
+import { ReactNode } from 'react';
+
+export default function Breadcrumb({
+  children,
+  showChevron = true,
+}: {
+  children?: ReactNode;
+  showChevron?: boolean;
+}) {
+  return (
+    <div className="align-center inline-flex justify-center gap-4">
+      <span className="text-primary-8">{children}</span>
+      {showChevron && (
+        <div className="text-gray-500">
+          <RightOutlined />
+        </div>
+      )}
+    </div>
+  );
+}
