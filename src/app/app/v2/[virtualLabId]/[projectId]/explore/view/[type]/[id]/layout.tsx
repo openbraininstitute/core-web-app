04857c9a004c9d19d9974557d7732d6457f90771
Gil Arturo Barrios del Villar <gilarturo.barriosdelvillar@openbraininstitute.org>
2025-08-13 17:11:08 +0200

    Fetch entity

--- src/app/app/v2/[virtualLabId]/[projectId]/explore/view/[type]/[id]/layout.tsx
+++ src/app/app/v2/[virtualLabId]/[projectId]/explore/view/[type]/[id]/layout.tsx
@@ -2,14 +2,27 @@
 
 import { ReactNode } from 'react';
 import NextLink from 'next/link';
-import { useParams } from 'next/navigation';
+import { notFound, useParams } from 'next/navigation';
 import Breadcrumb from '@/ui/molecules/breadcrumb';
 import { basePath } from '@/config';
-import useWorkspace from '@/ui/hooks/use-workspace';
+import { useWorkspace } from '@/ui/hooks/use-workspace';
+import { getEntityByExtendedType } from '@/entity-configuration/domain/helpers';
+import { TExtendedEntitiesTypeDict } from '@/api/entitycore/types/extended-entity-type';
 
 export default function Layout({ children }: { children: ReactNode }) {
   const { virtualLabId, projectId } = useWorkspace();
-  const { type } = useParams();
+  const { type, id } = useParams();
+
+  if (!type || !id) notFound();
+
+  const entityType = getEntityByExtendedType({ type: type as TExtendedEntitiesTypeDict });
+  if (!entityType) notFound();
+
+  const fetchEntity = entityType.api.query.one;
+  const entity =
+    fetchEntity && (await fetchEntity({ id: id as string, context: { virtualLabId, projectId } }));
+
+  if (!entity) notFound();
 
   return (
     <div className="ml-5 flex h-full rounded-md border-[1px] border-[#D9D9D9] px-5 py-3">
