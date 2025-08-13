70787c00241d0a58ce7fb705067e3e752be94859
Gil Arturo Barrios del Villar <gilarturo.barriosdelvillar@openbraininstitute.org>
2025-08-13 17:50:29 +0200

    breadcrumbs

--- src/app/app/v2/[virtualLabId]/[projectId]/explore/view/[type]/[id]/layout.tsx
+++ src/app/app/v2/[virtualLabId]/[projectId]/explore/view/[type]/[id]/layout.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { ReactNode } from 'react';
+import { useQuery } from '@tanstack/react-query';
 import NextLink from 'next/link';
 import { notFound, useParams } from 'next/navigation';
 import Breadcrumb from '@/ui/molecules/breadcrumb';
@@ -19,14 +20,19 @@ export default function Layout({ children }: { children: ReactNode }) {
   if (!entityType) notFound();
 
   const fetchEntity = entityType.api.query.one;
-  const entity =
-    fetchEntity && (await fetchEntity({ id: id as string, context: { virtualLabId, projectId } }));
 
-  if (!entity) notFound();
+  if (!fetchEntity) throw Error(`No fetch one function defined for type ${entityType}`);
+
+  const { data, error } = useQuery({
+    queryKey: [id as string],
+    queryFn: async () => fetchEntity({ id: id as string, context: { virtualLabId, projectId } }),
+  });
+
+  if (error) notFound();
 
   return (
     <div className="ml-5 flex h-full rounded-md border-[1px] border-[#D9D9D9] px-5 py-3">
-      <div className="flex basis-1/5">
+      <div className="basis-1/5">
         <div className="flex flex-wrap gap-3">
           <Breadcrumb>
             <NextLink href={`${basePath}/app/v2/${virtualLabId}/${projectId}/explore`}>
@@ -40,7 +46,7 @@ export default function Layout({ children }: { children: ReactNode }) {
               {type}
             </NextLink>
           </Breadcrumb>
-          <Breadcrumb showChevron={false}>One</Breadcrumb>
+          <Breadcrumb showChevron={false}>{data?.name}</Breadcrumb>
         </div>
       </div>
       <div className="grow basis-4/5">{children}</div>
