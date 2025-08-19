16be69a66152094db082b5d857efb96adbafef7f
loris-olivier-obi <olivier.loris@openbraininstitute.org>
2025-08-19 11:30:46 +0200

    Added prefix to the help query-keys in ui

--- src/ui/use-query-keys/help.ts
+++ src/ui/use-query-keys/help.ts
@@ -1,10 +1,12 @@
+const prefix = 'help';
+
 export const HELP_QUERY_KEYS = {
   entitycore: (cellType: 'e-type' | 'm-type', name: string) =>
-    ['entitycore', cellType, name] as const,
+    [prefix, 'entitycore', cellType, name] as const,
   entityTypes: (
     cellType: 'e-type' | 'm-type',
     activePage: number,
     pageSize: number,
     filter?: any
-  ) => ['entityTypes', cellType, activePage, pageSize, filter] as const,
+  ) => [prefix, 'entityTypes', cellType, activePage, pageSize, filter] as const,
 };
