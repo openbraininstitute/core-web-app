0ad7690e0c75ed5637830dc376b7191746156f23
Gil Arturo Barrios del Villar <gilarturo.barriosdelvillar@openbraininstitute.org>
2025-08-20 15:09:45 +0200

    Working on detail view

--- src/app/app/v2/[virtualLabId]/[projectId]/explore/view/[type]/[id]/overview/page.tsx
+++ src/app/app/v2/[virtualLabId]/[projectId]/explore/view/[type]/[id]/overview/page.tsx
@@ -1,7 +1,6 @@
 import type { TExtendedEntitiesTypeDict } from '@/api/entitycore/types/extended-entity-type';
 import type { ServerSideComponentProp, WorkspaceContext } from '@/types/common';
 import type { KebabCase } from '@/utils/type';
-import Overview from '@/features/details-view/overview';
 import {
   CommonSummaryViewFields,
   getViewDefinitionByExtendedType,
@@ -9,37 +8,38 @@ import {
 import { getEntityByExtendedType, getEntityBySlug } from '@/entity-configuration/domain/helpers';
 import { EntitySlugValue } from '@/entity-configuration/domain/slug';
 import { notFound } from 'next/navigation';
-// import { Field } from '@/features/details-view/overview';
+import { downloadEntity } from '../layout';
+import { Field } from '@/features/details-view/overview';
+import { getFieldDefinition } from '@/entity-configuration/definitions';
 
 export default async function Page({
   params,
 }: ServerSideComponentProp<WorkspaceContext & { type: EntitySlugValue; id: string }, null>) {
   const { virtualLabId, projectId, type, id } = await params;
 
-  const entityType = getEntityBySlug({ slug: type });
-  if (!entityType) notFound();
+  const { entity, entityType } = await downloadEntity({
+    type,
+    ctx: { virtualLabId, projectId },
+    id,
+  });
 
   const fields = getViewDefinitionByExtendedType(entityType.extendedType)?.summaryViewFields;
   if (!fields) notFound();
 
   const commonFields = CommonSummaryViewFields;
 
-
-
   return (
-    <div className="flex w-full flex-row gap-x-8">
-      {/* {commonFields.length > 0 && (
-        <div className="grid w-1/2 auto-rows-max grid-cols-2 gap-x-8 gap-y-6">
-          {commonFields.map(({ className, field }) => (
-            // <Field key={field} className={className} field={field} data={detail} />
-          ))}
-        </div>
-      )}
-      <div className="grid w-1/2 auto-rows-min grid-cols-3 gap-x-8 gap-y-6">
-        {fields.map(({ className, field }) => (
-          // <Field key={field} className={className} field={field} data={detail} />
-        ))}
-      </div> */}
+    <div className="p-10">
+      <div className="mb-5">
+        <div className="text-neutral-4 uppercase">Name</div>
+        <div className="text-primary-8 text-2xl font-bold">{entity.name}</div>
+      </div>
+      <div className="grid grid-cols-2 gap-4 rounded-lg border border-gray-300 p-5">
+        {[...commonFields, ...fields].map(({ className, field }) => {
+          const fieldObj = getFieldDefinition(field);
+          return fieldObj?.render?.(entity);
+        })}
+      </div>
     </div>
   );
 }
