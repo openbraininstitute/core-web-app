b08439a0a2abc7cca0940ed1d87490b1691fe508
Gil Arturo Barrios del Villar <gilarturo.barriosdelvillar@openbraininstitute.org>
2025-08-20 17:12:11 +0200

    add electrophysiology viz

--- src/app/app/v2/[virtualLabId]/[projectId]/explore/view/[type]/[id]/overview/page.tsx
+++ src/app/app/v2/[virtualLabId]/[projectId]/explore/view/[type]/[id]/overview/page.tsx
@@ -7,17 +7,17 @@ import {
 } from '@/entity-configuration/definitions/view-defs';
 import { EntitySlugValue } from '@/entity-configuration/domain/slug';
 import { Field } from '@/features/details-view/overview';
-import { MorphoViewerLoaderMemo } from '@/features/entities/reconstruction-morphology/detail-view';
-import { IReconstructionMorphology } from '@/api/entitycore/types';
+import Visualization from '@/ui/segments/viz';
 
 export default async function Page({
   params,
 }: ServerSideComponentProp<WorkspaceContext & { type: EntitySlugValue; id: string }, null>) {
   const { virtualLabId, projectId, type, id } = await params;
+  const ctx = { virtualLabId, projectId };
 
   const { entity, entityType } = await downloadEntity({
     type,
-    ctx: { virtualLabId, projectId },
+    ctx,
     id,
   });
 
@@ -26,28 +26,19 @@ export default async function Page({
 
   const commonFields = CommonSummaryViewFields;
 
-  function renderViz() {
-    if (entity.type === 'reconstruction_morphology') {
-      return <MorphoViewerLoaderMemo resource={entity as IReconstructionMorphology} />;
-    }
-
-    return null;
-  }
-
-  const viz = renderViz();
-
   return (
     <div className="h-full overflow-y-auto p-10">
       <div className="mb-5">
         <div className="text-neutral-4 uppercase">Name</div>
         <div className="text-primary-8 text-2xl font-bold">{entity.name}</div>
       </div>
-      <div className="grid grid-cols-3 gap-4 rounded-lg border border-gray-300 p-5">
+      <div className="mb-5 grid grid-cols-3 gap-4 rounded-lg border border-gray-300 p-5">
         {[...commonFields, ...fields].map(({ className, field }) => {
           return <Field key={field} className={className} field={field} data={entity} />;
         })}
       </div>
-      {viz && <div className="mt-10 h-full">{viz}</div>}
+
+      <Visualization entity={entity} ctx={ctx} />
     </div>
   );
 }
