73785b0f02917b19fd64b03899c5bc624c53401a
Gil Arturo Barrios del Villar <gilarturo.barriosdelvillar@openbraininstitute.org>
2025-08-20 16:02:00 +0200

    add morphology page

--- src/app/app/v2/[virtualLabId]/[projectId]/explore/view/[type]/[id]/overview/page.tsx
+++ src/app/app/v2/[virtualLabId]/[projectId]/explore/view/[type]/[id]/overview/page.tsx
@@ -7,6 +7,8 @@ import {
 } from '@/entity-configuration/definitions/view-defs';
 import { EntitySlugValue } from '@/entity-configuration/domain/slug';
 import { Field } from '@/features/details-view/overview';
+import { MorphoViewerLoaderMemo } from '@/features/entities/reconstruction-morphology/detail-view';
+import { IReconstructionMorphology } from '@/api/entitycore/types';
 
 export default async function Page({
   params,
@@ -24,8 +26,18 @@ export default async function Page({
 
   const commonFields = CommonSummaryViewFields;
 
+  function renderViz() {
+    if (entity.type === 'reconstruction_morphology') {
+      return <MorphoViewerLoaderMemo resource={entity as IReconstructionMorphology} />;
+    }
+
+    return null;
+  }
+
+  const viz = renderViz();
+
   return (
-    <div className="p-10">
+    <div className="h-full overflow-y-auto p-10">
       <div className="mb-5">
         <div className="text-neutral-4 uppercase">Name</div>
         <div className="text-primary-8 text-2xl font-bold">{entity.name}</div>
@@ -35,6 +47,7 @@ export default async function Page({
           return <Field key={field} className={className} field={field} data={entity} />;
         })}
       </div>
+      {viz && <div className="mt-10 h-full">{viz}</div>}
     </div>
   );
 }
