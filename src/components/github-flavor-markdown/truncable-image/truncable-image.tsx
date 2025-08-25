46166deb10b816026240be45e8938564c2cbfdd3
Tolokoban <contact@tolokoban.org>
2025-08-25 13:43:59 +0200

    250/ai assistant clear history (#747)
    * History is back and images are truncated now
    
    * Make the scroll more controllable
    
    * Linting
    
    * Optimize tools queries to prevent from loading description when not necessary
    
    * Zoom image
    
    * Keep image ratio when bigger than the screen
    
    * Rename Clear Chat into New Chat
    
    * Multitab session storage + local linkgs without target
    
    * Before merging
    
    * Fixed an issue with the history list

---
+++ src/components/github-flavor-markdown/truncable-image/truncable-image.tsx
@@ -0,0 +1,86 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable @next/next/no-img-element */
+import React from 'react';
+import Link from 'next/link';
+
+import { classNames } from '@/util/utils';
+import { logError } from '@/util/logger';
+
+import styles from './truncable-image.module.css';
+
+export interface TruncableImageProps {
+  className?: string;
+  src?: string;
+}
+
+export default function TruncableImage({ className, src }: TruncableImageProps) {
+  const refDialog = React.useRef<HTMLDialogElement | null>(null);
+  const [image, setImage] = React.useState<HTMLImageElement | null>(null);
+  const [error, setError] = React.useState(false);
+  React.useEffect(() => {
+    setError(false);
+    if (!src) {
+      setImage(null);
+      return;
+    }
+
+    const img = new Image();
+    img.src = src;
+    img.onload = () => {
+      setImage(img);
+    };
+    img.onerror = () => {
+      logError('Unable to load image:', src);
+      setError(true);
+      setImage(null);
+    };
+  }, [src]);
+  const handleShow = () => {
+    const dialog = refDialog.current;
+    if (!dialog) return;
+
+    dialog.showModal();
+  };
+  const handleHide = () => {
+    refDialog.current?.close();
+  };
+  if (error && src) {
+    return (
+      <Link className={styles.error} href={src} target="_blank">
+        Unable to load image!
+      </Link>
+    );
+  }
+
+  if (!image) return null;
+
+  return (
+    <>
+      <button
+        type="button"
+        className={classNames(className, styles.truncableImage)}
+        onClick={handleShow}
+        style={{
+          '--custom-aspect-ratio': `${image.width}/${image.height}`,
+          '--custom-image': `url(${image.src})`,
+          '--custom-width': `${image.width}px`,
+          '--custom-height': `${image.height}px`,
+        }}
+      />{' '}
+      <dialog ref={refDialog} className={styles.dialog}>
+        <button type="button" onClick={handleHide}>
+          <img
+            src={image.src}
+            width={image.width}
+            height={image.height}
+            style={{
+              width: `${image.width}px`,
+              height: `${image.height}px`,
+            }}
+          />
+        </button>
+      </dialog>
+    </>
+  );
+}
--- src/services/ai-agent/assistant/manager/thread.ts
+++ src/services/ai-agent/assistant/manager/thread.ts
@@ -1,8 +1,11 @@
-import { serviceAiAgentThreadCreate } from '../../api';
+import { serviceAiAgentThreadCreate, serviceAiAgentThreadExists } from '../../api';
 import { Signal } from '../signal';
 import { AssistantContext } from '../types';
+import { sharedSessionStorage } from '@/util/shared-session-storage';
 
 export class ThreadManager {
+  private context: AssistantContext | null = null;
+
   constructor(
     private readonly target: {
       threadId: Signal<string | undefined>;
@@ -11,32 +14,42 @@ export class ThreadManager {
 
   /**
    * The first time we open the AI Assistant, we try to get the
-   * last recently used thread. If no such thread exists, we
-   * create  new one.
+   * last recently used thread stored in session storage.
+   * If no such thread exists, we create a new one.
    */
   readonly init = async (context: AssistantContext) => {
+    this.context = context;
     const { target } = this;
-    const { accessToken, virtualLabId, projectId } = context;
-    // We comment this out for the moment, but we will need to bring it back soon.
-    // target.threadId.set(undefined);
-    // const threads = await serviceAiAgentThreadList({
-    //   accessToken,
-    //   virtualLabId,
-    //   projectId,
-    //   pageSize: 1,
-    //   excludeEmptyThreads: false,
-    // });
-    // const [result] = threads.results;
-    // if (result) {
-    //   target.threadId.set(result.thread_id);
-    // } else {
-    const thread = await serviceAiAgentThreadCreate({
-      accessToken,
-      virtualLabId,
-      projectId,
-      title: new Date().toUTCString(),
-    });
-    target.threadId.set(thread.threadId);
-    // }
+    const { accessToken } = context;
+    const sessionThreadId = await this.recoverLastThreadId(accessToken);
+    if (sessionThreadId) {
+      target.threadId.set(sessionThreadId);
+    } else {
+      await this.createThread();
+    }
   };
+
+  readonly createThread = async () => {
+    const { context, target } = this;
+    if (!context) throw new Error('ThreadManager has not been initialized yet!');
+
+    const params = {
+      ...context,
+      title: new Date().toUTCString(),
+    };
+    const thread = await serviceAiAgentThreadCreate(params);
+    const { threadId } = thread;
+    target.threadId.set(threadId);
+    sharedSessionStorage.setItem('AI-Assistant/threadId', threadId);
+    return threadId;
+  };
+
+  private async recoverLastThreadId(accessToken: string): Promise<string | null> {
+    const sessionThreadId = sharedSessionStorage.getItem('AI-Assistant/threadId') ?? '';
+    if (!sessionThreadId) return null;
+
+    // Check thread existence.
+    const exists = await serviceAiAgentThreadExists({ accessToken, threadId: sessionThreadId });
+    return exists ? sessionThreadId : null;
+  }
 }
