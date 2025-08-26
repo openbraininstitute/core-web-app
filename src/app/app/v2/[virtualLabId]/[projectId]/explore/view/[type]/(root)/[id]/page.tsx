7e884367af9dfcfa12a1fc88c782730eb969523f
Gil Arturo Barrios del Villar <gilarturo.barriosdelvillar@openbraininstitute.org>
2025-08-26 11:55:57 +0200

    remove unused routes

--- src/app/app/v2/[virtualLabId]/[projectId]/explore/view/[type]/(root)/[id]/page.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import { redirect, usePathname } from 'next/navigation';
-
-export default function DetailPage() {
-  const path = usePathname();
-  redirect(`${path}/overview`);
-}
