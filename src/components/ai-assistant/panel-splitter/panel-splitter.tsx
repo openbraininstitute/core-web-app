878115326aa88e738f8c09d01ebeaf9266a3d682
Tolokoban <contact@tolokoban.org>
2025-08-29 15:34:40 +0200

    Integrate Ai Assistant and adapt resizing (#764)

--- src/components/ai-assistant/panel-splitter/panel-splitter.tsx
+++ src/components/ai-assistant/panel-splitter/panel-splitter.tsx
@@ -2,39 +2,16 @@
 import React from 'react';
 
 import { usePointerHandler } from './pointer-handler';
+
 import { classNames } from '@/util/utils';
 
 import styles from './panel-splitter.module.css';
 
 interface PanelSplitterProps {
   className?: string;
-  panelWidth: number;
-  setPanelWidth(panelWidth: number): void;
 }
 
-export default function PanelSplitter({
-  className,
-  panelWidth,
-  setPanelWidth,
-}: PanelSplitterProps) {
+export default function PanelSplitter({ className }: PanelSplitterProps) {
   const handler = usePointerHandler();
-  React.useEffect(() => {
-    handler.panelWidth = panelWidth;
-  }, [panelWidth, handler]);
-  React.useEffect(() => {
-    handler.eventPanelWidthChange.addListener(setPanelWidth);
-    return () => handler.eventPanelWidthChange.removeListener(setPanelWidth);
-  }, [setPanelWidth, handler]);
-  return (
-    <div
-      className={classNames(
-        className,
-        styles.panelSplitter,
-        panelWidth >= 100 && styles.fullscreen
-      )}
-      {...handler.props}
-    >
-      <div />
-    </div>
-  );
+  return <div className={classNames(className, styles.panelSplitter)} {...handler.props} />;
 }
--- src/components/github-flavor-markdown/github-flavor-markdown.tsx
+++ src/components/github-flavor-markdown/github-flavor-markdown.tsx
@@ -4,6 +4,7 @@ import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 import rehypeKatex from 'rehype-katex';
 import remarkMath from 'remark-math';
+import TruncableImage from './truncable-image';
 
 import { classNames } from '@/util/utils';
 
@@ -12,16 +13,22 @@ import styles from './github-flavor-markdown.module.css';
 interface GithubFlavorMarkdownProps {
   className?: string;
   children: string;
+  onLinkClicked(external: boolean): void;
 }
 
-export function GithubFlavorMarkdown({ className, children }: GithubFlavorMarkdownProps) {
+export function GithubFlavorMarkdown({
+  className,
+  children,
+  onLinkClicked,
+}: GithubFlavorMarkdownProps) {
   return (
     <ReactMarkdown
       className={classNames(className, styles.githubFlavorMarkdown)}
       remarkPlugins={[remarkGfm, remarkMath]}
       rehypePlugins={[rehypeKatex]}
       components={{
-        a: LinkWithExternalTarget,
+        a: makeLink(onLinkClicked),
+        img: TruncableImage,
       }}
     >
       {children}
@@ -29,12 +36,40 @@ export function GithubFlavorMarkdown({ className, children }: GithubFlavorMarkdo
   );
 }
 
-function LinkWithExternalTarget({ href, children }: AnchorHTMLAttributes<HTMLAnchorElement>) {
-  if (!href) return null;
+function makeLink(onLinkClicked: (external: boolean, href: string) => void | boolean) {
+  function LinkWithExternalTarget({ href, children }: AnchorHTMLAttributes<HTMLAnchorElement>) {
+    if (!href) return null;
 
-  return (
-    <Link href={href} target="_blank">
-      {children}
-    </Link>
-  );
+    const info = resolveLinkTarget(href);
+    return (
+      <Link
+        href={info.href}
+        target={info.target}
+        onClick={(evt) => {
+          if (onLinkClicked(!!info.target, info.href)) {
+            evt.stopPropagation();
+            evt.preventDefault();
+          }
+        }}
+      >
+        {children}
+      </Link>
+    );
+  }
+  return LinkWithExternalTarget;
+}
+
+function resolveLinkTarget(href: string): { href: string; target?: string } {
+  const url = new URL(href);
+  if (url.origin === globalThis.location.origin) return { href };
+
+  if (url.origin === 'https://staging.openbraininstitute.org') {
+    // This is a hack for the dev, because Ai Agent
+    // returns absolute URLs instead of relative ones.
+    return {
+      href: `${globalThis.location.origin}${href.slice(url.origin.length)}`,
+    };
+  }
+
+  return { href, target: href };
 }
