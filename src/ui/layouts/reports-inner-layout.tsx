17f207b0f4622da88c11128a899b615fd59aaf84
loris-olivier-obi <olivier.loris@openbraininstitute.org>
2025-09-01 13:26:56 +0200

    Removed mini detail view from notebooks and reports

--- src/ui/layouts/reports-inner-layout.tsx
+++ src/ui/layouts/reports-inner-layout.tsx
@@ -1,34 +1,18 @@
 'use client';
 
 import { motion } from 'motion/react';
-import { type ReactNode, useState } from 'react';
-
-import { useSelectEntityClickEvent } from '@/ui/segments/mini-detail-view/event';
-import { cn } from '@/utils/css-class';
+import { type ReactNode } from 'react';
 
 type Props = {
   children: ReactNode;
 };
 
 export function ReportsInnerLayout({ children }: Props) {
-  const [miniViewPresent, setMiniViewPresent] = useState(false);
-  useSelectEntityClickEvent((ev) => {
-    setMiniViewPresent(ev.detail.display);
-  });
-
   return (
     <motion.div
       id="reports-inner-layout"
-      className={cn(
-        'bg-neutral-1 border-neutral-2 mx-2 mb-2 grid h-full max-h-[calc(100vh-8rem)] w-[calc(100%-10px)] content-start gap-4 overflow-hidden rounded-2xl border p-5 [grid-area:main]',
-        { "grid-cols-[1fr_3fr] [grid-template-areas:'aside_body']": !miniViewPresent },
-        { "grid-cols-[3fr_1fr] [grid-template-areas:'body_mini-view']": miniViewPresent }
-      )}
+      className="bg-neutral-1 border-neutral-2 mx-2 mb-2 grid h-full max-h-[calc(100vh-8rem)] w-[calc(100%-10px)] content-start gap-4 overflow-hidden rounded-2xl border p-5 [grid-area:main]"
       initial={false}
-      animate={{
-        gridTemplateColumns: miniViewPresent ? '3fr 1fr' : '1fr 3fr',
-        gridTemplateAreas: miniViewPresent ? "'body mini-view'" : "'aside body'",
-      }}
       transition={{
         type: 'spring',
         stiffness: 320,
