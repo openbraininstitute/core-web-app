7275bb9307f8d5af876a17fbb7d12b2d5c95c2e8
Gil Arturo Barrios del Villar <gilarturo.barriosdelvillar@openbraininstitute.org>
2025-09-08 13:55:51 +0200

    add tabs

--- src/ui/molecules/tabbed-page.tsx
+++ src/ui/molecules/tabbed-page.tsx
@@ -22,16 +22,23 @@ export default function Tabs({ children, defaultIndex = 0 }: TabsProps) {
 
   if (tabs.length === 0) return <div>No tabs defined.</div>;
 
+  function rounded(index: number) {
+    if (index === 0) return 'rounded-l-full!';
+    if (index === tabs.length - 1) return 'rounded-r-full!';
+    return undefined;
+  }
+
   return (
     <div className="w-full">
       {tabs.map((tab, index) => (
         <Button
           size="lg"
-          rounded
           variant="outline"
           key={tab.props.label}
           onClick={() => setActiveIndex(index)}
           active={activeIndex === index}
+          className={rounded(index)}
+          borderless
         >
           {tab.props.label}
         </Button>
