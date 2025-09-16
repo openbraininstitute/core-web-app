939b408bffff04050aa4d435d85ccd377072e549
bilalesi <b_meddah@esi.dz>
2025-09-16 12:51:11 +0100

    allow renaming virtual lab only for admins

---
+++ src/i18n/en/virtual-lab.ts
@@ -0,0 +1,11 @@
+export const messages = {
+  RenameVirtualLabSucceed: 'The virtual lab has been successfully renamed.',
+  RenameVirtualLabFailed: 'Failed to rename the virtual lab.',
+  RenameVirtualLabNameAlreadyExists:
+    'A Virtual Lab with this name already exists. Please choose a different name.',
+  RenameVirtualLabNameValidationFailed: 'Name validation failed, please try again.',
+  RenameVirtualLabFailedTitle: 'Failed to rename the virtual lab.',
+  RenameVirtualLabFailedDescription: 'Please try again or contact support if the issue persists.',
+  RenameVirtualLabSucceedTitle: 'Update virtual lab',
+  RenameVirtualLabSucceedDescription: 'The virtual lab has been successfully renamed.',
+};
