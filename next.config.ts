249ba994a7ba303e53f5ce26c9036fc914867a4b
bilalesi <b_meddah@esi.dz>
2025-09-26 13:17:30 +0200

    integrate sentry

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,4 @@
-import { withSentryConfig } from '@sentry/nextjs';
+import { SentryBuildOptions, withSentryConfig } from '@sentry/nextjs';
 import NextBundleAnalyzer from '@next/bundle-analyzer';
 import { PHASE_DEVELOPMENT_SERVER } from 'next/constants';
 
@@ -9,12 +9,33 @@ const withBundleAnalyzer = NextBundleAnalyzer({
   enabled: process.env.ANALYZE === 'true',
 });
 
-const SentryWebpackPluginOptions = { silent: true, dryRun: !env.NEXT_PUBLIC_SENTRY_DSN };
-
 const basePath = env.NEXT_PUBLIC_BASE_PATH;
 const cdnUri = env.NEXT_PUBLIC_CDN_URI || process.env.NEXT_PUBLIC_CDN_URI;
-
 const coreWebAppVersion = env.NEXT_PUBLIC_CORE_WEB_APP_VERSION;
+
+const SentryOptions: SentryBuildOptions = {
+  // For all available options, see:
+  // https://www.npmjs.com/package/@sentry/webpack-plugin#options
+
+  org: env.NEXT_PUBLIC_SENTRY_ORG,
+  project: env.NEXT_PUBLIC_SENTRY_PRJ,
+
+  // Only print logs for uploading source maps in CI
+  silent: !process.env.CI,
+
+  // For all available options, see:
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+  // Upload a larger set of source maps for prettier stack traces (increases build time)
+  widenClientFileUpload: true,
+
+  // Automatically tree-shake Sentry logger statements to reduce bundle size
+  disableLogger: true,
+  automaticVercelMonitors: false,
+  release: {
+    name: coreWebAppVersion,
+  },
+};
 
 const nextConfig = (phase: string): NextConfig => {
   const isDev = phase === PHASE_DEVELOPMENT_SERVER;
@@ -130,5 +151,5 @@ const nextConfig = (phase: string): NextConfig => {
 };
 
 export default function generateConfig(phase: string) {
-  return withBundleAnalyzer(withSentryConfig(nextConfig(phase), SentryWebpackPluginOptions));
+  return withBundleAnalyzer(withSentryConfig(nextConfig(phase), SentryOptions));
 }
