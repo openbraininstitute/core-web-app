7cb30365cf5064df4e97e71421bba61a1456a9a4
bilalesi <b_meddah@esi.dz>
2025-10-08 14:06:27 +0200

    remove unused files using knip

--- src/components/tutorials-carrousel/scroll.ts
+++
@@ -1,8 +0,0 @@
-export function scrollCardIntoView(container: HTMLDivElement, cardIndex: number) {
-  const node = container.querySelectorAll('a').item(cardIndex);
-  if (node) {
-    node.scrollIntoView({
-      behavior: 'smooth',
-    });
-  }
-}
