c78a21cbfefb346261e5527516fcce2efdeaa599
Gil Arturo Barrios del Villar <gilarturo.barriosdelvillar@openbraininstitute.org>
2025-10-23 23:08:25 +0200

    jupyter logo

--- src/services/notebooks/index.ts
+++ src/services/notebooks/index.ts
@@ -24,6 +24,21 @@ export interface NotebookStartRequest {
   };
 }
 
+export interface EmptyNotebookStartRequest {
+  vlabId: string;
+  projectId: string;
+  session: {
+    idToken: string;
+    accessToken: string;
+    user: {
+      email: string;
+      id: string;
+      name: string;
+      username: string;
+    };
+  };
+}
+
 export async function startNotebook(
   id: string,
   filename: string,
@@ -58,6 +73,71 @@ export async function startNotebook(
     },
   };
   const res = await authFetch(`${notebookSvcBaseUrl}/analysis_notebook_template/start`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(request),
+  });
+
+  if (!res.ok) {
+    if (res.status === 460) {
+      throw Error('AccountingReservationError', {
+        cause: {
+          error_code: 'ACCOUNTING_RESERVATION_ERROR',
+          hint: 'No reservation could be made in the accounting service',
+        },
+      });
+    }
+    if (res.status === 461) {
+      throw Error('InsufficientFundsError', {
+        cause: {
+          error_code: 'INSUFFICIENT_FUNDS_ERROR',
+          hint: 'Not enough credits to run the notebook',
+        },
+      });
+    }
+    if (res.status === 462) {
+      throw Error('JupyterError', {
+        cause: {
+          error_code: 'JUPYTER_ERROR',
+          hint: 'The notebook could not be launched in Jupyter',
+        },
+      });
+    }
+  }
+
+  return assertApiResponse(res);
+}
+
+export async function startEmptyNotebook(
+  vlabId: string,
+  projectId: string
+): Promise<NotebookStartResponse> {
+  const session = await getSession();
+
+  if (!session) {
+    throw Error('no session found', {
+      cause: {
+        error_code: 'SESSION_NOT_FOUND',
+        hint: 'You need to be logged in to start a notebook',
+      },
+    });
+  }
+
+  const request = {
+    vlabId,
+    projectId,
+    session: {
+      idToken: session.idToken,
+      accessToken: session.accessToken,
+      user: {
+        email: session.user.email ?? '',
+        id: session.user.id,
+        name: session.user.name ?? '',
+        username: session.user.username,
+      },
+    },
+  };
+  const res = await authFetch(`${notebookSvcBaseUrl}/empty/start`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(request),
--- src/ui/layouts/notebooks-layout.tsx
+++ src/ui/layouts/notebooks-layout.tsx
@@ -1,6 +1,12 @@
+'use client';
+
 import { ReactNode } from 'react';
 import NextLink from 'next/link';
+import Image from 'next/image';
+import { useWorkspace } from '../hooks/use-workspace';
 import { cn } from '@/utils/css-class';
+import { startEmptyNotebook } from '@/services/notebooks';
+import { useAppNotification } from '@/components/notification';
 
 type Props = {
   children: ReactNode;
@@ -8,28 +14,67 @@ type Props = {
 };
 
 export function NotebooksLayout({ children, active }: Props) {
+  const { virtualLabId, projectId } = useWorkspace();
+  const notification = useAppNotification();
+
+  async function handleRunNotebook() {
+    try {
+      const retval = await startEmptyNotebook(virtualLabId, projectId);
+      notification.success({
+        message: `Notebook starting`,
+        key: 'notebook-started-successfully',
+        placement: 'topRight',
+      });
+      window.open(retval.url, '_blank');
+    } catch (error) {
+      // Just show the hint message if we get some error
+      if (error instanceof Error && 'cause' in error) {
+        notification.error({
+          message: (error.cause as { error_code: string; hint: string }).hint,
+          key: 'notebook-error',
+          placement: 'topRight',
+        });
+      } else {
+        notification.error({
+          message: `Failed to start notebook, unknown error: ${error}`,
+          key: 'notebook-unknown-error',
+          placement: 'topRight',
+        });
+      }
+    }
+  }
   return (
     <div>
-      <div className="mb-5 ml-5 flex">
-        <NextLink
-          href="public"
-          className={cn(
-            'flex h-[45px] min-w-[150px] items-center justify-center rounded-l-full px-4 py-2 text-white',
-            active === 'public' ? 'bg-primary-9 font-bold text-white' : 'text-primary-9 bg-white'
-          )}
-        >
-          Public
-        </NextLink>
+      <div className="mb-5 ml-5 flex justify-between">
+        <div className="flex">
+          <NextLink
+            href="public"
+            className={cn(
+              'flex h-[40px] min-w-[150px] items-center justify-center rounded-l-full px-4 py-2 text-white',
+              active === 'public' ? 'bg-primary-9 font-bold text-white' : 'text-primary-9 bg-white'
+            )}
+          >
+            Public
+          </NextLink>
 
-        <NextLink
-          href="private"
-          className={cn(
-            'flex h-[45px] min-w-[150px] items-center justify-center rounded-r-full px-4 py-2 text-white',
-            active === 'private' ? 'bg-primary-9 font-bold text-white' : 'text-primary-9 bg-white'
-          )}
+          <NextLink
+            href="private"
+            className={cn(
+              'flex h-[40px] min-w-[150px] items-center justify-center rounded-r-full px-4 py-2 text-white',
+              active === 'private' ? 'bg-primary-9 font-bold text-white' : 'text-primary-9 bg-white'
+            )}
+          >
+            Project
+          </NextLink>
+        </div>
+        <button
+          type="button"
+          className="flex h-[40px] items-center justify-between gap-2 rounded-full border border-[#F37726] bg-white px-5 text-[#F37726]"
+          onClick={handleRunNotebook}
         >
-          Project
-        </NextLink>
+          <div>Open JupyterHub</div>
+          <Image src="/images/jupyter.svg" alt="Jupyter hub" width={20} height={20} />
+        </button>
       </div>
 
       <div
