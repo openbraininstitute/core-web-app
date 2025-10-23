6494be8e4306c5f36ba240e606ff2fe25501ce14
Tolokoban <contact@tolokoban.org>
2025-10-23 18:01:10 +0200

    Added interactive view for ion channel recording plots

--- src/features/ion-channel-recording-viewer/index.tsx
+++ src/features/ion-channel-recording-viewer/index.tsx
@@ -2,6 +2,8 @@
 
 import dynamic from 'next/dynamic';
 
-export const EphysViewer = dynamic(() => import('./ion-channel-recording-viewer'), { ssr: false });
+export const IonChannelRecordingViewer = dynamic(() => import('./ion-channel-recording-viewer'), {
+  ssr: false,
+});
 
-export default EphysViewer;
+export default IonChannelRecordingViewer;
