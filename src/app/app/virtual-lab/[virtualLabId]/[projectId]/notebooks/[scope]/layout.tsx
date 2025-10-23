aea6d078b31a67854237e99190eb2812361efdad
Gil Arturo Barrios del Villar <gilarturo.barriosdelvillar@openbraininstitute.org>
2025-10-23 14:26:08 +0200

    highlight private

--- src/app/app/virtual-lab/[virtualLabId]/[projectId]/notebooks/[scope]/layout.tsx
+++ src/app/app/virtual-lab/[virtualLabId]/[projectId]/notebooks/[scope]/layout.tsx
@@ -1,20 +1,23 @@
-'use client';
-
 import type { ReactNode } from 'react';
+import { notFound } from 'next/navigation';
 // import { useNextStep } from 'nextstepjs';
 // import find from 'es-toolkit/compat/find';
 
 import { ServerSideComponentProp, WorkspaceContext } from '@/types/common';
 import { NotebooksLayout } from '@/ui/layouts/notebooks-layout';
+
 // import { notebookTour } from '@/ui/segments/app-setup/discover-app';
 // import { LeftMenu } from '@/ui/segments/notebooks/left-nav-menu';
 // import { NotebookHeader } from '@/ui/segments/notebooks/header';
 // import { useLocalStorage } from '@/hooks/use-local-storage';
 // import { AUTO_ONBOARDING_TOURS } from '@/constants';
 
-export default function Page({
+export default async function Page({
   children,
-}: ServerSideComponentProp<WorkspaceContext, null> & { children: ReactNode }) {
+  params: promisedParams,
+}: ServerSideComponentProp<WorkspaceContext & { scope: 'public' | 'private' }, null> & {
+  children: ReactNode;
+}) {
   // const { startNextStep } = useNextStep();
   // const [onboardingState] = useLocalStorage<{
   //   tours: Array<{
@@ -35,5 +38,9 @@ export default function Page({
   //   }
   // }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
-  return <NotebooksLayout active="public">{children}</NotebooksLayout>;
+  const params = await promisedParams;
+  const { scope } = params;
+  if (!['public', 'private'].includes(scope)) notFound();
+
+  return <NotebooksLayout active={scope}>{children}</NotebooksLayout>;
 }
