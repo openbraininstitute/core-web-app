de7be469d1a2de531f4e296f65712a6afbcaf6a0
Pavlo Getta <pavlo.getta@openbraininstitute.org>
2025-10-29 15:04:59 +0100

    Apply prettier

--- src/ui/segments/project/credits/job-report-list.tsx
+++ src/ui/segments/project/credits/job-report-list.tsx
@@ -56,11 +56,16 @@ const scaleLabel: Record<ServiceSubtype, string> = {
   [ServiceSubtype.MlRag]: 'ML',
 
   [SingleCellSimulation]: 'Single: Single neuron + extrinsic connectivity',
-  [PairCellSimulation]: 'Pair: Two connected neurons + intrinsic connectivity + extrinsic connectivity',
-  [SmallMicrocircuitSimulation]: 'Small: Microcircuit (3-20 neurons) + intrinsic connectivity + extrinsic connectivity',
-  [MicrocircuitSimulation]: 'Microcircuit: Any circuit larger than 20 neurons but not being a region, system, or whole-brain circuit',
-  [RegionSimulation]: 'Region: Atlas-based continuous volume of an entire brain region or a set of continuous sub-regions',
-  [SystemSimulation]: 'System: Non-continuous circuit consisting of at least two microcircuits/regions that are connected by inter-region connectivity',
+  [PairCellSimulation]:
+    'Pair: Two connected neurons + intrinsic connectivity + extrinsic connectivity',
+  [SmallMicrocircuitSimulation]:
+    'Small: Microcircuit (3-20 neurons) + intrinsic connectivity + extrinsic connectivity',
+  [MicrocircuitSimulation]:
+    'Microcircuit: Any circuit larger than 20 neurons but not being a region, system, or whole-brain circuit',
+  [RegionSimulation]:
+    'Region: Atlas-based continuous volume of an entire brain region or a set of continuous sub-regions',
+  [SystemSimulation]:
+    'System: Non-continuous circuit consisting of at least two microcircuits/regions that are connected by inter-region connectivity',
   [WholeBrainSimulation]: 'Circuit representing an entire brain',
 };
 
