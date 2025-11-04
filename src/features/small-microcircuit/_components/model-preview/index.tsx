53f0b8c42bfd65082177cb5443b86740acd102a3
Pavlo Getta <pavlo.getta@openbraininstitute.org>
2025-11-04 14:03:56 +0100

    Memoize preview component

--- src/features/small-microcircuit/_components/model-preview/index.tsx
+++ src/features/small-microcircuit/_components/model-preview/index.tsx
@@ -1,3 +1,4 @@
+import { memo } from 'react';
 import { match } from 'ts-pattern';
 
 import { EntityTypeDict, IMEModel } from '@/api/entitycore/types';
@@ -5,13 +6,13 @@ import { ICircuit } from '@/api/entitycore/types/entities/circuit';
 import { CircuitPreview } from '@/features/small-microcircuit/_components/model-preview/circuit-preview';
 import { NeuronVisualizer } from '@/ui/segments/workflows/simulate/single-neuron/shared/steps/neuron-visualizer';
 
-export default function ModelPreview({ model }: { model: ICircuit | IMEModel }) {
-  const sessionId = crypto.randomUUID();
-
+function ModelPreview({ model }: { model: ICircuit | IMEModel }) {
   return match(model.type)
     .with(EntityTypeDict.Circuit, () => <CircuitPreview circuit={model as ICircuit} />)
     .with(EntityTypeDict.Memodel, () => (
-      <NeuronVisualizer memodelId={model.id} sessionId={sessionId} />
+      <NeuronVisualizer memodelId={model.id} sessionId={model.id} />
     ))
     .otherwise(() => null);
 }
+
+export default memo(ModelPreview);
