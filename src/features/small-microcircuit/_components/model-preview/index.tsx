42e03cd6dbfb19b8b2c851de4008745d6f1ad775
Pavlo Getta <pavlo.getta@openbraininstitute.org>
2025-11-04 10:20:14 +0100

    Add me-model-circuit visualization

--- src/features/small-microcircuit/_components/model-preview/index.tsx
+++ src/features/small-microcircuit/_components/model-preview/index.tsx
@@ -3,10 +3,11 @@ import { match } from 'ts-pattern';
 import { EntityTypeDict, IMEModel } from '@/api/entitycore/types';
 import { ICircuit } from '@/api/entitycore/types/entities/circuit';
 import { CircuitPreview } from '@/features/small-microcircuit/_components/model-preview/circuit-preview';
+import { NeuronVisualizer } from '@/ui/segments/workflows/simulate/single-neuron/shared/steps/neuron-visualizer';
 
 export default function ModelPreview({ model }: { model: ICircuit | IMEModel }) {
   return match(model.type)
     .with(EntityTypeDict.Circuit, () => <CircuitPreview circuit={model as ICircuit} />)
-    .with(EntityTypeDict.Memodel, () => <div>ME Model</div>)
+    .with(EntityTypeDict.Memodel, () => <NeuronVisualizer sessionId="abc" memodelId={model.id} />)
     .otherwise(() => null);
 }
