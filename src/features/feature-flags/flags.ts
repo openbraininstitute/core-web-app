2071fe264cc7d218e634eae5fe926e2103b80db1
Pavlo Getta <pavlo.getta@openbraininstitute.org>
2025-11-10 14:24:33 +0100

    Enable unified single neuron circuit simulation by default

--- src/features/feature-flags/flags.ts
+++ src/features/feature-flags/flags.ts
@@ -1,9 +1,10 @@
 import { env } from '@/env';
 import { FlagDefinition } from '@/features/feature-flags/config';
 
+// TODO: Feature is considered to be ready for prod, flag to be removed
 export const unifiedSingleNeuronSimulationFlowFlag = {
   key: 'unifiedSingleNeuronSimulationFlow',
-  defaultValue: false,
+  defaultValue: true,
   description: 'Enable new single neuron unified (circuit) simulation flow with OBI-ONE',
   visible: ['local', 'development', 'staging'].includes(env.NEXT_PUBLIC_DEPLOYMENT_ENV),
 } satisfies FlagDefinition;
