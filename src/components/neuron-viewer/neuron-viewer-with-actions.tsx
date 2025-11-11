3175b45d1c5cd23cf6fe8c0e748bceb34ee5640a
Tolokoban <contact@tolokoban.org>
2025-11-11 11:39:52 +0100

    Fix: sysnapses were not stored if context was null

--- src/components/neuron-viewer/neuron-viewer-with-actions.tsx
+++ src/components/neuron-viewer/neuron-viewer-with-actions.tsx
@@ -49,7 +49,6 @@ export function NeuronViewerContainer({
   const [neuronViewerHoverData, setNeuronViewerOnHoverData] =
     useState<TNeuronViewerHoverData | null>(null);
   const newViewer = true;
-  console.log('🚀 [synapse-configuration] sessionId =', sessionId); // @FIXME: Remove this line written on 2025-11-11 at 11:17
 
   return (
     <ErrorBoundary
--- src/ui/segments/workflows/simulate/single-neuron/shared/steps/webgl-neuron-selector/painter/manager.ts
+++ src/ui/segments/workflows/simulate/single-neuron/shared/steps/webgl-neuron-selector/painter/manager.ts
@@ -278,7 +278,8 @@ export class PainterManager {
   showSynapses(synapses: Array<{ color: string; data: Float32Array }>) {
     this.synapses = synapses;
     const { context, groupSynapses } = this;
-    console.log('🚀 [manager] synapses, context =', synapses, context); // @FIXME: Remove this line written on 2025-11-11 at 11:08
+    this.synapses = synapses;
+    console.log('🚀 [manager] synapses, context.name =', synapses, context?.name); // @FIXME: Remove this line written on 2025-11-11 at 11:08
     if (!context) return;
 
     groupSynapses.removeAll();
