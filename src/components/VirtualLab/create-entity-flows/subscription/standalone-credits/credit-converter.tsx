ea6842f7f2a197d3431545a3cb9766fb29b918ad
Bilal MEDDAH <b_meddah@esi.dz>
2025-11-13 15:21:14 +0100

    fix conversion rate to .1chf==1credit (#1099)

--- src/components/VirtualLab/create-entity-flows/subscription/standalone-credits/credit-converter.tsx
+++ src/components/VirtualLab/create-entity-flows/subscription/standalone-credits/credit-converter.tsx
@@ -9,7 +9,7 @@ const formatInputValue = (value: number) => {
   return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, "'");
 };
 
-export const CONVERSION_RATE = 0.05;
+export const CONVERSION_RATE = 0.1;
 export const creditAtom = atom<{ credits: number; step: 'overview' | 'pay' | null }>({
   credits: 0,
   step: 'overview',
