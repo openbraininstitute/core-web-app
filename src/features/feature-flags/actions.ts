e12eaca1f42cdca00439f8ee9626701f7746a7fd
Pavlo Getta <pavlo.getta@openbraininstitute.org>
2025-11-21 14:46:47 +0100

    Apply prettier

--- src/features/feature-flags/actions.ts
+++ src/features/feature-flags/actions.ts
@@ -24,10 +24,7 @@ export async function getAllFlags(): Promise<FeatureFlags> {
   }
 }
 
-export async function setFlag<K extends keyof FeatureFlags>(
-  key: K,
-  value: FeatureFlags[K]
-) {
+export async function setFlag<K extends keyof FeatureFlags>(key: K, value: FeatureFlags[K]) {
   const cookieStore = await cookies();
   const flags = await getAllFlags();
 
