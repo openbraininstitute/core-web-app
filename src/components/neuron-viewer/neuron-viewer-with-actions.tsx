c7c0509e4af0eb76b538c1f89439b18ff4dfabae
Tolokoban <contact@tolokoban.org>
2025-12-12 10:10:17 +0100

    morphology is now an attribute of the viewer

--- src/components/neuron-viewer/neuron-viewer-with-actions.tsx
+++ src/components/neuron-viewer/neuron-viewer-with-actions.tsx
@@ -1,28 +1,53 @@
 'use client';
 
+import React from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
+import { useAtomValue } from 'jotai';
 
 import ReloadIcon from '../icons/Reload';
+import { NeuronLoader } from './plugins/neuron-loader';
 
 import { DefaultLoadingSuspense } from '@/components/DefaultLoadingSuspense';
 import { withErrorConfig } from '@/components/GenericErrorFallback';
 import { WebglNeuronSelector } from '@/ui/segments/workflows/simulate/single-neuron/shared/steps/webgl-neuron-selector';
+import { useCleanMorphology } from '@/ui/segments/workflows/simulate/single-neuron/shared/steps/hooks';
+import { logError } from '@/utils/logger';
+import {
+  SimulationStatus,
+  simulationStatusAtomFamily,
+} from '@/ui/segments/workflows/simulate/single-neuron/shared/context';
 
 import styles from './neuron-viewer-with-actions.module.css';
 
 type Props = {
   meModelId: string;
+  sessionId: string;
   disableElectrodes?: boolean;
   disableSynapses?: boolean;
-  sessionId: string;
 };
 
 export function NeuronViewerContainer({
   meModelId,
+  sessionId,
   disableElectrodes,
   disableSynapses,
-  sessionId,
 }: Props) {
+  const simulationStatus = useAtomValue(simulationStatusAtomFamily(sessionId));
+  const { loading, error, morphology } = useCleanMorphology(meModelId, sessionId);
+
+  if (error) {
+    logError('Unable to load morphology:', error);
+    throw new Error('Unable to load morphology!');
+  }
+
+  if (loading) {
+    return (
+      <div className={styles.center}>
+        <NeuronLoader text="Loading Neuron" />
+      </div>
+    );
+  }
+
   return (
     <ErrorBoundary
       FallbackComponent={withErrorConfig({
@@ -43,10 +68,11 @@ export function NeuronViewerContainer({
     >
       <DefaultLoadingSuspense>
         <WebglNeuronSelector
+          morphology={morphology}
+          sessionId={sessionId}
           disableElectrodes={disableElectrodes}
           disableSynapses={disableSynapses}
-          meModelId={meModelId}
-          sessionId={sessionId}
+          disableClick={simulationStatus?.status === SimulationStatus.LAUNCHED}
         />
       </DefaultLoadingSuspense>
     </ErrorBoundary>
