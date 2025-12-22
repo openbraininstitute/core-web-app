d53b6e46a03ca580c118e3f5e99467bbdee5274b
Pavlo Getta <pavlo.getta@openbraininstitute.org>
2025-12-22 14:34:46 +0100

    Revert some other files 2

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,4 +1,4 @@
-import { getGalleryContent } from '@/services/sanity/content/gallery';
+import { getGalleryContent } from '@/api/sanity/gallery/route';
 import GalleryPage from '@/ui/segments/gallery';
 
 export const dynamic = 'force-dynamic';
