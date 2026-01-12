a650edfe476d23d18fb22e03abff7a44195e4783
Gil Arturo Barrios del Villar <gilarturo.barriosdelvillar@openbraininstitute.org>
2026-01-12 11:35:54 +0100

    style

--- src/features/scan-config/components/root-element.tsx
+++ src/features/scan-config/components/root-element.tsx
@@ -201,7 +201,7 @@ export function RootElement({
                     }
                   }}
                 >
-                  <div className="w-full">
+                  <div className="w-full text-left">
                     {isSelected && isEditingKey && !readOnly && (
                       <>
                         <Input
