2457a4f60d40829ce584ca24617aafc21cfeffdf
Boris Bergsma <boris.bergsma@openbraininstitute.org>
2026-01-21 15:54:31 +0100

    small UI change

--- src/components/ai-assistant/icons/new-chat.tsx
+++ src/components/ai-assistant/icons/new-chat.tsx
@@ -6,12 +6,14 @@ export function IconNewChat() {
       viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     >
       <title>New Chat</title>
-      <path
-        d="M3 17.25V21H6.75L17.81 9.94L14.06 6.19L3 17.25ZM20.71 7.04C21.1 6.65 21.1 6.02 20.71 5.63L18.37 3.29C17.98 2.9 17.35 2.9 16.96 3.29L15.13 5.12L18.88 8.87L20.71 7.04Z"
-        fill="currentColor"
-      />
+      <path d="M17 3a2.85 2.83 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5Z" />
+      <path d="m15 5 4 4" />
     </svg>
   );
 }
