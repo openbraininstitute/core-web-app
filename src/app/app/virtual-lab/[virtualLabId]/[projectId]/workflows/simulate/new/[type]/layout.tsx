a2dfab27b178762d398da583b3fe0eeec71a7c4b
bilalesi <b_meddah@esi.dz>
2026-01-23 13:36:47 +0100

    move scope selector outside the frame in workflow/simulate

--- src/app/app/virtual-lab/[virtualLabId]/[projectId]/workflows/simulate/new/[type]/layout.tsx
+++ src/app/app/virtual-lab/[virtualLabId]/[projectId]/workflows/simulate/new/[type]/layout.tsx
@@ -2,10 +2,10 @@
 
 import { motion } from 'motion/react';
 import type { ReactNode } from 'react';
-
-import { useMiniDetailView, useSelectEntityClickEvent } from '@/ui/segments/mini-detail-view/event';
-import { SimulateWorkflowsBreadcrumb } from '@/ui/segments/workflows/elements/simulate-breadcrumb';
 import { useDisableElementOverflow } from '@/ui/hooks/use-disable-element-overflow';
+import { useMiniDetailView, useSelectEntityClickEvent } from '@/ui/segments/mini-detail-view/event';
+import { WorkflowScopeTabs } from '@/ui/segments/workflows/elements/scope-selector';
+import { SimulateWorkflowsBreadcrumb } from '@/ui/segments/workflows/elements/simulate-breadcrumb';
 import { cn } from '@/utils/css-class';
 
 export default function Layout({ children }: { children: ReactNode }) {
@@ -16,31 +16,34 @@ export default function Layout({ children }: { children: ReactNode }) {
   });
 
   return (
-    <div className="bg-background border-neutral-2 mx-2 h-full max-h-[calc(100vh-6rem)] w-[calc(100%-10px)] overflow-hidden rounded-2xl border">
-      <SimulateWorkflowsBreadcrumb />
-      <motion.div
-        id="workflow-new-inner-layout"
-        className={cn(
-          'grid gap-2 [grid-area:main]',
-          'h-full max-h-[calc(100%-4rem)] px-3 py-2',
-          { "grid-cols-1 [grid-template-areas:'body']": !mdv },
-          { "grid-cols-[3fr_2fr] [grid-template-areas:'body_mini-view']": mdv }
-        )}
-        initial={false}
-        animate={{
-          gridTemplateColumns: mdv ? '3fr 2fr' : '1fr',
-          gridTemplateAreas: mdv ? "'body mini-view'" : "'body'",
-        }}
-        transition={{
-          type: 'spring',
-          stiffness: 320,
-          damping: 30,
-          mass: 0.6,
-        }}
-        style={{ willChange: 'grid-template-columns, grid-template-areas' }}
-      >
-        {children}
-      </motion.div>
+    <div className="h-full mx-2 flex flex-col max-h-[calc(100vh-6rem)] w-[calc(100%-10px)] overflow-hidden">
+      <WorkflowScopeTabs className="max-w-max mb-3" />
+      <div className="bg-background border-neutral-2 h-full  overflow-hidden rounded-2xl border">
+        <SimulateWorkflowsBreadcrumb />
+        <motion.div
+          id="workflow-new-inner-layout"
+          className={cn(
+            'grid gap-2 [grid-area:main]',
+            'h-full max-h-[calc(100%-4rem)] px-3 py-2',
+            { "grid-cols-1 [grid-template-areas:'body']": !mdv },
+            { "grid-cols-[3fr_2fr] [grid-template-areas:'body_mini-view']": mdv }
+          )}
+          initial={false}
+          animate={{
+            gridTemplateColumns: mdv ? '3fr 2fr' : '1fr',
+            gridTemplateAreas: mdv ? "'body mini-view'" : "'body'",
+          }}
+          transition={{
+            type: 'spring',
+            stiffness: 320,
+            damping: 30,
+            mass: 0.6,
+          }}
+          style={{ willChange: 'grid-template-columns, grid-template-areas' }}
+        >
+          {children}
+        </motion.div>
+      </div>
     </div>
   );
 }
