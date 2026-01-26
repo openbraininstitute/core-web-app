ae1f4e3fa4efa794c049b01845853725baef6e2b
Tolokoban <contact@tolokoban.org>
2026-01-26 13:59:27 +0100

    Fix the bbox when there is no dendrite

--- src/ui/segments/workflows/simulate/single-neuron/shared/steps/webgl-neuron-selector/painter/manager.ts
+++ src/ui/segments/workflows/simulate/single-neuron/shared/steps/webgl-neuron-selector/painter/manager.ts
@@ -428,34 +428,6 @@ export class PainterManager {
     }
     return tgdCalcMapRange(normalizedZoom, 0, +1, 1, maxZoom, true);
   }
-
-  private makeHoverPainter(item: StructureItem): TgdPainter | null {
-    const { context } = this;
-    if (!context) return null;
-
-    const segments = new TgdPainterSegmentsData();
-    const uv: ArrayNumber2 = [
-      (StructureItemType.Selected + 0.5) / (StructureItemType.Unknown + 1),
-      0,
-    ];
-    const radius = item.radius * 1.5;
-    segments.add([...item.start, radius], [...item.end, radius], uv, uv);
-
-    return new TgdPainterSegments(context, {
-      roundness: 32,
-      minRadius: 2,
-      makeDataset: segments.makeDataset,
-      material: new TgdMaterialDiffuse({
-        color: [0.8, 0.6, 0.3, 1],
-        specularExponent: 1,
-        specularIntensity: 0.5,
-        lockLightsToCamera: true,
-        light: new TgdLight({
-          direction: new TgdVec3(0, 0, -1),
-        }),
-      }),
-    });
-  }
 }
 
 export function usePainterManager(morphology: Morphology) {
