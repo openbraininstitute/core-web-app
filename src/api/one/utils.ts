803afc677d9c44175f2643de8b8b7a8e2dbc7f2d
Daniel Keller <dkeller@mac.home>
2026-01-27 13:26:31 +0100

    reverted utils.ts

--- src/api/one/utils.ts
+++ src/api/one/utils.ts
@@ -1,17 +1,7 @@
-//import { authApiClient } from '@/api/apiClient';
-//import { config } from '@/config';
-
-//export async function obioneApi(url?: string) {
-//  const api = await authApiClient(url ?? config.OBI_ONE_URL);
-//  return api;
-//}
-
 import { authApiClient } from '@/api/apiClient';
 import { config } from '@/config';
 
-const obioneUrl = 'http://127.0.0.1:8100';
-
 export async function obioneApi(url?: string) {
-  const api = await authApiClient(url ?? obioneUrl);
+  const api = await authApiClient(url ?? config.OBI_ONE_URL);
   return api;
 }
