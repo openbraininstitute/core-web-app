41f691cae38e6d6c98ed6078837e7dfdb002e7e6
Pavlo Getta <pavlo.getta@openbraininstitute.org>
2026-01-28 13:19:34 +0100

    Fix linter

--- src/entity-configuration/domain/simulation/memodel-circuit-simulation.ts
+++ src/entity-configuration/domain/simulation/memodel-circuit-simulation.ts
@@ -127,6 +127,9 @@ export async function resolveSimulationByCampaignId({
   context: WorkspaceContext | undefined;
 }) {
   const campaign = await getCircuitSimulationCampaign({ id, context });
+  if (!campaign) {
+    throw new Error(`Campaign with id ${id} not found`);
+  }
   const source = await getCircuitSimulations({ context, filters: { simulation_campaign_id: id } });
 
   const simulation = source.data.at(0);
@@ -139,7 +142,7 @@ export async function resolveSimulationByCampaignId({
   if (!configAsset) throw Error('No campaign config asset found');
 
   const rawConfig = await downloadAsset({
-    entityId: campaign?.id!,
+    entityId: campaign.id,
     entityType: EntityTypeDict.SimulationCampaign,
     id: configAsset?.id,
     ctx: context,
