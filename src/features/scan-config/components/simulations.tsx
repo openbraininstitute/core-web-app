3d9ae9709275e9f6a86a2e8f568c7c59f7038b60
Pavlo Getta <pavlo.getta@openbraininstitute.org>
2026-01-28 15:32:37 +0100

    Fix linting

--- src/features/scan-config/components/simulations.tsx
+++ src/features/scan-config/components/simulations.tsx
@@ -208,7 +208,7 @@ export default function SimulationsTab({
         ctx: { virtualLabId, projectId },
         simulationIds: simIds,
         onInit: () => {
-          simIds.forEach((simId) => setSimStatus(simId, EntitycoreExecutionStatus.PENDING));
+          simIds.forEach((simId) => void setSimStatus(simId, EntitycoreExecutionStatus.PENDING));
           setSelectedSimulationIds([]);
           setSimRequestInProgress(false);
         },
