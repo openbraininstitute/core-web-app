516ecb2668c1c64d1ca8fa599ddd6c01a886fe27
Gil Arturo Barrios del Villar <gilarturo.barriosdelvillar@openbraininstitute.org>
2026-02-02 10:05:25 +0100

    move to components

---
+++ src/features/scan-config/components/block-dictionary-entries.tsx
@@ -0,0 +1,377 @@
+import {
+  CheckCircleFilled,
+  CheckOutlined,
+  CloseOutlined,
+  DeleteOutlined,
+  EditOutlined,
+  PlusCircleOutlined,
+  WarningFilled,
+} from '@ant-design/icons';
+import type { ErrorObject } from 'ajv';
+import { Input } from 'antd';
+import isEqual from 'es-toolkit/compat/isEqual';
+import { atom } from 'jotai';
+import type React from 'react';
+import { classNames } from '@/util/utils';
+import { cn } from '@/utils/css-class';
+import type { AtomsMap } from '../types';
+import type { Config, ConfigValue } from './components';
+
+import { isAtom, isPlainObject } from './utils';
+
+export default function BlockDictionaryEntries({
+  config,
+  aiConfig,
+  rootElement,
+  selectedEntry,
+  selectedRootElement,
+  handleEntryClick,
+  campaignId,
+  loading,
+  readOnly,
+  isChatReady,
+  setEditing,
+  setSelectedEntry,
+  singularName,
+  allEntries,
+  newKey,
+  setNewKey,
+  isEditingKey,
+  setIsEditingKey,
+  atomsMap,
+  setAtomsMap,
+  errors,
+}: {
+  config: Config;
+  aiConfig: Config | null;
+  rootElement: string;
+  selectedEntry: string;
+  selectedRootElement: string;
+  handleEntryClick: (entry: string) => void;
+  campaignId: string;
+  loading: boolean;
+  readOnly: boolean;
+  isChatReady: boolean;
+  setEditing: (editing: boolean) => void;
+  setSelectedEntry: (entry: string) => void;
+  singularName: string;
+  allEntries: Set<string>;
+  newKey: string;
+  setNewKey: (k: string) => void;
+  isEditingKey: boolean;
+  setIsEditingKey: (k: boolean) => void;
+  atomsMap: AtomsMap;
+  setAtomsMap: React.Dispatch<React.SetStateAction<AtomsMap>>;
+  errors: ErrorObject<string, Record<string, any>, unknown>[] | null | undefined;
+}) {
+  const newKeyError = allEntries.has(newKey) || !newKey || newKey === selectedEntry;
+
+  const onNameChangeConfirm = (
+    e: React.MouseEvent<HTMLSpanElement, MouseEvent> | React.KeyboardEvent<HTMLInputElement>
+  ) => {
+    e.stopPropagation();
+    if (newKeyError) return;
+    const selectedTabAtoms = atomsMap[selectedRootElement];
+    if (newKey === selectedEntry) return;
+    if (isAtom(selectedTabAtoms)) return;
+    if (!isPlainObject(config[selectedRootElement])) return;
+
+    allEntries.delete(selectedEntry);
+    allEntries.add(newKey);
+
+    selectedTabAtoms[newKey] = selectedTabAtoms[selectedEntry];
+    delete selectedTabAtoms[selectedEntry];
+
+    // Rename references
+
+    // Initialize case
+    const configInitialize = config.initialize;
+    if (
+      isPlainObject(configInitialize) &&
+      isPlainObject(configInitialize.node_set) &&
+      typeof configInitialize.node_set.block_name === 'string' &&
+      configInitialize.node_set.block_name === selectedEntry
+    ) {
+      atomsMap.initialize = atom<Record<string, ConfigValue>>({
+        ...configInitialize,
+        node_set: { ...configInitialize.node_set, block_name: newKey },
+      });
+    }
+
+    // Check all keys in the config
+    Object.entries(config)
+      .filter(([configK]) => configK !== 'initialize')
+      .forEach(([configK, configV]) => {
+        if (typeof configV !== 'object') return;
+
+        // Check all keys in a section (e.g stimuli, recordings)
+        Object.entries(configV).forEach(([_, entryV]) => {
+          if (!isPlainObject(entryV)) return;
+
+          // Check all values in a particular object (a single stimuli, a single timestamp, etc)
+          Object.entries(entryV).forEach(([fieldK, field]) => {
+            if (
+              !isPlainObject(entryV) ||
+              !isPlainObject(field) ||
+              typeof field.block_name !== 'string' ||
+              isAtom(atomsMap[configK]) || // skip top level atoms (e.g initialize)
+              field.block_name !== selectedEntry
+            )
+              return;
+
+            // Renaming the reference to current object
+            entryV[fieldK].block_name = newKey;
+          });
+        });
+      });
+
+    setAtomsMap({ ...atomsMap });
+
+    setIsEditingKey(false);
+    setSelectedEntry(newKey);
+    setNewKey('');
+  };
+
+  function renderBlockTab(entry: string) {
+    const isSelected = selectedRootElement === rootElement && entry === selectedEntry;
+
+    return (
+      <button
+        type="button"
+        key={entry}
+        className={classNames(
+          'text-primary-8 flex h-[50px] min-h-[50px] w-[90%] min-w-[150px] items-center justify-between rounded-full bg-gray-100 px-5 py-2 text-sm drop-shadow hover:bg-gradient-to-r hover:from-[#003A8C] hover:to-[#001026] hover:text-white',
+          isSelected ? 'bg-gradient-to-r from-[#003A8C] to-[#001026] text-white' : ''
+        )}
+        tabIndex={0}
+        onClick={() => handleEntryClick(entry)}
+        onKeyDown={(evt) => {
+          if (evt.key === ' ' || evt.key === 'Enter') {
+            handleEntryClick(entry);
+          }
+        }}
+      >
+        <div className="w-full text-left"> {entry}</div>
+      </button>
+    );
+  }
+
+  return (
+    <>
+      {Object.entries(config[rootElement])
+        .filter(([block_key, block_schema]) => {
+          if (!aiConfig) return true;
+          if (!isPlainObject(aiConfig[rootElement])) return true;
+          return isEqual(block_schema, aiConfig[rootElement][block_key]);
+        })
+        .map(([subkey]) => {
+          const isSelected = selectedRootElement === rootElement && subkey === selectedEntry;
+
+          return (
+            <button
+              type="button"
+              key={subkey}
+              className={classNames(
+                'text-primary-8 flex h-[50px] min-h-[50px] w-[90%] min-w-[150px] items-center justify-between rounded-full bg-gray-100 px-5 py-2 text-sm drop-shadow hover:bg-gradient-to-r hover:from-[#003A8C] hover:to-[#001026] hover:text-white',
+                isSelected ? 'bg-gradient-to-r from-[#003A8C] to-[#001026] text-white' : ''
+              )}
+              tabIndex={0}
+              onClick={() => handleEntryClick(subkey)}
+              onKeyDown={(evt) => {
+                if (evt.key === ' ' || evt.key === 'Enter') {
+                  handleEntryClick(subkey);
+                }
+              }}
+            >
+              <div className="w-full text-left">
+                {isSelected && isEditingKey && !readOnly && (
+                  <>
+                    <Input
+                      value={newKey}
+                      className="inline-block h-[20px] w-[70%] text-sm outline-none"
+                      classNames={{
+                        input: 'border-none !bg-transparent text-white',
+                      }}
+                      ref={(element) => element?.focus()}
+                      onKeyDown={(e) => {
+                        e.stopPropagation();
+                        if (e.key === 'Enter') {
+                          onNameChangeConfirm(e);
+                        }
+                      }}
+                      onClick={(e) => e.stopPropagation()}
+                      onChange={(v) => {
+                        v.stopPropagation();
+                        setNewKey(v.currentTarget.value);
+                      }}
+                      status={newKeyError ? 'error' : undefined}
+                      size="small"
+                    />
+                    <div className="ml-3 inline-block">
+                      <CheckOutlined
+                        className={cn('mr-2', newKeyError && 'opacity-30')}
+                        disabled={newKeyError}
+                        onClick={onNameChangeConfirm}
+                      />
+
+                      <CloseOutlined
+                        onClick={() => {
+                          setIsEditingKey(false);
+                          setNewKey('');
+                        }}
+                      />
+                    </div>
+                  </>
+                )}
+
+                {(!isSelected || (isSelected && !isEditingKey)) && (
+                  <>
+                    {subkey}
+                    {!readOnly && !campaignId && !aiConfig && isChatReady && (
+                      <EditOutlined
+                        className="ml-3"
+                        onClick={(e) => {
+                          e.stopPropagation();
+                          setSelectedEntry(subkey);
+                          setIsEditingKey(true);
+                          setNewKey(subkey);
+                        }}
+                      />
+                    )}
+                  </>
+                )}
+              </div>
+
+              <div className="flex gap-2">
+                {errors?.find((error) =>
+                  error.instancePath.startsWith(`/${rootElement}/${subkey}`)
+                ) ? (
+                  <WarningFilled className="text-yellow-400" />
+                ) : (
+                  <CheckCircleFilled className="text-green-600" />
+                )}
+
+                {!campaignId && !loading && !readOnly && isChatReady && !aiConfig && (
+                  <DeleteOutlined
+                    className="cursor-pointer"
+                    onClick={(e) => {
+                      e.stopPropagation();
+
+                      setEditing(false);
+
+                      const selectedTabAtoms = atomsMap[selectedRootElement];
+
+                      if (!isAtom(selectedTabAtoms)) {
+                        delete selectedTabAtoms[subkey];
+
+                        // Initialize case
+                        const configInitialize = config.initialize;
+                        if (
+                          isPlainObject(configInitialize) &&
+                          isPlainObject(configInitialize.node_set) &&
+                          typeof configInitialize.node_set.block_name === 'string' &&
+                          configInitialize.node_set.block_name === subkey
+                        ) {
+                          atomsMap.initialize = atom<Record<string, ConfigValue>>({
+                            ...configInitialize,
+                            node_set: null,
+                          });
+                        }
+
+                        // Check all keys in the config
+                        Object.entries(config)
+                          .filter(([configK]) => configK !== 'initialize')
+                          .forEach(([configK, configV]) => {
+                            if (typeof configV !== 'object') return;
+
+                            // Check all keys in a section (e.g stimuli, recordings)
+                            Object.entries(configV).forEach(([entryKey, entryV]) => {
+                              if (!isPlainObject(entryV)) return;
+
+                              // Check all values in a particular object (a single stimuli, a single timestamp, etc)
+                              Object.entries(entryV).forEach(([fieldK, field]) => {
+                                if (
+                                  !isPlainObject(entryV) ||
+                                  !isPlainObject(field) ||
+                                  typeof field.block_name !== 'string' ||
+                                  isAtom(atomsMap[configK]) || // skip top level atoms (e.g initialize)
+                                  field.block_name !== subkey
+                                )
+                                  return;
+
+                                // Deleting the reference to current object
+
+                                delete entryV[fieldK]; //eslint-disable-line
+
+                                // The atom that has a reference to current object
+                                atomsMap[configK][entryKey] =
+                                  atom<Record<string, ConfigValue>>(entryV);
+                              });
+                            });
+                          });
+
+                        setAtomsMap({
+                          ...atomsMap,
+                          [selectedRootElement]: {
+                            ...selectedTabAtoms,
+                          },
+                        });
+                      }
+
+                      setSelectedEntry('');
+                      allEntries.delete(subkey);
+                    }}
+                  />
+                )}
+              </div>
+            </button>
+          );
+        })}
+
+      {/* AI suggested changes */}
+
+      {!!aiConfig && !campaignId && (
+        <div className="border-red-500 border-2">
+          {Object.entries(config[rootElement])
+            .filter(([block_key, block_schema]) => {
+              if (!aiConfig) return false;
+              if (!isPlainObject(aiConfig[rootElement])) return false;
+              const blockAIConfig = aiConfig[rootElement][block_key];
+              return !!blockAIConfig && !isEqual(block_schema, blockAIConfig);
+            })
+            .map(([entry]) => renderBlockTab(entry))}
+
+          {Object.entries(config[rootElement])
+            .filter(([block_key, _]) => {
+              if (!aiConfig) return false;
+              if (!isPlainObject(aiConfig[rootElement])) return false;
+              const blockAIConfig = aiConfig[rootElement][block_key];
+              return !blockAIConfig;
+            })
+            .map(([entry]) => renderBlockTab(entry))}
+
+          {Object.entries(aiConfig[rootElement])
+            .filter(([block_key, _]) => {
+              if (!isPlainObject(config[rootElement])) return false;
+              const currentBlockConfig = config[rootElement][block_key];
+              return !currentBlockConfig;
+            })
+            .map(([entry]) => renderBlockTab(entry))}
+        </div>
+      )}
+      {!campaignId && !loading && !readOnly && isChatReady && !aiConfig && (
+        <button
+          className="text-primary-8 flex h-[50px] min-h-[50px] w-[90%] min-w-[150px] items-center justify-between rounded-full bg-gray-100 px-5 py-2 text-sm drop-shadow"
+          type="button"
+          onClick={() => {
+            setEditing(true);
+            setSelectedEntry('');
+          }}
+        >
+          Add {singularName}
+          <PlusCircleOutlined />
+        </button>
+      )}
+    </>
+  );
+}
