65b449951c4181c140bbc6d3b1a7c0a4401121f4
Bilal MEDDAH <b_meddah@esi.dz>
2026-02-10 13:19:32 +0100

    fix: keyBuilderHierarchy.hierarchies Query data cannot be undefined (#1371)

--- src/app/app/virtual-lab/[virtualLabId]/[projectId]/layout.tsx
+++ src/app/app/virtual-lab/[virtualLabId]/[projectId]/layout.tsx
@@ -32,6 +32,7 @@ export default async function Layout({ children }: Props) {
             gcTime: Infinity,
           });
         });
+      return result;
     },
     staleTime: Infinity,
     gcTime: Infinity,
