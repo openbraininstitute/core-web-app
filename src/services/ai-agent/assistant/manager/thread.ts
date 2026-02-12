f45a6bf104b57faaeaf9e221d8244ca36ba18597
Boris Bergsma <boris.bergsma@openbraininstitute.org>
2026-02-12 10:18:20 +0100

    new chat on reload

--- src/services/ai-agent/assistant/manager/thread.ts
+++ src/services/ai-agent/assistant/manager/thread.ts
@@ -1,5 +1,5 @@
-import { sharedSessionStorage } from '@/util/shared-session-storage';
-import { serviceAiAgentThreadCreate, serviceAiAgentThreadExists } from '../../api';
+import { serviceAiAgentThreadCreate } from '../../api';
+
 import type { Signal } from '../signal';
 import type { AssistantContext } from '../types';
 
@@ -13,20 +13,11 @@ export class ThreadManager {
   ) {}
 
   /**
-   * The first time we open the AI Assistant, we try to get the
-   * last recently used thread stored in session storage.
-   * If no such thread exists, we create a new thread.
+   * The first time we open the AI Assistant, we create a new thread.
    */
   readonly init = async (context: AssistantContext) => {
     this.context = context;
-    const { target } = this;
-    const { accessToken } = context;
-    const sessionThreadId = await this.recoverLastThreadId(accessToken);
-    if (sessionThreadId) {
-      target.threadId.set(sessionThreadId);
-    } else {
-      await this.createThread();
-    }
+    await this.createThread();
   };
 
   readonly createThread = async () => {
@@ -40,16 +31,6 @@ export class ThreadManager {
     const thread = await serviceAiAgentThreadCreate(params);
     const { threadId } = thread;
     target.threadId.set(threadId);
-    sharedSessionStorage.setItem('AI-Assistant/threadId', threadId);
     return threadId;
   };
-
-  private async recoverLastThreadId(accessToken: string): Promise<string | null> {
-    const sessionThreadId = sharedSessionStorage.getItem('AI-Assistant/threadId') ?? '';
-    if (!sessionThreadId) return null;
-
-    // Check thread existence.
-    const exists = await serviceAiAgentThreadExists({ accessToken, threadId: sessionThreadId });
-    return exists ? sessionThreadId : null;
-  }
 }
