76393a1db4206c8f9314b6e62f23ca74cc1009ee
Pavlo Getta <pavlo.getta@openbraininstitute.org>
2026-02-17 14:39:21 +0100

    Rename process_data to process

--- src/ui/segments/workflows/elements/helpers.tsx
+++ src/ui/segments/workflows/elements/helpers.tsx
@@ -396,7 +396,7 @@ export const ActivityDict: readonly ActivityDictEntry[] = [
   },
   {
     label: 'Process Data',
-    value: WorkflowActivityDictValue.processData,
+    value: WorkflowActivityDictValue.process,
     disabled: false,
     name: 'Processing Data',
     configType: 'process',
@@ -455,7 +455,7 @@ export function getDropdownOptionsByCategory(
 
     return { allOptions: options, enabledOptions };
   }
-  if (category === WorkflowActivityDictValue.processData) {
+  if (category === WorkflowActivityDictValue.process) {
     const grouped = groupBy(processActivitiesConfiguration, 'group');
     const options = Object.entries(grouped).map(([k, v]) => {
       return {
@@ -551,7 +551,7 @@ export function getAllOptionsOrdered(
     });
   }
 
-  if (category === WorkflowActivityDictValue.processData) {
+  if (category === WorkflowActivityDictValue.process) {
     return processNewConfiguration;
   }
 
