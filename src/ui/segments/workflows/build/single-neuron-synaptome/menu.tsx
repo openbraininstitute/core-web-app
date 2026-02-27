c9d7f85affbcfd2e9a5b91f8835ecc96edffca03
Tolokoban <contact@tolokoban.org>
2026-02-27 10:55:30 +0100

    Fixed disabled apply button

--- src/ui/segments/workflows/build/single-neuron-synaptome/menu.tsx
+++ src/ui/segments/workflows/build/single-neuron-synaptome/menu.tsx
@@ -1,27 +1,12 @@
 'use client';
 
-import { useRouter } from '@bprogress/next';
-import { useMutation, useQueryClient } from '@tanstack/react-query';
-import delay from 'es-toolkit/compat/delay';
-import kebabCase from 'es-toolkit/compat/kebabCase';
 import { useSearchParams } from 'next/navigation';
-import { z } from 'zod';
 
-import { SingleNeuronSynaptomeConfigurationSchema } from '@/api/entitycore/types/entities/single-neuron-synaptome';
-import { ExtendedEntitiesTypeDict } from '@/api/entitycore/types/extended-entity-type';
-import { createSingleNeuronSynaptome } from '@/api/small-scale-simulator';
-import { tryCatch } from '@/api/utils';
-import { useAppNotification } from '@/components/notification';
-import { config } from '@/config';
-import { messages } from '@/i18n/en/synaptome';
 import { useDefaultBreakpoint } from '@/ui/hooks/create-break-point';
-import { useWorkspace } from '@/ui/hooks/use-workspace';
 import {
   BuildStep,
   useBuildSingleNeuronSynaptomeSessionState,
 } from '@/ui/segments/workflows/build/single-neuron-synaptome/helpers';
-import { ActivityValues } from '@/ui/segments/workflows/elements/helpers';
-import { keyBuilder } from '@/ui/use-query-keys/workspace';
 
 import { useStepChangeHandler } from './hooks';
 import SectionBuildSynaptomeButton from './sections/section-build-synaptome-button';
