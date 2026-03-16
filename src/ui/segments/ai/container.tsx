fb0759cb43b35f1f729bf23a9b436402f77b38e4
Boris Bergsma <boris.bergsma@openbraininstitute.org>
2026-03-16 15:03:45 +0100

    small change and remove side accent

--- src/ui/segments/ai/container.tsx
+++ src/ui/segments/ai/container.tsx
@@ -76,7 +76,7 @@ export function Container() {
               margin: 0,
             }}
           >
-            AI Assistant
+            OBI Assistant
           </div>
         </button>
       ) : (
