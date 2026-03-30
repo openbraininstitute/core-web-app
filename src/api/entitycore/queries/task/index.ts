ebfae81fdfb60147282c17fca01493fc8bc86315
Bilal MEDDAH <b_meddah@esi.dz>
2026-03-30 14:12:20 +0200

    add task-config and task-activity endpoints (#1546)

---
+++ src/api/entitycore/queries/task/index.ts
@@ -0,0 +1,14 @@
+export {
+  createTaskActivity,
+  deleteTaskActivity,
+  getTaskActivities,
+  getTaskActivity,
+  updateTaskActivity,
+} from '@/api/entitycore/queries/task/task-activity';
+export {
+  createTaskConfig,
+  deleteTaskConfig,
+  getTaskConfig,
+  getTaskConfigs,
+  updateTaskConfig,
+} from '@/api/entitycore/queries/task/task-config';
