90b6347fbf05b7242f01cfcc1486d3d7015b207f
bilalesi <b_meddah@esi.dz>
2026-04-06 11:20:45 +0200

    enable experimental view transition

--- next.config.ts
+++ next.config.ts
@@ -34,6 +34,7 @@ const config = {
   },
   productionBrowserSourceMaps: !isPreviewBuild,
   experimental: {
+    viewTransition: true,
     turbopackFileSystemCacheForDev: true,
     serverSourceMaps: !isPreviewBuild,
   },
