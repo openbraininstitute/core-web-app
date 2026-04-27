adf9300dc605138b0d5cb504bd02817b8ed63f91
James Isbister <isbisterjb@gmail.com>
2026-04-27 19:32:41 +0200

    Working changes

--- src/ui/segments/project/get-started/elements/news-view.tsx
+++ src/ui/segments/project/get-started/elements/news-view.tsx
@@ -62,7 +62,7 @@ export function NewsView() {
   );
 
   return (
-    <div className="border-neutral-2 bg-background flex h-full max-h-full w-full flex-col overflow-hidden rounded-2xl border p-4">
+    <div className="flex h-full max-h-full w-full flex-col overflow-hidden">
       {items.length ? (
         <div className="flex w-full flex-col gap-4 overflow-y-auto pr-2">
           {items.map((item) => (
