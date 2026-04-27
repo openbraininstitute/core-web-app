73681ac03bcf7def175694c9f78e4b087bbc3241
James Isbister <isbisterjb@gmail.com>
2026-04-27 18:29:58 +0200

    Working changes

--- src/ui/segments/help/glossary/index.tsx
+++ src/ui/segments/help/glossary/index.tsx
@@ -78,7 +78,7 @@ export default function GlossarySection() {
   }
 
   return (
-    <div className="border-neutral-2 bg-background flex h-full max-h-[82vh] w-full overflow-hidden rounded-2xl border p-4">
+    <div className="border-neutral-2 bg-background mb-32 flex h-full max-h-[calc(100vh-18rem)] w-full overflow-hidden rounded-2xl border p-4">
       <div className="border-neutral-2 w-1/4 shrink-0 overflow-y-auto border-r pr-4">
         <GlossaryNavigation glossarySectionsTypes={glossarySectionsTypes} />
       </div>
