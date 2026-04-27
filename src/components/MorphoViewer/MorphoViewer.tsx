314c29f3fd276fe9eefb4b0de98bcc2ae9ee90a1
Tolokoban <contact@tolokoban.org>
2026-04-27 15:11:44 +0200

    Fix missing colors

--- src/components/MorphoViewer/MorphoViewer.tsx
+++ src/components/MorphoViewer/MorphoViewer.tsx
@@ -26,12 +26,11 @@ interface MorphoViewerProps {
    * Text content of a SWC file.
    */
   swc: string;
-  mode?: 'light' | 'dark';
   // We disable enhanced somas until they are fixed on the backend.
   // contentUrl?: string;
 }
 
-function MorphoViewerComponent({ className, swc, mode }: MorphoViewerProps) {
+function MorphoViewerComponent({ className, swc }: MorphoViewerProps) {
   const refDiv = useRef<HTMLDivElement | null>(null);
   const refMorphoCanvas = useRef(new MorphologyCanvas());
   const morphoCanvas = refMorphoCanvas.current;
@@ -44,8 +43,9 @@ function MorphoViewerComponent({ className, swc, mode }: MorphoViewerProps) {
   useEffect(() => {
     morphoCanvas.canvas = refCanvas.current;
     morphoCanvas.swc = swc;
-    if (mode === 'dark') morphoCanvas.colors.background = '#000';
-    else if (mode === 'light') morphoCanvas.colors.background = '#fff';
+    morphoCanvas.colors.background = '#777';
+    if (isDarkMode) morphoCanvas.colors.background = '#000';
+    else morphoCanvas.colors.background = '#fff';
     const handleWarning = () => {
       setWarning(true);
     };
@@ -53,7 +53,7 @@ function MorphoViewerComponent({ className, swc, mode }: MorphoViewerProps) {
     return () => {
       morphoCanvas.eventMouseWheelWithoutCtrl.removeListener(handleWarning);
     };
-  }, [morphoCanvas, swc, setWarning, mode]);
+  }, [morphoCanvas, swc, setWarning, isDarkMode]);
 
   const handleFullscreen = () => {
     const div = refDiv.current;
