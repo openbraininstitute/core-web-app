08ced46f649566861d1d2c7c65f166b6fbba8e56
Pavlo Getta <pavlo.getta@openbraininstitute.org>
2026-04-30 13:36:14 +0200

    Allow project Costs page for project members, show vlab balance and transfer btn per-role

--- src/ui/segments/project/credits/balance-card.tsx
+++ src/ui/segments/project/credits/balance-card.tsx
@@ -1,4 +1,4 @@
-import { SwapOutlined } from '@ant-design/icons';
+import { SwapOutlined, WarningOutlined } from '@ant-design/icons';
 import { useQuery } from '@tanstack/react-query';
 
 import { useWorkspaceMembership } from '@/hooks/use-user-membership';
@@ -6,7 +6,9 @@ import { getVirtualLabAccountBalance } from '@/services/virtual-lab/labs';
 import { useWorkspace } from '@/ui/hooks/use-workspace';
 import { Button } from '@/ui/molecules/button';
 import { Card, CardContent } from '@/ui/molecules/card';
+import { Tooltip, TooltipContent, TooltipTrigger } from '@/ui/molecules/tooltip';
 import { keyBuilder } from '@/ui/use-query-keys/workspace';
+import { cn } from '@/utils/css-class';
 
 type Props = {
   onTransferCredits?: () => void;
@@ -27,28 +29,46 @@ export function BalanceCard({ onTransferCredits }: Props) {
     <Card shadowless>
       <CardContent className="flex items-center justify-between">
         <div className="flex items-center justify-center gap-10">
-          <div className="text-primary-9 flex flex-col gap-1.5">
-            <div className="font-light">Virtual lab credits</div>
-            <div className="text-xl font-bold">{virtualLabBalance}</div>
-          </div>
+          {isAdmin && (
+            <div className="text-primary-9 flex flex-col gap-1.5">
+              <div className="font-light">Virtual lab credits</div>
+              <div className="text-xl font-bold">{virtualLabBalance}</div>
+            </div>
+          )}
           <div className="text-primary-9 flex flex-col gap-1.5">
             <div className="font-light">Project credits</div>
             <div className="text-xl font-bold">{ProjectBalance?.balance}</div>
           </div>
         </div>
-        {isAdmin && (
-          <Button
-            rounded
-            borderless
-            className="group text-primary-9 hover:bg-primary-8 bg-white px-4 shadow-2xl select-none hover:border-white hover:text-white"
-            size="md"
-            variant="outline"
-            onClick={onTransferCredits}
+
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <span className={cn({ 'cursor-not-allowed': !isAdmin })}>
+              <Button
+                rounded
+                borderless
+                className="text-primary-9 hover:bg-primary-8 not-disabled:shadow-2xl hover:border-white hover:text-white disabled:text-gray-500"
+                size="md"
+                variant="outline"
+                onClick={isAdmin ? onTransferCredits : undefined}
+                disabled={!isAdmin}
+              >
+                Transfer credits
+                <SwapOutlined />
+              </Button>
+            </span>
+          </TooltipTrigger>
+          <TooltipContent
+            avoidCollisions
+            side="left"
+            sideOffset={1}
+            collisionPadding={{ bottom: 20 }}
+            className="text-primary-8 max-w-[200px] bg-white text-base text-balance shadow-lg"
+            arrowClassName="bg-white"
           >
-            Transfer credits
-            <SwapOutlined />
-          </Button>
-        )}
+            <WarningOutlined /> Only available to Virtual Lab admins
+          </TooltipContent>
+        </Tooltip>
       </CardContent>
     </Card>
   );
