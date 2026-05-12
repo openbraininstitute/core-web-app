2825d72f9da7ebe10904abffd75bb830c1db1d71
Nicolas Frank <58003267+WonderPG@users.noreply.github.com>
2026-05-12 15:23:11 +0200

    Point to neuroagent-ts

--- src/config/schema.ts
+++ src/config/schema.ts
@@ -120,7 +120,7 @@ const configFields = {
 } as const;
 
 const platformApiUrlFields = {
-  AI_AGENT_URL: '/agent',
+  AI_AGENT_URL: '/agent-ts/api',
   AUTH_MANAGER_URL: '/auth-manager/v1',
   CELL_API_URL: '/circuit',
   ENTITY_CORE_URL: '/entitycore',
