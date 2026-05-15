241c73bfcb8562365eb6fec94d7a949b7c72d80e
Pavlo Getta <pavlo.getta@openbraininstitute.org>
2026-05-15 12:56:10 +0200

    Make nodes table overflow the preview image

--- src/features/scan-config/components/model-preview/circuit-preview.tsx
+++ src/features/scan-config/components/model-preview/circuit-preview.tsx
@@ -57,44 +57,41 @@ export function CircuitImage({ className, circuit }: CircuitPreviewProps) {
 
   return (
     <div className={classNames(styles.container, className)}>
-      {tableMode !== 'full' && (
-        <div className={classNames(styles.imageSlot, styles[`imageSlot_${tableMode}`])}>
-          {isLoading && (
-            <Skeleton className="flex rounded-2xl items-center justify-center w-full h-full">
-              <ImageIcon className="w-20 h-20 text-gray-300 rounded-2xl" />
-            </Skeleton>
-          )}
-          {!isLoading && (error || !data) && (
-            <Skeleton
-              active={false}
-              className="flex rounded-2xl items-center justify-center w-full h-full"
-            >
-              <BrokenImageIcon className="w-20 h-20 text-gray-300 rounded-2xl" />
-            </Skeleton>
-          )}
-          {!isLoading && !error && data && !loaded && (
-            <Skeleton className="flex rounded-2xl items-center justify-center w-full h-full">
-              <ImageIcon className="w-20 h-20 text-gray-300 rounded-2xl" />
-            </Skeleton>
-          )}
-          {!isLoading && !error && data && loaded && (
-            <div
-              id="scan-config-circuit-preview"
-              className="w-full h-full min-h-0 p-2 overflow-hidden rounded-2xl shadow-[inset_0_0_0_1px_#fff,0_0_0_1px_rgba(0,0,0,0.04)] bg-white"
-            >
-              <div className="w-full h-full overflow-hidden bg-white rounded-2xl [&_.ant-image]:block! [&_.ant-image]:w-full! [&_.ant-image]:h-full! [&_.ant-image-img]:w-full! [&_.ant-image-img]:h-full! [&_.ant-image-img]:object-contain!">
-                <AntdImage
-                  src={data}
-                  alt="Circuit preview"
-                  className="block! w-full! h-full! [&_.ant-image-mask]:rounded-2xl"
-                  style={{ width: '100%', height: '100%', display: 'block' }}
-                />
-              </div>
+      <div className={styles.imageSlot}>
+        {isLoading && (
+          <Skeleton className="flex rounded-2xl items-center justify-center w-full h-full">
+            <ImageIcon className="w-20 h-20 text-gray-300 rounded-2xl" />
+          </Skeleton>
+        )}
+        {!isLoading && (error || !data) && (
+          <Skeleton
+            active={false}
+            className="flex rounded-2xl items-center justify-center w-full h-full"
+          >
+            <BrokenImageIcon className="w-20 h-20 text-gray-300 rounded-2xl" />
+          </Skeleton>
+        )}
+        {!isLoading && !error && data && !loaded && (
+          <Skeleton className="flex rounded-2xl items-center justify-center w-full h-full">
+            <ImageIcon className="w-20 h-20 text-gray-300 rounded-2xl" />
+          </Skeleton>
+        )}
+        {!isLoading && !error && data && loaded && (
+          <div id="scan-config-circuit-preview" className="w-full h-full min-h-0 p-2">
+            <div className="w-full h-full overflow-hidden [&_.ant-image]:block! [&_.ant-image]:w-full! [&_.ant-image]:h-full! [&_.ant-image-img]:w-full! [&_.ant-image-img]:h-full! [&_.ant-image-img]:object-contain!">
+              <AntdImage
+                src={data}
+                alt="Circuit preview"
+                className="block! w-full! h-full!"
+                style={{ width: '100%', height: '100%', display: 'block' }}
+              />
             </div>
-          )}
-        </div>
-      )}
-      <CircuitNodesTable circuit={circuit} onModeChange={setTableMode} />
+          </div>
+        )}
+      </div>
+      <div className={styles.tableSlot} data-mode={tableMode}>
+        <CircuitNodesTable circuit={circuit} onModeChange={setTableMode} />
+      </div>
     </div>
   );
 }
