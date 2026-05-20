36d4f06252381d8dce91d1da218a4f3485a470e1
Tolokoban <contact@tolokoban.org>
2026-05-20 12:29:47 +0200

    Fix modal dialog

--- src/ui/segments/workflows/simulate/single-neuron/single-neuron-synaptome/item/synaptic-input-deletion-confirmation-dialog/synaptic-input-deletion-confirmation-dialog.tsx
+++ src/ui/segments/workflows/simulate/single-neuron/single-neuron-synaptome/item/synaptic-input-deletion-confirmation-dialog/synaptic-input-deletion-confirmation-dialog.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 import { Button } from '@/ui/molecules/button';
 import { cn } from '@/utils/css-class';
 
@@ -19,32 +21,40 @@ export function SynapticInputDeletionConfirmationDialog({
   onOpenChange,
   onConfirm,
 }: SynapticInputDeletionConfirmationDialogProps) {
-  const handleClose = () => onOpenChange(false);
+  const refDialog = React.useRef<HTMLDialogElement | null>(null);
+  const handleClose = () => {
+    const dialog = refDialog.current;
+    if (!dialog) return;
 
-  if (!open) return null;
+    dialog.close();
+    onOpenChange(false);
+  };
+  React.useEffect(() => {
+    const dialog = refDialog.current;
+    if (!dialog) return;
+
+    if (open) dialog.showModal();
+    else dialog.close();
+  }, [open]);
 
   return (
     <dialog
+      ref={refDialog}
       className={cn(className, styles.synapticInputDeletionConfirmationDialog)}
-      open={open}
-      onClose={handleClose}
       closedby="any"
-      onClick={handleClose}
-      onKeyDown={handleClose}
+      onClose={handleClose}
     >
-      <div>
-        <p className="text-primary-9 text-justify text-sm">
-          Are you sure you want to delete this synaptic input configuration?
-        </p>
-        <footer>
-          <Button variant="outline" onClick={handleClose}>
-            Cancel
-          </Button>
-          <Button variant="success" onClick={onConfirm}>
-            Confirm
-          </Button>
-        </footer>
-      </div>
+      <p className="text-primary-9 text-justify text-sm">
+        Are you sure you want to delete this synaptic input configuration?
+      </p>
+      <footer>
+        <Button variant="outline" onClick={handleClose}>
+          Cancel
+        </Button>
+        <Button variant="success" onClick={onConfirm}>
+          Confirm
+        </Button>
+      </footer>
     </dialog>
   );
 }
