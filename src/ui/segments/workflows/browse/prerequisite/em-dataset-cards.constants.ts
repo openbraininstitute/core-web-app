ce5ec28d2877b97f5bb88db558bc2be525db9a8d
bilalesi <b_meddah@esi.dz>
2026-06-19 10:02:13 +0200

    fix the right id of the em dataset

--- src/ui/segments/workflows/browse/prerequisite/em-dataset-cards.constants.ts
+++ src/ui/segments/workflows/browse/prerequisite/em-dataset-cards.constants.ts
@@ -3,5 +3,5 @@ export const EM_DENSE_RECONSTRUCTION_DATASET_TYPE = 'em_dense_reconstruction_dat
 /** Portion 65 of the IARPA MICrONS dataset */
 export const MICRONS_PORTION_65_DATASET_IDS = [
   '0fbc27b9-9ea5-4d54-92db-1a19fbfffb87',
-  'c4f83b9a-58fd-4ec1-8a21-bdea9c5cda72',
+  '6ee16ab4-3068-46e6-86c9-4e1f6dde5927',
 ] as const;
