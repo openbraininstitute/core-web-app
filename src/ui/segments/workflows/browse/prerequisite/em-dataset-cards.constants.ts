859edefab5fead3967d8a06d174c5c841f0e6954
bilalesi <b_meddah@esi.dz>
2026-06-19 09:23:59 +0200

    fix: add prod em-dataset-id

--- src/ui/segments/workflows/browse/prerequisite/em-dataset-cards.constants.ts
+++ src/ui/segments/workflows/browse/prerequisite/em-dataset-cards.constants.ts
@@ -1,4 +1,7 @@
 export const EM_DENSE_RECONSTRUCTION_DATASET_TYPE = 'em_dense_reconstruction_dataset';
 
 /** Portion 65 of the IARPA MICrONS dataset */
-export const MICRONS_PORTION_65_DATASET_IDS = ['0fbc27b9-9ea5-4d54-92db-1a19fbfffb87'] as const;
+export const MICRONS_PORTION_65_DATASET_IDS = [
+  '0fbc27b9-9ea5-4d54-92db-1a19fbfffb87',
+  'c4f83b9a-58fd-4ec1-8a21-bdea9c5cda72',
+] as const;
